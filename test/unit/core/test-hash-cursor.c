e99be20b6731f5d827d20b57dccc997b0ec4e1a5
Kouhei Sutou <kou@cozmixng.org>
2009-03-15 12:36:49 +0900

    * test/unit/core/test-inverted-index.c: make buildable.

--- test/unit/core/test-hash-cursor.c
+++ test/unit/core/test-hash-cursor.c
@@ -1,6 +1,6 @@
 /* -*- c-basic-offset: 2; coding: utf-8 -*- */
 /*
-  Copyright (C) 2008  Kouhei Sutou <kou@cozmixng.org>
+  Copyright (C) 2008-2009  Kouhei Sutou <kou@cozmixng.org>
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
