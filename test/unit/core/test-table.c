groonga/groonga

commit 390b63a1df4758a16fc97fd9a3a86834d4af2448

changed the default value of DEFAULT_TOKENIZER into NULL
* groonga.h (grn_table_create): changed default value of DEFAULT_TOKENIZER into NULL.
* groonga.h (grn_obj_get_info): support GRN_INFO_DEFAULT_TOKENIZER.

test/unit/core/test-table.c
@@ -100,22 +100,6 @@ test_temporary_table_no_path(gpointer data)
   cut_assert_equal_string(NULL, grn_obj_path(&context, table));
 }
 
-/* FIXME!!! */
-grn_rc grn_table_get_info(grn_ctx *ctx, grn_obj *table, grn_obj_flags *flags,
-                          grn_encoding *encoding, grn_obj **tokenizer);
-
-void
-data_temporary_table_default_tokenizer(void)
-{
-#define ADD_DATA(label, flags)                                          \
-  cut_add_data(label, GINT_TO_POINTER(flags), NULL, NULL)
-
-  ADD_DATA("hash", GRN_OBJ_TABLE_HASH_KEY);
-  ADD_DATA("patricia trie", GRN_OBJ_TABLE_PAT_KEY);
-
-#undef ADD_DATA
-}
-
 void
 test_temporary_table_default_tokenizer(gpointer data)
 {
@@ -129,7 +113,7 @@ test_temporary_table_default_tokenizer(gpointer data)
                            NULL, sizeof(grn_id));
   grn_obj_set_info(&context, table, GRN_INFO_DEFAULT_TOKENIZER,
                    OBJECT("<token:trigram>"));
-  grn_table_get_info(&context, table, NULL, NULL, &tokenizer);
+  tokenizer = grn_obj_get_info(&context, table, GRN_INFO_DEFAULT_TOKENIZER, NULL);
   grn_obj_name(&context, tokenizer, name, sizeof(name));
   cut_assert_equal_string("<token:trigram>", name);
 }

lib/db.c
@@ -3022,14 +3022,14 @@ grn_obj_get_info(grn_ctx *ctx, grn_obj *obj, grn_info_type type, grn_obj *valueb
     ERR(GRN_INVALID_ARGUMENT, "grn_obj_get_info failed");
     goto exit;
   }
-  if (!valuebuf) {
-    if (!(valuebuf = grn_obj_open(ctx, GRN_BULK, 0, 0))) {
-      ERR(GRN_INVALID_ARGUMENT, "grn_obj_get_info failed");
-      goto exit;
-    }
-  }
   switch (type) {
   case GRN_INFO_ENCODING :
+    if (!valuebuf) {
+      if (!(valuebuf = grn_obj_open(ctx, GRN_BULK, 0, 0))) {
+        ERR(GRN_INVALID_ARGUMENT, "grn_obj_get_info failed");
+        goto exit;
+      }
+    }
     {
       grn_encoding enc;
       switch (obj->header.type) {
@@ -3051,11 +3051,27 @@ grn_obj_get_info(grn_ctx *ctx, grn_obj *obj, grn_info_type type, grn_obj *valueb
     }
     break;
   case GRN_INFO_SOURCE :
+    if (!valuebuf) {
+      if (!(valuebuf = grn_obj_open(ctx, GRN_BULK, 0, 0))) {
+        ERR(GRN_INVALID_ARGUMENT, "grn_obj_get_info failed");
+        goto exit;
+      }
+    }
     if (!GRN_DB_OBJP(obj)) {
       ERR(GRN_INVALID_ARGUMENT, "only db_obj can accept GRN_INFO_SOURCE");
       goto exit;
     }
     grn_bulk_write(ctx, valuebuf, DB_OBJ(obj)->source, DB_OBJ(obj)->source_size);
+    break;
+  case GRN_INFO_DEFAULT_TOKENIZER :
+    switch (DB_OBJ(obj)->header.type) {
+    case GRN_TABLE_HASH_KEY :
+      valuebuf = ((grn_hash *)obj)->tokenizer;
+      break;
+    case GRN_TABLE_PAT_KEY :
+      valuebuf = ((grn_pat *)obj)->tokenizer;
+      break;
+    }
     break;
   default :
     /* todo */
@@ -3212,16 +3228,16 @@ grn_obj_set_info(grn_ctx *ctx, grn_obj *obj, grn_info_type type, grn_obj *value)
     rc = GRN_SUCCESS;
     break;
   case GRN_INFO_DEFAULT_TOKENIZER :
-    if (value && DB_OBJ(value)->header.type == GRN_PROC) {
+    if (!value || DB_OBJ(value)->header.type == GRN_PROC) {
       switch (DB_OBJ(obj)->header.type) {
       case GRN_TABLE_HASH_KEY :
         ((grn_hash *)obj)->tokenizer = value;
-        ((grn_hash *)obj)->header->tokenizer = DB_OBJ(value)->id;
+        ((grn_hash *)obj)->header->tokenizer = grn_obj_id(ctx, value);
         rc = GRN_SUCCESS;
         break;
       case GRN_TABLE_PAT_KEY :
         ((grn_pat *)obj)->tokenizer = value;
-        ((grn_pat *)obj)->header->tokenizer = DB_OBJ(value)->id;
+        ((grn_pat *)obj)->header->tokenizer = grn_obj_id(ctx, value);
         rc = GRN_SUCCESS;
         break;
       }

lib/hash.c
@@ -743,7 +743,7 @@ io_hash_init(grn_hash *ih, grn_ctx *ctx, const char *path, uint32_t key_size,
   header->max_offset = m - 1;
   header->n_entries = 0;
   header->n_garbages = 0;
-  header->tokenizer = GRN_DB_BIGRAM;
+  header->tokenizer = 0;
   GRN_DB_OBJ_SET_TYPE(ih, GRN_TABLE_HASH_KEY);
   ih->obj.flags = flags;
   ih->ctx = ctx;
@@ -799,7 +799,7 @@ tiny_hash_init(grn_hash *ah, grn_ctx *ctx, const char *path, uint32_t key_size,
   ah->n_garbages_ = 0;
   ah->n_entries_ = 0;
   ah->garbages = GRN_ID_NIL;
-  ah->tokenizer = grn_ctx_get(ctx, GRN_DB_BIGRAM);
+  ah->tokenizer = NULL;
   grn_tiny_array_init(ctx, &ah->a, entry_size, GRN_TINY_ARRAY_CLEAR);
   grn_tiny_array_init(ctx, &ah->bitmap, 1, GRN_TINY_ARRAY_CLEAR);
   return GRN_SUCCESS;

lib/ii.c
@@ -4892,8 +4892,12 @@ token_info_build(grn_ctx *ctx, grn_obj *lexicon, grn_ii *ii, const char *string,
       ti = token_info_open(ctx, lexicon, ii, key, size, token->pos, ef & EX_SUFFIX);
       break;
     case grn_token_done :
+      /*
       key = _grn_table_key(ctx, lexicon, tid, &size);
       ti = token_info_open(ctx, lexicon, ii, key, size, token->pos, ef);
+      */
+      ti = token_info_open(ctx, lexicon, ii, (char *)token->orig,
+                           token->orig_blen, 0, ef);
       break;
     case grn_token_not_found :
       ti = token_info_open(ctx, lexicon, ii, (char *)token->orig,

lib/pat.c
@@ -392,7 +392,7 @@ grn_pat_create(grn_ctx *ctx, const char *path, uint32_t key_size,
   header->curr_del = 0;
   header->curr_del2 = 0;
   header->curr_del3 = 0;
-  header->tokenizer = GRN_DB_BIGRAM;
+  header->tokenizer = 0;
   if (!(pat = GRN_MALLOC(sizeof(grn_pat)))) {
     grn_io_close(ctx, io);
     return NULL;

lib/token.c
@@ -470,7 +470,6 @@ grn_token_open(grn_ctx *ctx, grn_obj *table, const char *str, size_t str_len,
   grn_encoding encoding;
   grn_obj *tokenizer;
   if (grn_table_get_info(ctx, table, NULL, &encoding, &tokenizer)) { return NULL; }
-  if (!tokenizer) { return NULL; }
   if (!(token = GRN_MALLOC(sizeof(grn_token)))) { return NULL; }
   token->table = table;
   token->flags = flags;
@@ -483,16 +482,18 @@ grn_token_open(grn_ctx *ctx, grn_obj *table, const char *str, size_t str_len,
   token->pos = -1;
   token->status = grn_token_doing;
   token->force_prefix = 0;
-  token->pctx.user_data.ptr = NULL;
-  token->pctx.obj = table;
-  token->pctx.hooks = NULL;
-  token->pctx.currh = NULL;
-  token->pctx.phase = PROC_INIT;
-  token->pctx.data[0].ptr = (void *)str;
-  token->pctx.data[1].int_value = str_len;
-  token->pctx.data[2].int_value = 0;
-  ((grn_proc *)tokenizer)->funcs[PROC_INIT](ctx, table, &token->pctx.user_data,
-                                            3, token->pctx.data);
+  if (tokenizer) {
+    token->pctx.user_data.ptr = NULL;
+    token->pctx.obj = table;
+    token->pctx.hooks = NULL;
+    token->pctx.currh = NULL;
+    token->pctx.phase = PROC_INIT;
+    token->pctx.data[0].ptr = (void *)str;
+    token->pctx.data[1].int_value = str_len;
+    token->pctx.data[2].int_value = 0;
+    ((grn_proc *)tokenizer)->funcs[PROC_INIT](ctx, table, &token->pctx.user_data,
+                                              3, token->pctx.data);
+  }
   if (ctx->rc) {
     GRN_FREE(token);
     token = NULL;
@@ -508,22 +509,28 @@ grn_token_next(grn_ctx *ctx, grn_token *token)
   grn_obj *table = token->table;
   grn_obj *tokenizer = token->tokenizer;
   while (token->status != grn_token_done) {
-    token->pctx.data[0].ptr = NULL;
-    token->pctx.data[1].int_value = 0;
-    token->pctx.data[2].int_value = 0;
-    ((grn_proc *)tokenizer)->funcs[PROC_NEXT](ctx, table, &token->pctx.user_data,
-                                              4, token->pctx.data);
-    token->curr = token->pctx.data[0].ptr;
-    token->curr_size = token->pctx.data[1].int_value;
-    status = token->pctx.data[2].int_value;
-    token->status = (status & GRN_TOKEN_LAST) ? grn_token_done : grn_token_doing;
-    token->force_prefix = 0;
-    if (status & GRN_TOKEN_UNMATURED) {
-      if (status & GRN_TOKEN_OVERLAP) {
-        if (!(token->flags & GRN_TABLE_ADD)) { continue; }
-      } else {
-        if (status & GRN_TOKEN_LAST) { token->force_prefix = 1; }
+    if (tokenizer) {
+      token->pctx.data[0].ptr = NULL;
+      token->pctx.data[1].int_value = 0;
+      token->pctx.data[2].int_value = 0;
+      ((grn_proc *)tokenizer)->funcs[PROC_NEXT](ctx, table, &token->pctx.user_data,
+                                                4, token->pctx.data);
+      token->curr = token->pctx.data[0].ptr;
+      token->curr_size = token->pctx.data[1].int_value;
+      status = token->pctx.data[2].int_value;
+      token->status = (status & GRN_TOKEN_LAST) ? grn_token_done : grn_token_doing;
+      token->force_prefix = 0;
+      if (status & GRN_TOKEN_UNMATURED) {
+        if (status & GRN_TOKEN_OVERLAP) {
+          if (!(token->flags & GRN_TABLE_ADD)) { continue; }
+        } else {
+          if (status & GRN_TOKEN_LAST) { token->force_prefix = 1; }
+        }
       }
+    } else {
+      token->curr = token->orig;
+      token->curr_size = token->orig_blen;
+      token->status = grn_token_done;
     }
     if (token->flags & GRN_TABLE_ADD) {
       switch (table->header.type) {
@@ -583,9 +590,11 @@ grn_rc
 grn_token_close(grn_ctx *ctx, grn_token *token)
 {
   if (token) {
-    ((grn_proc *)token->tokenizer)->funcs[PROC_FIN](ctx, token->table,
-                                                    &token->pctx.user_data,
-                                                    0, token->pctx.data);
+    if (token->tokenizer) {
+      ((grn_proc *)token->tokenizer)->funcs[PROC_FIN](ctx, token->table,
+                                                      &token->pctx.user_data,
+                                                      0, token->pctx.data);
+    }
     GRN_FREE(token);
     return GRN_SUCCESS;
   } else {

test/unit/core/test-inverted-index.c
@@ -112,6 +112,10 @@ setup(void)
                              table_path,
                              GRN_OBJ_PERSISTENT | GRN_OBJ_TABLE_PAT_KEY,
                              type, VALUE_SIZE);
+
+  grn_obj_set_info(context, lexicon, GRN_INFO_DEFAULT_TOKENIZER,
+                   grn_ctx_get(context, GRN_DB_BIGRAM));
+
   g_free(table_path);
 
   vgram_path = g_build_filename(tmp_directory, "vgram", NULL);

commit 6f8a8baadfb20392d22e26cd1c14ff66e0581a47

revert a removed data function.
data_temporary_table_default_tokenizer() was removed in
390b63a1df4758a16fc97fd9a3a86834d4af2448 accidentally.

Pointed out by Yoshinori K. Okuji. Thanks!!!

test/unit/core/test-table.c
@@ -102,6 +102,18 @@ test_temporary_table_no_path(gpointer data)
 }
 
 void
+data_temporary_table_default_tokenizer(void)
+{
+#define ADD_DATA(label, flags)                                          \
+  cut_add_data(label, GINT_TO_POINTER(flags), NULL, NULL)
+
+  ADD_DATA("hash", GRN_OBJ_TABLE_HASH_KEY);
+  ADD_DATA("patricia trie", GRN_OBJ_TABLE_PAT_KEY);
+
+#undef ADD_DATA
+}
+
+void
 test_temporary_table_default_tokenizer(gpointer data)
 {
   grn_obj *table;