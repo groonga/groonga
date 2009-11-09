b37101e09a8e2aee3f2f65a103142c43eba1539f
Kouhei Sutou <kou@clear-code.com>
2009-11-09 17:41:24 +0900

    split static HTML tests.

---
+++ test/unit/http/test-static-html.rb
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2009  Kouhei Sutou <kou@clear-code.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License version 2.1 as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+class StaticHTMLTest < Test::Unit::TestCase
+  include GroongaTestUtils
+  include GroongaHTTPTestUtils
+
+  def setup
+    setup_server
+  end
+
+  def teardown
+    teardown_server
+  end
+
+  def test_normal
+    response = get("/index.html")
+    assert_equal("text/html", response.content_type)
+    assert_equal(File.read(File.join(@resource_dir, "index.html")),
+                 response.body)
+  end
+
+  def test_with_fragment
+    response = get("/index.html#anchor")
+    assert_equal("text/html", response.content_type)
+    assert_equal(File.read(File.join(@resource_dir, "index.html")),
+                 response.body)
+  end
+
+  def test_with_query
+    response = get("/index.html?key=value")
+    assert_equal("text/html", response.content_type)
+    assert_equal(File.read(File.join(@resource_dir, "index.html")),
+                 response.body)
+  end
+
+  def test_with_query_and_fragment
+    response = get("/index.html?key=value#anchor")
+    assert_equal("text/html", response.content_type)
+    assert_equal(File.read(File.join(@resource_dir, "index.html")),
+                 response.body)
+  end
+end
