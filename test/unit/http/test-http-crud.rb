c71de5b91916192d2e076ce786e7cb42b6acb11d
Kouhei Sutou <kou@clear-code.com>
2009-11-13 18:28:08 +0900

    revert accidently removed set tests.

--- test/unit/http/test-http-crud.rb
+++ test/unit/http/test-http-crud.rb
@@ -163,6 +163,57 @@ module HTTPCRUDTest
     end
   end
 
+  class HTTPSetTest < Test::Unit::TestCase
+    include GroongaHTTPTestUtils
+
+    def setup
+      setup_server
+    end
+
+    def teardown
+      teardown_server
+    end
+
+    def test_normal
+      populate_users
+
+      response = get(command_path(:get,
+                                  :table => "users",
+                                  :key => "ryoqun",
+                                  :output_columns => "_key real_name"))
+      assert_response([[Result::SUCCESS],
+                       ["ryoqun", "Ryo Onodera"]],
+                      response,
+                      :content_type => "application/json")
+
+      response = get(command_path(:set,
+                                  :table => "users",
+                                  :key => "ryoqun",
+                                  :values => json({:real_name => "daijiro"})))
+      assert_response([[Result::SUCCESS]], response,
+                      :content_type => "application/json")
+
+      response = get(command_path(:get,
+                                  :table => "users",
+                                  :key => "ryoqun",
+                                  :output_columns => "_key real_name"))
+      assert_response([[Result::SUCCESS],
+                       ["ryoqun", "daijiro"]],
+                      response,
+                      :content_type => "application/json")
+    end
+
+    def test_nonexistent_table
+      response = get(command_path(:set,
+                                  :table => "nonexistent",
+                                  :key => "mori",
+                                  :values => json({:_value => "mori daijiro"})))
+      assert_response([[Result::UNKNOWN_ERROR, "table doesn't exist"]],
+                      response,
+                      :content_type => "application/json")
+    end
+  end
+
   class HTTPGetTest < Test::Unit::TestCase
     include GroongaHTTPTestUtils
 
