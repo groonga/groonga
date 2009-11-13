86af6a29eaba3f4e0b2feff1ac01b186028e20f3
Kouhei Sutou <kou@clear-code.com>
2009-11-13 16:01:30 +0900

    split add/set/get.

--- test/unit/http/test-http-crud.rb
+++ test/unit/http/test-http-crud.rb
@@ -15,98 +15,127 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-class HTTPCRUDTest < Test::Unit::TestCase
-  include GroongaHTTPTestUtils
+module HTTPCRUDTest
+  class HTTPAddTest < Test::Unit::TestCase
+    include GroongaHTTPTestUtils
 
-  def setup
-    setup_server
-    populate_users
+    def setup
+      setup_server
+    end
+
+    def teardown
+      teardown_server
+    end
+
+    def test_normal
+      populate_users
+
+      response = get(command_path(:get,
+                                  :table => "users",
+                                  :key => "mori",
+                                  :output_columns => "_key real_name"))
+      assert_response([[Result::UNKNOWN_ERROR, "not found"]],
+                      response,
+                      :content_type => "application/json")
+
+      response = get(command_path(:add,
+                                  :table => "users",
+                                  :key => "mori",
+                                  :values => json({:real_name => "daijiro"}),
+                                  :output_columns => "_key real_name"))
+      assert_response([[Result::SUCCESS],
+                       ["mori", "mori daijiro"]], response,
+                      :content_type => "application/json")
+
+      response = get(command_path(:get,
+                                  :table => "users",
+                                  :key => "mori",
+                                  :output_columns => "_key real_name"))
+      assert_response([[Result::SUCCESS],
+                       ["mori", "daijiro"]],
+                      response,
+                      :content_type => "application/json")
+    end
+
+    def test_nonexistent_table
+      response = get(command_path(:add,
+                                  :table => "nonexistent",
+                                  :key => "mori"))
+      assert_response([[Result::UNKNOWN_ERROR, "table doesn't exist"]],
+                       response,
+                       :content_type => "application/json")
+    end
   end
 
-  def teardown
-    teardown_server
+  class HTTPSetTest < Test::Unit::TestCase
+    include GroongaHTTPTestUtils
+
+    def setup
+      setup_server
+    end
+
+    def teardown
+      teardown_server
+    end
+
+    def test_normal
+      populate_users
+
+      response = get(command_path(:get,
+                                  :table => "users",
+                                  :key => "ryoqun",
+                                  :output_columns => "_key real_name"))
+      assert_response([[Result::SUCCESS],
+                       ["ryoqun", "Ryo Onodera"]],
+                      response,
+                      :content_type => "application/json")
+
+      response = get(command_path(:set,
+                                  :table => "users",
+                                  :key => "ryoqun",
+                                  :values => json({:real_name => "daijiro"})))
+      assert_response([[Result::SUCCESS]], response,
+                      :content_type => "application/json")
+
+      response = get(command_path(:get,
+                                  :table => "users",
+                                  :key => "ryoqun",
+                                  :output_columns => "_key real_name"))
+      assert_response([[Result::SUCCESS],
+                       ["ryoqun", "daijiro"]],
+                      response,
+                      :content_type => "application/json")
+    end
+
+    def test_nonexistent_table
+      response = get(command_path(:set,
+                                  :table => "nonexistent",
+                                  :key => "mori",
+                                  :values => json({:_value => "mori daijiro"})))
+      assert_response([[Result::UNKNOWN_ERROR, "table doesn't exist"]],
+                      response,
+                      :content_type => "application/json")
+    end
   end
 
-  def test_set
-    response = get(command_path(:get,
-                                :table => "users",
-                                :key => "ryoqun",
-                                :output_columns => "_key real_name"))
-    assert_response([[Result::SUCCESS],
-                     ["ryoqun", "Ryo Onodera"]],
-                    response,
-                    :content_type => "application/json")
+  class HTTPGetTest < Test::Unit::TestCase
+    include GroongaHTTPTestUtils
 
-    response = get(command_path(:set,
-                                :table => "users",
-                                :key => "ryoqun",
-                                :values => json({:real_name => "mori daijiro"})))
-    assert_response([[Result::SUCCESS]], response,
-                    :content_type => "application/json")
+    def setup
+      setup_server
+    end
 
-    response = get(command_path(:get,
-                                :table => "users",
-                                :key => "ryoqun",
-                                :output_columns => "_key real_name"))
-    assert_response([[Result::SUCCESS],
-                     ["ryoqun", "mori daijiro"]],
-                    response,
-                    :content_type => "application/json")
-  end
+    def teardown
+      teardown_server
+    end
 
-  def test_add
-    response = get(command_path(:get,
-                                :table => "users",
-                                :key => "mori",
-                                :output_columns => "_key real_name"))
-    assert_response([[Result::UNKNOWN_ERROR, "not found"]],
-                    response,
-                    :content_type => "application/json")
-
-    response = get(command_path(:add,
-                                :table => "users",
-                                :key => "mori",
-                                :values => json({:real_name => "mori daijiro"}),
-                                :output_columns => "_key real_name"))
-    assert_response([[Result::SUCCESS],
-                     ["mori", "mori daijiro"]], response,
-                    :content_type => "application/json")
-
-    response = get(command_path(:get,
-                                :table => "users",
-                                :key => "mori",
-                                :output_columns => "_key real_name"))
-    assert_response([[Result::SUCCESS],
-                     ["mori", "mori daijiro"]],
-                    response,
-                    :content_type => "application/json")
-  end
-
-  def test_add_to_nonexistent_table
-    response = get(command_path(:add,
-                                :table => "nonexistent",
-                                :key => "mori"))
-    assert_response([[Result::UNKNOWN_ERROR, "table doesn't exist"]],
-                     response,
-                     :content_type => "application/json")
-  end
-
-  def test_set_to_nonexistent_table
-    response = get(command_path(:set,
-                                :table => "nonexistent",
-                                :key => "mori",
-                                :values => json({:_value => "mori daijiro"})))
-    assert_response([[Result::UNKNOWN_ERROR, "table doesn't exist"]],
-                     response,
-                     :content_type => "application/json")
-  end
-
-  def test_get_from_nonexistent_table
-    response = get(command_path(:get,
-                                :table => "nonexistent",
-                                :key => "mori"))
-    assert_response([[Result::UNKNOWN_ERROR, "table doesn't exist"]],
-                     response,
-                     :content_type => "application/json")
+    def test_nonexistent_table
+      response = get(command_path(:get,
+                                  :table => "nonexistent",
+                                  :key => "mori"))
+      assert_response([[Result::UNKNOWN_ERROR, "table doesn't exist"]],
+                      response,
+                      :content_type => "application/json")
+    end
   end
 end
