2c942c8f02ba70af5ccb8257d78ec138c9003680
Kouhei Sutou <kou@clear-code.com>
2009-11-24 19:04:36 +0900

    add tests for int64/uint64 -> XXX cast.

--- test/unit/core/test-cast.c
+++ test/unit/core/test-cast.c
@@ -67,6 +67,32 @@ void test_uint32_to_uint64(void);
 void test_uint32_to_float(void);
 void test_uint32_to_time(void);
 
+void data_int64_to_bool(void);
+void test_int64_to_bool(gconstpointer data);
+void test_int64_to_int8(void);
+void test_int64_to_uint8(void);
+void test_int64_to_int16(void);
+void test_int64_to_uint16(void);
+void test_int64_to_int32(void);
+void test_int64_to_uint32(void);
+void test_int64_to_int64(void);
+void test_int64_to_uint64(void);
+void test_int64_to_float(void);
+void test_int64_to_time(void);
+
+void data_uint64_to_bool(void);
+void test_uint64_to_bool(gconstpointer data);
+void test_uint64_to_int8(void);
+void test_uint64_to_uint8(void);
+void test_uint64_to_int16(void);
+void test_uint64_to_uint16(void);
+void test_uint64_to_int32(void);
+void test_uint64_to_uint32(void);
+void test_uint64_to_int64(void);
+void test_uint64_to_uint64(void);
+void test_uint64_to_float(void);
+void test_uint64_to_time(void);
+
 static grn_logger_info *logger;
 static grn_ctx context;
 static grn_obj src, dest;
@@ -453,5 +479,255 @@ test_uint32_to_text(void)
 {
   grn_obj_reinit(&context, &dest, GRN_DB_TEXT, 0);
   cast_uint32(2929);
+  cut_assert_equal_string("2929", GRN_TEXT_VALUE(&dest));
+}
+
+static void
+cast_int64(int number)
+{
+  grn_obj_reinit(&context, &src, GRN_DB_INT64, 0);
+  GRN_INT64_SET(&context, &src, number);
+  grn_test_assert(grn_obj_cast(&context, &src, &dest, GRN_FALSE));
+}
+
+void
+data_int64_to_bool(void)
+{
+#define ADD_DATA(label, expected, number)               \
+  gcut_add_datum(label,                                 \
+                 "expected", G_TYPE_UINT, expected,     \
+                 "number", G_TYPE_INT, number,          \
+                 NULL)
+
+  ADD_DATA("true", GRN_TRUE, 1);
+  ADD_DATA("false", GRN_FALSE, 0);
+  ADD_DATA("not zero", GRN_TRUE, 100);
+
+#undef ADD_DATA
+}
+
+void
+test_int64_to_bool(gconstpointer data)
+{
+  grn_obj_reinit(&context, &dest, GRN_DB_BOOL, 0);
+  cast_int64(gcut_data_get_int(data, "number"));
+  cut_assert_equal_boolean(gcut_data_get_uint(data, "expected"),
+                           GRN_BOOL_VALUE(&dest));
+}
+
+void
+test_int64_to_int8(void)
+{
+  grn_obj_reinit(&context, &dest, GRN_DB_INT8, 0);
+  cast_int64(-29);
+  cut_assert_equal_int(-29, GRN_INT8_VALUE(&dest));
+}
+
+void
+test_int64_to_uint8(void)
+{
+  grn_obj_reinit(&context, &dest, GRN_DB_UINT8, 0);
+  cast_int64(29);
+  cut_assert_equal_uint(29, GRN_UINT8_VALUE(&dest));
+}
+
+void
+test_int64_to_int16(void)
+{
+  grn_obj_reinit(&context, &dest, GRN_DB_INT16, 0);
+  cast_int64(-2929);
+  cut_assert_equal_int(-2929, GRN_INT16_VALUE(&dest));
+}
+
+void
+test_int64_to_uint16(void)
+{
+  grn_obj_reinit(&context, &dest, GRN_DB_UINT16, 0);
+  cast_int64(2929);
+  cut_assert_equal_uint(2929, GRN_UINT16_VALUE(&dest));
+}
+
+void
+test_int64_to_int32(void)
+{
+  grn_obj_reinit(&context, &dest, GRN_DB_INT32, 0);
+  cast_int64(-29292929);
+  cut_assert_equal_int(-29292929, GRN_INT32_VALUE(&dest));
+}
+
+void
+test_int64_to_uint32(void)
+{
+  grn_obj_reinit(&context, &dest, GRN_DB_UINT32, 0);
+  cast_int64(29292929);
+  cut_assert_equal_uint(29292929, GRN_UINT32_VALUE(&dest));
+}
+
+void
+test_int64_to_int64(void)
+{
+  grn_obj_reinit(&context, &dest, GRN_DB_INT64, 0);
+  cast_int64(-29292929);
+  cut_assert_equal_int(-29292929, GRN_INT64_VALUE(&dest));
+}
+
+void
+test_int64_to_uint64(void)
+{
+  grn_obj_reinit(&context, &dest, GRN_DB_UINT64, 0);
+  cast_int64(2929292929);
+  cut_assert_equal_uint(2929292929, GRN_UINT64_VALUE(&dest));
+}
+
+void
+test_int64_to_float(void)
+{
+  grn_obj_reinit(&context, &dest, GRN_DB_FLOAT, 0);
+  cast_int64(29);
+  cut_assert_equal_double(29, 0.1, GRN_FLOAT_VALUE(&dest));
+}
+
+void
+test_int64_to_time(void)
+{
+  long long int sec, usec;
+
+  grn_obj_reinit(&context, &dest, GRN_DB_TIME, 0);
+  cast_int64(1259009530);
+  GRN_TIME_UNPACK(GRN_TIME_VALUE(&dest), sec, usec);
+  cut_assert_equal_int(1259009530, sec);
+  cut_assert_equal_int(0, usec);
+}
+
+void
+test_int64_to_text(void)
+{
+  grn_obj_reinit(&context, &dest, GRN_DB_TEXT, 0);
+  cast_int64(2929);
+  cut_assert_equal_string("2929", GRN_TEXT_VALUE(&dest));
+}
+
+static void
+cast_uint64(int number)
+{
+  grn_obj_reinit(&context, &src, GRN_DB_UINT64, 0);
+  GRN_UINT64_SET(&context, &src, number);
+  grn_test_assert(grn_obj_cast(&context, &src, &dest, GRN_FALSE));
+}
+
+void
+data_uint64_to_bool(void)
+{
+#define ADD_DATA(label, expected, number)               \
+  gcut_add_datum(label,                                 \
+                 "expected", G_TYPE_UINT, expected,     \
+                 "number", G_TYPE_UINT, number,         \
+                 NULL)
+
+  ADD_DATA("true", GRN_TRUE, 1);
+  ADD_DATA("false", GRN_FALSE, 0);
+  ADD_DATA("not zero", GRN_TRUE, 100);
+
+#undef ADD_DATA
+}
+
+void
+test_uint64_to_bool(gconstpointer data)
+{
+  grn_obj_reinit(&context, &dest, GRN_DB_BOOL, 0);
+  cast_uint64(gcut_data_get_uint(data, "number"));
+  cut_assert_equal_boolean(gcut_data_get_uint(data, "expected"),
+                           GRN_BOOL_VALUE(&dest));
+}
+
+void
+test_uint64_to_int8(void)
+{
+  grn_obj_reinit(&context, &dest, GRN_DB_INT8, 0);
+  cast_uint64(29);
+  cut_assert_equal_int(29, GRN_UINT8_VALUE(&dest));
+}
+
+void
+test_uint64_to_uint8(void)
+{
+  grn_obj_reinit(&context, &dest, GRN_DB_UINT8, 0);
+  cast_uint64(29);
+  cut_assert_equal_uint(29, GRN_UINT8_VALUE(&dest));
+}
+
+void
+test_uint64_to_int16(void)
+{
+  grn_obj_reinit(&context, &dest, GRN_DB_INT16, 0);
+  cast_uint64(2929);
+  cut_assert_equal_int(2929, GRN_UINT16_VALUE(&dest));
+}
+
+void
+test_uint64_to_uint16(void)
+{
+  grn_obj_reinit(&context, &dest, GRN_DB_UINT16, 0);
+  cast_uint64(2929);
+  cut_assert_equal_uint(2929, GRN_UINT16_VALUE(&dest));
+}
+
+void
+test_uint64_to_int32(void)
+{
+  grn_obj_reinit(&context, &dest, GRN_DB_UINT32, 0);
+  cast_uint64(29292929);
+  cut_assert_equal_int(29292929, GRN_INT32_VALUE(&dest));
+}
+
+void
+test_uint64_to_uint32(void)
+{
+  grn_obj_reinit(&context, &dest, GRN_DB_UINT32, 0);
+  cast_uint64(29292929);
+  cut_assert_equal_uint(29292929, GRN_UINT32_VALUE(&dest));
+}
+
+void
+test_uint64_to_int64(void)
+{
+  grn_obj_reinit(&context, &dest, GRN_DB_INT64, 0);
+  cast_uint64(29292929);
+  cut_assert_equal_int(29292929, GRN_INT64_VALUE(&dest));
+}
+
+void
+test_uint64_to_uint64(void)
+{
+  grn_obj_reinit(&context, &dest, GRN_DB_UINT64, 0);
+  cast_uint64(2929292929);
+  cut_assert_equal_uint(2929292929, GRN_UINT64_VALUE(&dest));
+}
+
+void
+test_uint64_to_float(void)
+{
+  grn_obj_reinit(&context, &dest, GRN_DB_FLOAT, 0);
+  cast_uint64(29);
+  cut_assert_equal_double(29, 0.1, GRN_FLOAT_VALUE(&dest));
+}
+
+void
+test_uint64_to_time(void)
+{
+  long long int sec, usec;
+
+  grn_obj_reinit(&context, &dest, GRN_DB_TIME, 0);
+  cast_uint64(1259009530);
+  GRN_TIME_UNPACK(GRN_TIME_VALUE(&dest), sec, usec);
+  cut_assert_equal_int(1259009530, sec);
+  cut_assert_equal_int(0, usec);
+}
+
+void
+test_uint64_to_text(void)
+{
+  grn_obj_reinit(&context, &dest, GRN_DB_TEXT, 0);
+  cast_uint64(2929);
   cut_assert_equal_string("2929", GRN_TEXT_VALUE(&dest));
 }
