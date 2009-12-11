c2c954fbe2ffba7c67d692a1d9a1c2cb87a9f11d
Kouhei Sutou <kou@clear-code.com>
2009-12-11 17:17:55 +0900

    support ^= in script syntax.

--- lib/db.c
+++ lib/db.c
@@ -7071,6 +7071,7 @@ grn_expr_append_obj(grn_ctx *ctx, grn_obj *expr, grn_obj *obj, grn_operator op, 
     case GRN_OP_SHIFTRR_ASSIGN :
     case GRN_OP_AND_ASSIGN :
     case GRN_OP_OR_ASSIGN :
+    case GRN_OP_XOR_ASSIGN :
       {
         if (obj) {
           type = obj->header.type;
@@ -8423,6 +8424,18 @@ grn_expr_exec(grn_ctx *ctx, grn_obj *expr, int nargs)
           ARITHMETIC_OPERATION_NO_CHECK,
           {
             ERR(GRN_INVALID_ARGUMENT, "variable |= \"string\" isn't supported");
+            goto exit;
+          });
+        break;
+      case GRN_OP_XOR_ASSIGN :
+        ARITHMETIC_OPERATION_AND_ASSIGN_DISPATCH(
+          INTEGER_ARITHMETIC_OPERATION_BITWISE_XOR,
+          INTEGER_ARITHMETIC_OPERATION_BITWISE_XOR,
+          FLOAT_ARITHMETIC_OPERATION_BITWISE_XOR,
+          ARITHMETIC_OPERATION_NO_CHECK,
+          ARITHMETIC_OPERATION_NO_CHECK,
+          {
+            ERR(GRN_INVALID_ARGUMENT, "variable ^= \"string\" isn't supported");
             goto exit;
           });
         break;
--- test/unit/core/test-expr-script.c
+++ test/unit/core/test-expr-script.c
@@ -682,6 +682,36 @@ data_arithmetic_operator_and_assign(void)
 }
 
 static void
+data_arithmetic_operator_xor_assign(void)
+{
+  ADD_DATUM("integer ^= integer",
+            gcut_list_string_new("fuga fuga", "hoge hoge", NULL),
+            "size <= 9 && (size ^= 3) && size == 10");
+  ADD_DATUM("integer ^= float",
+            gcut_list_string_new("fuga fuga", "hoge hoge", NULL),
+            "size <= 9 && ((size ^= 3.1) || 1) && size == 10");
+  ADD_DATUM("integer ^= integer-string",
+            gcut_list_string_new("fuga fuga", "hoge hoge", NULL),
+            "size <= 9 && ((size ^= \"3\") || 1) && size == 10");
+
+  ADD_DATUM("float ^= float",
+            gcut_list_string_new("fuga fuga", "hoge hoge", NULL),
+            "size_in_float <= 9.1 && "
+            "((size_in_float ^= 3.1) || 1) && "
+            "9.9 < size_in_float && size_in_float < 10.1");
+  ADD_DATUM("float ^= integer",
+            gcut_list_string_new("fuga fuga", "hoge hoge", NULL),
+            "size_in_float <= 9.1 && "
+            "((size_in_float ^= 3) || 1) && "
+            "9.9 < size_in_float && size_in_float < 10.1");
+  ADD_DATUM("float ^= float-string",
+            gcut_list_string_new("fuga fuga", "hoge hoge", NULL),
+            "size_in_float <= 9.1 && "
+            "((size_in_float ^= \"3.1\") || 1) && "
+            "9.9 < size_in_float && size_in_float < 10.1");
+}
+
+static void
 data_arithmetic_operator_or_assign(void)
 {
   ADD_DATUM("integer |= integer",
@@ -1070,6 +1100,7 @@ data_arithmetic_operator(void)
   data_arithmetic_operator_shift_r_assign();
   data_arithmetic_operator_shift_rr_assign();
   data_arithmetic_operator_and_assign();
+  data_arithmetic_operator_xor_assign();
   data_arithmetic_operator_or_assign();
   data_arithmetic_operator_bitwise_or();
   data_arithmetic_operator_bitwise_xor();
