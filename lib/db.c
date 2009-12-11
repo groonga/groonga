bceb7b05300d0c707ae621b36420200e854d5019
Kouhei Sutou <kou@clear-code.com>
2009-12-11 18:29:20 +0900

    '+=' isn't allowed without GRN_EXPR_ALLOW_UPDATE.

--- lib/db.c
+++ lib/db.c
@@ -11819,8 +11819,13 @@ parse_script(grn_ctx *ctx, efs_info *q)
         PARSE(GRN_EXPR_TOKEN_INCR);
         break;
       case '=' :
-        q->cur++;
-        PARSE(GRN_EXPR_TOKEN_PLUS_ASSIGN);
+        if (q->flags & GRN_EXPR_ALLOW_UPDATE) {
+          q->cur++;
+          PARSE(GRN_EXPR_TOKEN_PLUS_ASSIGN);
+        } else {
+          ERR(GRN_UPDATE_NOT_ALLOWED,
+              "'+=' is not allowed (%*s)", q->str_end - q->str, q->str);
+        }
         break;
       default :
         PARSE(GRN_EXPR_TOKEN_PLUS);
