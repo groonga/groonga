f7ee4d31841301c7bef115d4f21c606338ca6f4d
Kouhei Sutou <kou@clear-code.com>
2009-12-11 16:47:06 +0900

    support >>= in script syntax.

--- lib/db.c
+++ lib/db.c
@@ -7067,6 +7067,7 @@ grn_expr_append_obj(grn_ctx *ctx, grn_obj *expr, grn_obj *obj, grn_operator op, 
     case GRN_OP_PLUS_ASSIGN :
     case GRN_OP_MINUS_ASSIGN :
     case GRN_OP_SHIFTL_ASSIGN :
+    case GRN_OP_SHIFTR_ASSIGN :
       {
         if (obj) {
           type = obj->header.type;
@@ -8370,6 +8371,18 @@ grn_expr_exec(grn_ctx *ctx, grn_obj *expr, int nargs)
           ARITHMETIC_OPERATION_NO_CHECK,
           {
             ERR(GRN_INVALID_ARGUMENT, "variable <<= \"string\" isn't supported");
+            goto exit;
+          });
+        break;
+      case GRN_OP_SHIFTR_ASSIGN :
+        ARITHMETIC_OPERATION_AND_ASSIGN_DISPATCH(
+          INTEGER_ARITHMETIC_OPERATION_SHIFTR,
+          INTEGER_ARITHMETIC_OPERATION_SHIFTR,
+          FLOAT_ARITHMETIC_OPERATION_SHIFTR,
+          ARITHMETIC_OPERATION_NO_CHECK,
+          ARITHMETIC_OPERATION_NO_CHECK,
+          {
+            ERR(GRN_INVALID_ARGUMENT, "variable >>= \"string\" isn't supported");
             goto exit;
           });
         break;
