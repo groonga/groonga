56fd3090993cffb3feb2c3563040754265f94f83
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2010-02-23 17:46:48 +0900

    Added column range on output of grn_select().

--- lib/str.c
+++ lib/str.c
@@ -20,6 +20,7 @@
 #include <string.h>
 #include "ctx.h"
 #include "str.h"
+#include "pat.h"
 
 #ifndef _ISOC99_SOURCE
 #define _ISOC99_SOURCE
@@ -2401,10 +2402,30 @@ grn_text_otoj(grn_ctx *ctx, grn_obj *bulk, grn_obj *obj, grn_obj_format *format)
         if (format->flags & GRN_OBJ_FORMAT_WITH_COLUMN_NAMES) {
           GRN_TEXT_PUTS(ctx, bulk, "[");
           for (j = 0; j < ncolumns; j++) {
+            grn_id range_id;
             if (j) { GRN_TEXT_PUTC(ctx, bulk, ','); }
+            GRN_TEXT_PUTS(ctx, bulk, "[");
             GRN_BULK_REWIND(&buf);
             grn_column_name_(ctx, columns[j], &buf);
             grn_text_otoj(ctx, bulk, &buf, NULL);
+            GRN_TEXT_PUTC(ctx, bulk, ',');
+            /* column range */
+            range_id = grn_obj_get_range(ctx, columns[j]);
+            if (range_id == GRN_ID_NIL) {
+              GRN_TEXT_PUTS(ctx, bulk, "null");
+            } else {
+              int name_len;
+              grn_obj *range_obj;
+              char name_buf[GRN_PAT_MAX_KEY_SIZE];
+
+              range_obj = grn_ctx_at(ctx, range_id);
+              name_len = grn_obj_name(ctx, range_obj, name_buf,
+                                      GRN_PAT_MAX_KEY_SIZE);
+              GRN_BULK_REWIND(&buf);
+              GRN_TEXT_PUT(ctx, &buf, name_buf, name_len);
+              grn_text_otoj(ctx, bulk, &buf, NULL);
+            }
+            GRN_TEXT_PUTS(ctx, bulk, "]");
           }
           GRN_TEXT_PUTS(ctx, bulk, "],");
         }
@@ -2445,10 +2466,30 @@ grn_text_otoj(grn_ctx *ctx, grn_obj *bulk, grn_obj *obj, grn_obj_format *format)
         if (format->flags & GRN_OBJ_FORMAT_WITH_COLUMN_NAMES) {
           GRN_TEXT_PUTS(ctx, bulk, ",[");
           for (j = 0; j < ncolumns; j++) {
+            grn_id range_id;
             if (j) { GRN_TEXT_PUTC(ctx, bulk, ','); }
+            GRN_TEXT_PUTS(ctx, bulk, "[");
             GRN_BULK_REWIND(&buf);
             grn_column_name_(ctx, columns[j], &buf);
             grn_text_otoj(ctx, bulk, &buf, NULL);
+            GRN_TEXT_PUTC(ctx, bulk, ',');
+            /* column range */
+            range_id = grn_obj_get_range(ctx, columns[j]);
+            if (range_id == GRN_ID_NIL) {
+              GRN_TEXT_PUTS(ctx, bulk, "null");
+            } else {
+              int name_len;
+              grn_obj *range_obj;
+              char name_buf[GRN_PAT_MAX_KEY_SIZE];
+
+              range_obj = grn_ctx_at(ctx, range_id);
+              name_len = grn_obj_name(ctx, range_obj, name_buf,
+                                      GRN_PAT_MAX_KEY_SIZE);
+              GRN_BULK_REWIND(&buf);
+              GRN_TEXT_PUT(ctx, &buf, name_buf, name_len);
+              grn_text_otoj(ctx, bulk, &buf, NULL);
+            }
+            GRN_TEXT_PUTS(ctx, bulk, "]");
           }
           GRN_TEXT_PUTC(ctx, bulk, ']');
         }
@@ -2562,10 +2603,30 @@ grn_text_otoj(grn_ctx *ctx, grn_obj *bulk, grn_obj *obj, grn_obj_format *format)
       if (format->flags & GRN_OBJ_FORMAT_WITH_COLUMN_NAMES) {
         GRN_TEXT_PUTS(ctx, bulk, ",[");
         for (j = 0; j < ncolumns; j++) {
+          grn_id range_id;
           if (j) { GRN_TEXT_PUTC(ctx, bulk, ','); }
+          GRN_TEXT_PUTS(ctx, bulk, "[");
           GRN_BULK_REWIND(&buf);
           grn_column_name_(ctx, columns[j], &buf);
           grn_text_otoj(ctx, bulk, &buf, NULL);
+          GRN_TEXT_PUTC(ctx, bulk, ',');
+          /* column range */
+          range_id = grn_obj_get_range(ctx, columns[j]);
+          if (range_id == GRN_ID_NIL) {
+            GRN_TEXT_PUTS(ctx, bulk, "null");
+          } else {
+            int name_len;
+            grn_obj *range_obj;
+            char name_buf[GRN_PAT_MAX_KEY_SIZE];
+
+            range_obj = grn_ctx_at(ctx, range_id);
+            name_len = grn_obj_name(ctx, range_obj, name_buf,
+                                    GRN_PAT_MAX_KEY_SIZE);
+            GRN_BULK_REWIND(&buf);
+            GRN_TEXT_PUT(ctx, &buf, name_buf, name_len);
+            grn_text_otoj(ctx, bulk, &buf, NULL);
+          }
+          GRN_TEXT_PUTS(ctx, bulk, "]");
         }
         GRN_TEXT_PUTC(ctx, bulk, ']');
       }
