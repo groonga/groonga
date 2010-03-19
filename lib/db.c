58073c4c43dbdbdfe70a7cd19e87929a2c6e5745
Daijiro MORI <daijiro.mori@gmail.com>
2010-03-19 17:30:12 +0900

    put filter event to query log.

--- lib/db.c
+++ lib/db.c
@@ -10054,6 +10054,15 @@ grn_view_select(grn_ctx *ctx, grn_obj *table, grn_obj *expr,
   return res;
 }
 
+#define LAP(msg) {\
+  uint64_t et;\
+  grn_timeval tv;\
+  grn_timeval_now(ctx, &tv);\
+  et = (tv.tv_sec - ctx->impl->tv.tv_sec) * GRN_TIME_USEC_PER_SEC\
+    + (tv.tv_usec - ctx->impl->tv.tv_usec);\
+  GRN_LOG(ctx, GRN_LOG_NONE, "%08x|:%012zu %s", (intptr_t)ctx, et, msg);\
+}
+
 grn_obj *
 grn_table_select(grn_ctx *ctx, grn_obj *table, grn_obj *expr,
                  grn_obj *res, grn_operator op)
@@ -10262,6 +10271,7 @@ grn_table_select(grn_ctx *ctx, grn_obj *table, grn_obj *expr,
           }
         }
         SI_FREE(si);
+        LAP("filter");
       }
       GRN_OBJ_FIN(ctx, &res_stack);
       GRN_FREE(sis);
@@ -10941,15 +10951,6 @@ grn_table_sort_key_close(grn_ctx *ctx, grn_table_sort_key *keys, unsigned nkeys)
   }
   GRN_FREE(keys);
   return ctx->rc;
-}
-
-#define LAP(msg) {\
-  uint64_t et;\
-  grn_timeval tv;\
-  grn_timeval_now(ctx, &tv);\
-  et = (tv.tv_sec - ctx->impl->tv.tv_sec) * GRN_TIME_USEC_PER_SEC\
-    + (tv.tv_usec - ctx->impl->tv.tv_usec);\
-  GRN_LOG(ctx, GRN_LOG_NONE, "%08x:l%012zu %s", (intptr_t)ctx, et, msg);\
 }
 
 grn_rc
