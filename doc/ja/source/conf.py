90ad2cef3f5d1100dbfeb6f79813724e7d34e3fb
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2010-04-01 18:10:44 +0900

    Refactored reference.

--- doc/ja/source/conf.py
+++ doc/ja/source/conf.py
@@ -229,5 +229,9 @@ pdf_documents = [
    u'groonga %s document' % (release,),
    u'groonga project')
 ]
-
 pdf_stylesheets = ['sphinx', 'kerning', 'a4', 'ja']
+pdf_font_path = ['/usr/share/fonts']
+pdf_language = 'ja'
+pdf_fit_mode = "shrink"
+pdf_inline_footnotes = True
+pdf_break_level = 2
