511c363b7fb6514f5f52f4098df1103cd8bb1c5f
Kouhei Sutou <kou@clear-code.com>
2010-04-09 10:54:43 +0900

    format.

--- test/unit/lib/grn-test-utils.h
+++ test/unit/lib/grn-test-utils.h
@@ -1,6 +1,6 @@
 /* -*- c-basic-offset: 2; coding: utf-8 -*- */
 /*
-  Copyright (C) 2008-2009  Kouhei Sutou <kou@cozmixng.org>
+  Copyright (C) 2008-2010  Kouhei Sutou <kou@clear-code.com>
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
@@ -41,44 +41,50 @@
 
 typedef void (*grn_test_set_parameters_func) (void);
 
-const gchar *grn_rc_to_string(grn_rc rc);
-const gchar *grn_test_get_base_dir(void);
+const gchar *grn_rc_to_string              (grn_rc rc);
+const gchar *grn_test_get_base_dir         (void);
 
-grn_logger_info *grn_collect_logger_new(void);
-void grn_collect_logger_clear_messages(grn_logger_info *logger);
-const GList *grn_collect_logger_get_messages(grn_logger_info *logger);
-gchar *grn_collect_logger_to_string(grn_logger_info *logger);
-void grn_collect_logger_print_messages(grn_logger_info *logger);
-void grn_collect_logger_free(grn_logger_info *logger);
+grn_logger_info *
+             grn_collect_logger_new        (void);
+void         grn_collect_logger_clear_messages
+                                           (grn_logger_info *logger);
+const GList *grn_collect_logger_get_messages
+                                           (grn_logger_info *logger);
+gchar       *grn_collect_logger_to_string  (grn_logger_info *logger);
+void         grn_collect_logger_print_messages
+                                           (grn_logger_info *logger);
+void         grn_collect_logger_free       (grn_logger_info  *logger);
 
-grn_logger_info *setup_grn_logger(void);
-void teardown_grn_logger(grn_logger_info *logger);
+grn_logger_info *
+             setup_grn_logger              (void);
+void         teardown_grn_logger           (grn_logger_info  *logger);
 
-GString *grn_long_path_new(const gchar *base_path, gssize max_size);
+GString     *grn_long_path_new             (const gchar      *base_path,
+                                            gssize            max_size);
 
-GList      *grn_test_pat_cursor_get_keys  (grn_ctx          *context,
-                                           grn_table_cursor *cursor);
-GList      *grn_test_pat_get_keys         (grn_ctx          *context,
-                                           grn_obj          *patricia_trie);
-GList      *grn_test_pat_cursor_get_pairs (grn_ctx          *context,
-                                           grn_table_cursor *cursor);
-GHashTable *grn_test_pat_get_pairs        (grn_ctx          *context,
-                                           grn_obj          *patricia_trie);
-const gchar *grn_test_type_inspect        (grn_ctx          *context,
-                                           unsigned char     type);
-void        grn_test_object_inspect       (GString          *output,
-                                           grn_ctx          *context,
-                                           grn_obj          *object);
+GList       *grn_test_pat_cursor_get_keys  (grn_ctx          *context,
+                                            grn_table_cursor *cursor);
+GList       *grn_test_pat_get_keys         (grn_ctx          *context,
+                                            grn_obj          *patricia_trie);
+GList       *grn_test_pat_cursor_get_pairs (grn_ctx          *context,
+                                            grn_table_cursor *cursor);
+GHashTable  *grn_test_pat_get_pairs        (grn_ctx          *context,
+                                            grn_obj          *patricia_trie);
+const gchar *grn_test_type_inspect         (grn_ctx          *context,
+                                            unsigned char     type);
+void         grn_test_object_inspect       (GString          *output,
+                                            grn_ctx          *context,
+                                            grn_obj          *object);
 
-const gchar *grn_test_send_command        (grn_ctx          *context,
-                                           const gchar      *command);
+const gchar *grn_test_send_command         (grn_ctx          *context,
+                                            const gchar      *command);
 
-const GList *grn_test_table_collect_string(grn_ctx          *context,
-                                           grn_obj          *table,
-                                           const gchar      *text_column_name);
-const GList *grn_test_view_collect_string (grn_ctx          *context,
-                                           grn_obj          *view,
-                                           const gchar      *text_column_name);
+const GList *grn_test_table_collect_string (grn_ctx          *context,
+                                            grn_obj          *table,
+                                            const gchar      *text_column_name);
+const GList *grn_test_view_collect_string  (grn_ctx          *context,
+                                            grn_obj          *view,
+                                            const gchar      *text_column_name);
 
 
 #endif
