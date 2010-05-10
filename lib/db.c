5ee6852b9b119f9475505090c4e544074209e18f
Daijiro MORI <daijiro.mori@gmail.com>
2010-05-10 20:59:39 +0900

    Retry while other thread seems to be opening the target object.

--- lib/db.c
+++ lib/db.c
@@ -5449,7 +5449,13 @@ grn_ctx_at(grn_ctx *ctx, grn_id id)
           }
           GRN_FUTEX_WAKE(vp->ptr);
         } else {
-          GRN_FUTEX_WAIT(vp->ptr);
+          for (ntrial = 0; !vp->ptr; ntrial++) {
+            if (ntrial >= 1000) {
+              GRN_LOG(ctx, GRN_LOG_NOTICE, "max trial in ctx_at(%p,%d)!", vp->ptr, vp->lock);
+              break;
+            }
+            GRN_FUTEX_WAIT(vp->ptr);
+          }
         }
       }
       res = vp->ptr;
