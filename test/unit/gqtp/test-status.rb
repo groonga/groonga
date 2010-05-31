3f957073465c29145dbb5544aa99f6c3cd25b46c
Nobuyoshi Nakada <nobu@ruby-lang.org>
2010-05-31 18:02:32 +0900

    add test for #311.

---
+++ test/unit/gqtp/test-status.rb
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2010  Nobuyoshi Nakada <nakada@clear-code.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License version 2.1 as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+class LoadTest < Test::Unit::TestCase
+  include GroongaLocalGQTPTestUtils
+
+  def setup
+    setup_local_database
+  end
+
+  def teardown
+    teardown_local_database
+  end
+
+  def test_status_exit_successfully
+    output = run_groonga(@database_path, "status")
+    assert($?.success?)
+  end
+end
