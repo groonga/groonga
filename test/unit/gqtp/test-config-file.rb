deca5205d6d8e08af8a3d815ca0953630fc31de8
Kouhei Sutou <kou@clear-code.com>
2010-06-11 15:36:41 +0900

    fix test case name.

--- test/unit/gqtp/test-config-file.rb
+++ test/unit/gqtp/test-config-file.rb
@@ -15,7 +15,7 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-class OptionPidFileTest < Test::Unit::TestCase
+class ConfigFileTest < Test::Unit::TestCase
   include GroongaTestUtils
 
   def setup
