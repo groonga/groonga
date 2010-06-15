0f1d0a2137586f93d18c4c6b5cd635b5392c603b
Kouhei Sutou <kou@clear-code.com>
2010-06-15 16:25:35 +0900

    fix indent.

--- lib/proc.c
+++ lib/proc.c
@@ -673,8 +673,8 @@ proc_column_list(grn_ctx *ctx, int nargs, grn_obj **args, grn_user_data *user_da
     grn_obj_unlink(ctx, table);
   } else {
     ERR(GRN_INVALID_ARGUMENT, "table '%.*s' is not exist.",
-      GRN_TEXT_LEN(VAR(0)),
-      GRN_TEXT_VALUE(VAR(0)));
+        GRN_TEXT_LEN(VAR(0)),
+        GRN_TEXT_VALUE(VAR(0)));
   }
   return NULL;
 }
