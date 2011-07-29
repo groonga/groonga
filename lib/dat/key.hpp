f05a8ea1280f46e4f1ab5f1a05c62ebf79cb691d
Kouhei Sutou <kou@clear-code.com>
2011-07-29 11:16:09 +0900

    [dat] add license headers.

--- lib/dat/key.hpp
+++ lib/dat/key.hpp
@@ -1,3 +1,20 @@
+/* -*- c-basic-offset: 2 -*- */
+/* Copyright(C) 2011 Brazil
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License version 2.1 as published by the Free Software Foundation.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
 #ifndef GRN_DAT_KEY_HPP_
 #define GRN_DAT_KEY_HPP_
 
