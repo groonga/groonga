55dec09a3b0bd6a3139059f812fdf48b5ce63db8
Kouhei Sutou <kou@clear-code.com>
2011-09-11 21:50:28 +0900

    [test][query-expansion] add a test for COLUMN:= case.

--- test/unit/core/test-command-select-query-expansion.c
+++ test/unit/core/test-command-select-query-expansion.c
@@ -28,6 +28,7 @@ void test_expand_word_with_space(void);
 void test_not_expand_recursively(void);
 void test_expand_OR_quoted(void);
 void test_expand_column_value(void);
+void test_expand_equal(void);
 void test_not_expand_OR(void);
 void test_not_expand_OR_at_the_end(void);
 void test_not_expand_OR_with_leading_space(void);
@@ -114,6 +115,7 @@ setup_data(void)
                       "[\"*\", \"前方一致\"],\n"
                       "[\"(\", \"かっこ\"],\n"
                       "[\")\", \"こっか\"]\n"
+                      "[\"=\", \"\\\"Start rroonga!\\\"\"]\n"
                       "]");
 }
 
@@ -221,6 +223,20 @@ test_expand_column_value(void)
         "[9,1316358000.0,\"Learning Ruby and groonga...\"]]]",
     send_command("select Diaries --sortby _id "
                  "--match_columns content --query 'content:@rroonga' "
+                 "--query_expand Synonyms.words"));
+}
+
+void
+test_expand_equal(void)
+{
+  cut_assert_equal_string(
+      "[[[1],"
+       "[[\"_id\",\"UInt32\"],"
+        "[\"_key\",\"Time\"],"
+        "[\"content\",\"Text\"]],"
+        "[3,1315839600.0,\"Start rroonga!\"]]]",
+    send_command("select Diaries --sortby _id "
+                 "--match_columns content --query 'content:=' "
                  "--query_expand Synonyms.words"));
 }
 
