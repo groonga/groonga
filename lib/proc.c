f6d511705323393b969ff5f6551385ab6be4e256
Ryo Onodera <onodera@clear-code.com>
2011-12-26 13:42:26 +0900

    rename: value => res_value

--- lib/proc.c
+++ lib/proc.c
@@ -1641,11 +1641,11 @@ proc_delete(grn_ctx *ctx, int nargs, grn_obj **args, grn_user_data *user_data)
                      GRN_EXPR_SYNTAX_SCRIPT);
       if (!ctx->rc) {
         grn_obj *res;
-        void *res_key, *value;
+        void *res_key, *res_value;
         uint32_t res_key_size;
 
         res = grn_table_select(ctx, table, cond, NULL, GRN_OP_OR);
-        GRN_TABLE_EACH(ctx, res, 0, 0, res_id, &res_key, &res_key_size, &value, {
+        GRN_TABLE_EACH(ctx, res, 0, 0, res_id, &res_key, &res_key_size, &res_value, {
           grn_id id = *(grn_id *)res_key;
           grn_table_delete_by_id(ctx, table, id);
         });
