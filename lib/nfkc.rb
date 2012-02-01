70d6610414ad91e332a62732c33ed80b77f67733
Kouhei Sutou <kou@clear-code.com>
2012-02-01 16:35:57 +0900

    update FSF address.
    Suggested by Daiki Ueno. Thanks!!!

--- lib/nfkc.rb
+++ lib/nfkc.rb
@@ -14,7 +14,7 @@
 #
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 $KCODE = 'u'
 
@@ -342,7 +342,7 @@ template = <<END
 
   You should have received a copy of the GNU Lesser General Public
   License along with this library; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 don't edit this file by hand. it generated automatically by nfkc.rb
 */
