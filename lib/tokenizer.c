f6c6eaa4d1cb8ad84897427b6399639fa6f7f1af
Kouhei Sutou <kou@clear-code.com>
2012-02-10 19:13:32 +0900

    [tokenizer] removed needless flags.

--- lib/tokenizer.c
+++ lib/tokenizer.c
@@ -184,13 +184,13 @@ grn_tokenizer_query *grn_tokenizer_query_create(grn_ctx *ctx,
 
     {
       grn_obj * const table = args[0];
-      grn_obj_flags table_flags;
       grn_encoding table_encoding;
-      grn_table_get_info(ctx, table, &table_flags, &table_encoding, NULL);
+      int flags = 0;
+      grn_table_get_info(ctx, table, NULL, &table_encoding, NULL);
       {
         grn_str * const str = grn_str_open_(ctx, GRN_TEXT_VALUE(query_str),
                                             GRN_TEXT_LEN(query_str),
-                                            table_flags & GRN_OBJ_KEY_NORMALIZE,
+                                            flags | GRN_OBJ_KEY_NORMALIZE,
                                             table_encoding);
         if (str == NULL) {
           GRN_TOKENIZER_FREE(ctx, query);
