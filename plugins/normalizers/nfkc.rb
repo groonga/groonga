e437bc502bdf95e7cba178abe9b6d9221b37ff8d
Kouhei Sutou <kou@clear-code.com>
2012-02-13 18:01:45 +0900

    [nfkc] removed needless NO_NFKC.

--- plugins/normalizers/nfkc.rb
+++ plugins/normalizers/nfkc.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
 #
-# Copyright(C) 2010 Brazil
+# Copyright(C) 2010-2012 Brazil
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -329,7 +329,7 @@ end
 
 template = <<END
 /* -*- c-basic-offset: 2 -*- */
-/* Copyright(C) 2010 Brazil
+/* Copyright(C) 2010-2012 Brazil
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
@@ -349,8 +349,6 @@ don't edit this file by hand. it generated automatically by nfkc.rb
 
 #include "nfkc.h"
 
-#ifndef NO_NFKC
-
 unsigned char
 grn_nfkc_ctype(const unsigned char *str)
 {
@@ -368,8 +366,6 @@ grn_nfkc_map2(const unsigned char *prefix, const unsigned char *suffix)
 {
 %  return 0;
 }
-
-#endif /* NO_NFKC */
 
 END
 
