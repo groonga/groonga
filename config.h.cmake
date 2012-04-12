a083ef2dfd5699110370537f5692513994f3c038
Kouhei Sutou <kou@clear-code.com>
2012-04-12 15:09:43 +0900

    cmake: add CMake related files but it's not usable yet

---
+++ config.h.cmake
@@ -0,0 +1,111 @@
+/* config.h.cmake.  Generated from CMakeLists.txt by cmake.  */
+
+#define CONFIGURE_OPTIONS "${CONFIGURE_OPTIONS}"
+
+#cmakedefine ENABLE_MEMORY_DEBUG 1
+
+#define GRN_CONFIG_PATH   "${GRN_CONFIG_PATH}"
+
+#define GRN_DEFAULT_DB_KEY "${GRN_DEFAULT_DB_KEY}"
+#define GRN_DEFAULT_ENCODING "${GRN_DEFAULT_ENCODING}"
+#define GRN_DEFAULT_MATCH_ESCALATION_THRESHOLD \
+  ${GRN_DEFAULT_MATCH_ESCALATION_THRESHOLD}
+
+#define GRN_PLUGIN_SUFFIX "${GRN_PLUGIN_SUFFIX}"
+
+#cmakedefine GROONGA_USE_MAP_HUGETLB
+
+#cmakedefine HAVE_BACKTRACE
+#cmakedefine HAVE_CLOCK_GETTIME
+#cmakedefine HAVE_CLOCK
+#cmakedefine HAVE_DLFCN_H
+#cmakedefine HAVE_ERRNO_H
+#cmakedefine HAVE_EXECINFO_H
+#cmakedefine HAVE_FPCLASSIFY
+#cmakedefine HAVE_GMTIME_R
+#cmakedefine HAVE_INTTYPES_H
+#cmakedefine HAVE_LIBEDIT
+#cmakedefine HAVE_LIBEVENT
+#cmakedefine HAVE_LIBM
+#cmakedefine HAVE_LIBRT
+#cmakedefine HAVE_LINUX_FUTEX_H
+#cmakedefine HAVE_LOCALTIME_R
+#cmakedefine HAVE_MECAB_DICTIONARY_INFO_T
+#cmakedefine HAVE_MEMORY_H
+#cmakedefine HAVE_MESSAGE_PACK
+#cmakedefine HAVE_MKOSTEMP
+#cmakedefine HAVE_NETDB_H
+#cmakedefine HAVE_NETINET_IN_H
+#cmakedefine HAVE_NETINET_TCP_H
+#cmakedefine HAVE_OPEN
+#cmakedefine HAVE_PTHREAD_H
+#cmakedefine HAVE_READ
+#cmakedefine HAVE_SIGNAL_H
+#cmakedefine HAVE_STDINT_H
+#cmakedefine HAVE_STDLIB_H
+#cmakedefine HAVE_STRINGS_H
+#cmakedefine HAVE_STRING_H
+#cmakedefine HAVE_SYS_MMAN_H
+#cmakedefine HAVE_SYS_PARAM_H
+#cmakedefine HAVE_SYS_RESOURCE_H
+#cmakedefine HAVE_SYS_SELECT_H
+#cmakedefine HAVE_SYS_SOCKET_H
+#cmakedefine HAVE_SYS_STAT_H
+#cmakedefine HAVE_SYS_SYSCALL_H
+#cmakedefine HAVE_SYS_SYSCTL_H
+#cmakedefine HAVE_SYS_TIMEB_H
+#cmakedefine HAVE_SYS_TIME_H
+#cmakedefine HAVE_SYS_TYPES_H
+#cmakedefine HAVE_SYS_WAIT_H
+#cmakedefine HAVE_TIME_H
+#cmakedefine HAVE_UCONTEXT_H
+#cmakedefine HAVE_UNISTD_H
+#cmakedefine HAVE_WRITE
+#cmakedefine HAVE_ZEROMQ
+
+#define HOST_CPU "${CMAKE_HOST_SYSTEM_PROCESSOR}"
+#define HOST_OS "${CMAKE_HOST_SYSTEM_NAME}"
+
+#cmakedefine NO_LZO
+#cmakedefine NO_NFKC
+#cmakedefine NO_ZLIB
+
+#define PACKAGE "${PROJECT_NAME}"
+#cmakedefine PACKAGE_NAME
+#cmakedefine PACKAGE_STRING
+#cmakedefine PACKAGE_TARNAME
+#cmakedefine PACKAGE_URL
+#cmakedefine PACKAGE_VERSION
+#define VERSION "${VERSION}"
+
+#cmakedefine STDC_HEADERS
+#cmakedefine USE_AIO
+#cmakedefine USE_DYNAMIC_MALLOC_CHANGE
+#cmakedefine USE_EPOLL
+#cmakedefine USE_EXACT_ALLOC_COUNT
+#cmakedefine USE_FAIL_MALLOC
+#cmakedefine USE_FUTEX
+#cmakedefine USE_KQUEUE
+#cmakedefine USE_MSG_MORE
+#cmakedefine USE_MSG_NOSIGNAL
+#cmakedefine USE_POLL
+#cmakedefine USE_QUERY_ABORT
+#cmakedefine USE_SELECT
+
+#cmakedefine WITH_BENCHMARK
+#cmakedefine WITH_CUTTER
+#cmakedefine WITH_KYTEA
+#cmakedefine WITH_LIBMEMCACHED
+#cmakedefine WITH_MECAB
+
+#cmakedefine WORDS_BIGENDIAN
+
+#cmakedefine _FILE_OFFSET_BITS
+#cmakedefine _GNU_SOURCE
+#cmakedefine _ISOC99_SOURCE
+#cmakedefine _LARGE_FILES
+#cmakedefine _NETBSD_SOURCE
+#cmakedefine _XOPEN_SOURCE
+#cmakedefine _XPG4_2
+#cmakedefine __EXTENSIONS__
+
