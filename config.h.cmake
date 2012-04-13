cd3ff65ff67c8501a123478b6078f2e04e6eba47
Kouhei Sutou <kou@clear-code.com>
2012-04-13 17:59:57 +0900

    cmake: group definitions

--- config.h.cmake
+++ config.h.cmake
@@ -1,49 +1,88 @@
 /* config.h.cmake.  Generated from CMakeLists.txt by cmake.  */
 
-#define CONFIGURE_OPTIONS "${CONFIGURE_OPTIONS}"
+/* general constants */
+#define CONFIGURE_OPTIONS    "${CONFIGURE_OPTIONS}"
 
-#cmakedefine ENABLE_MEMORY_DEBUG 1
+#define HOST_CPU             "${CMAKE_HOST_SYSTEM_PROCESSOR}"
+#define HOST_OS              "${CMAKE_HOST_SYSTEM_NAME}"
 
-#define GRN_CONFIG_PATH   "${GRN_CONFIG_PATH}"
-#define GRN_LOG_PATH      "${GRN_LOG_PATH}"
-#define GRN_VERSION       "${GRN_VERSION}"
+#define VERSION              "${GRN_VERSION}"
+#define PACKAGE              "${PROJECT_NAME}"
+#define PACKAGE_NAME         "${PROJECT_NAME}"
+#define PACKAGE_STRING       "${PROJECT_NAME} ${GRN_VERSION}"
+#define PACKAGE_TARNAME      "${PACKAGE_TARNAME}"
+#define PACKAGE_URL          "${PACKAGE_URL}"
+#define PACKAGE_VERSION      "${GRN_VERSION}"
 
-#define GRN_DEFAULT_DB_KEY "${GRN_DEFAULT_DB_KEY}"
+/* groonga related constants */
+#define GRN_CONFIG_PATH      "${GRN_CONFIG_PATH}"
+#define GRN_LOG_PATH         "${GRN_LOG_PATH}"
+#define GRN_VERSION          "${GRN_VERSION}"
+
+#define GRN_DEFAULT_DB_KEY   "${GRN_DEFAULT_DB_KEY}"
 #define GRN_DEFAULT_ENCODING "${GRN_DEFAULT_ENCODING}"
-#define GRN_DEFAULT_MATCH_ESCALATION_THRESHOLD ${GRN_DEFAULT_MATCH_ESCALATION_THRESHOLD}
-#define GRN_DEFAULT_RELATIVE_DOCUMENT_ROOT "${GRN_DEFAULT_RELATIVE_DOCUMENT_ROOT}"
-#define GRN_DEFAULT_DOCUMENT_ROOT "${GRN_DEFAULT_DOCUMENT_ROOT}"
+#define GRN_DEFAULT_MATCH_ESCALATION_THRESHOLD \
+  ${GRN_DEFAULT_MATCH_ESCALATION_THRESHOLD}
+#define GRN_DEFAULT_RELATIVE_DOCUMENT_ROOT \
+  "${GRN_DEFAULT_RELATIVE_DOCUMENT_ROOT}"
+#define GRN_DEFAULT_DOCUMENT_ROOT \
+  "${GRN_DEFAULT_DOCUMENT_ROOT}"
 
-#define GRN_PLUGINS_DIR "${GRN_PLUGINS_DIR}"
-#define GRN_PLUGIN_SUFFIX "${GRN_PLUGIN_SUFFIX}"
+#define GRN_PLUGINS_DIR      "${GRN_PLUGINS_DIR}"
+#define GRN_PLUGIN_SUFFIX    "${GRN_PLUGIN_SUFFIX}"
 
-#cmakedefine GROONGA_USE_MAP_HUGETLB
+/* build switches */
+#cmakedefine USE_MEMORY_DEBUG
+#cmakedefine USE_MAP_HUGETLB
+#cmakedefine USE_AIO
+#cmakedefine USE_DYNAMIC_MALLOC_CHANGE
+#cmakedefine USE_EPOLL
+#cmakedefine USE_EXACT_ALLOC_COUNT
+#cmakedefine USE_FAIL_MALLOC
+#cmakedefine USE_FUTEX
+#cmakedefine USE_KQUEUE
+#cmakedefine USE_MSG_MORE
+#cmakedefine USE_MSG_NOSIGNAL
+#cmakedefine USE_POLL
+#cmakedefine USE_QUERY_ABORT
+#cmakedefine USE_SELECT
 
-#cmakedefine HAVE_BACKTRACE
-#cmakedefine HAVE_CLOCK_GETTIME
-#cmakedefine HAVE_CLOCK
+/* compiler specific build options */
+#cmakedefine _FILE_OFFSET_BITS
+#cmakedefine _GNU_SOURCE
+#cmakedefine _ISOC99_SOURCE
+#cmakedefine _LARGE_FILES
+#cmakedefine _NETBSD_SOURCE
+#cmakedefine _XOPEN_SOURCE
+#cmakedefine _XPG4_2
+#cmakedefine __EXTENSIONS__
+
+/* build environment */
+#cmakedefine WORDS_BIGENDIAN
+
+/* packages */
+#cmakedefine WITH_BENCHMARK
+#cmakedefine WITH_CUTTER
+#cmakedefine WITH_KYTEA
+#cmakedefine WITH_LIBMEMCACHED
+#cmakedefine WITH_LZO
+#cmakedefine WITH_MECAB
+#cmakedefine WITH_MESSAGE_PACK
+#cmakedefine WITH_NFKC
+#cmakedefine WITH_ZEROMQ
+#cmakedefine WITH_ZLIB
+
+/* headers */
 #cmakedefine HAVE_DLFCN_H
 #cmakedefine HAVE_ERRNO_H
 #cmakedefine HAVE_EXECINFO_H
-#cmakedefine HAVE_FPCLASSIFY
-#cmakedefine HAVE_GMTIME_R
 #cmakedefine HAVE_INTTYPES_H
-#cmakedefine HAVE_LIBEDIT
-#cmakedefine HAVE_LIBEVENT
-#cmakedefine HAVE_LIBM
-#cmakedefine HAVE_LIBRT
 #cmakedefine HAVE_LINUX_FUTEX_H
-#cmakedefine HAVE_LOCALTIME_R
-#cmakedefine HAVE_MECAB_DICTIONARY_INFO_T
 #cmakedefine HAVE_MEMORY_H
-#cmakedefine HAVE_MESSAGE_PACK
-#cmakedefine HAVE_MKOSTEMP
 #cmakedefine HAVE_NETDB_H
 #cmakedefine HAVE_NETINET_IN_H
 #cmakedefine HAVE_NETINET_TCP_H
-#cmakedefine HAVE_OPEN
 #cmakedefine HAVE_PTHREAD_H
-#cmakedefine HAVE_READ
 #cmakedefine HAVE_SIGNAL_H
 #cmakedefine HAVE_STDINT_H
 #cmakedefine HAVE_STDLIB_H
@@ -64,52 +103,24 @@
 #cmakedefine HAVE_TIME_H
 #cmakedefine HAVE_UCONTEXT_H
 #cmakedefine HAVE_UNISTD_H
+
+/* libraries */
+#cmakedefine HAVE_LIBEDIT
+#cmakedefine HAVE_LIBEVENT
+#cmakedefine HAVE_LIBM
+#cmakedefine HAVE_LIBRT
+
+/* structs */
+#cmakedefine HAVE_MECAB_DICTIONARY_INFO_T
+
+/* functions */
+#cmakedefine HAVE_BACKTRACE
+#cmakedefine HAVE_CLOCK
+#cmakedefine HAVE_CLOCK_GETTIME
+#cmakedefine HAVE_FPCLASSIFY
+#cmakedefine HAVE_GMTIME_R
+#cmakedefine HAVE_LOCALTIME_R
+#cmakedefine HAVE_MKOSTEMP
+#cmakedefine HAVE_OPEN
+#cmakedefine HAVE_READ
 #cmakedefine HAVE_WRITE
-#cmakedefine HAVE_ZEROMQ
-
-#define HOST_CPU "${CMAKE_HOST_SYSTEM_PROCESSOR}"
-#define HOST_OS "${CMAKE_HOST_SYSTEM_NAME}"
-
-#cmakedefine WITH_LZO
-#cmakedefine WITH_NFKC
-#cmakedefine WITH_ZLIB
-
-#define PACKAGE "${PROJECT_NAME}"
-#define PACKAGE_NAME "${PROJECT_NAME}"
-#cmakedefine PACKAGE_STRING
-#cmakedefine PACKAGE_TARNAME
-#cmakedefine PACKAGE_URL
-#define PACKAGE_VERSION "${GRN_VERSION}"
-#define VERSION "${GRN_VERSION}"
-
-#cmakedefine STDC_HEADERS
-#cmakedefine USE_AIO
-#cmakedefine USE_DYNAMIC_MALLOC_CHANGE
-#cmakedefine USE_EPOLL
-#cmakedefine USE_EXACT_ALLOC_COUNT
-#cmakedefine USE_FAIL_MALLOC
-#cmakedefine USE_FUTEX
-#cmakedefine USE_KQUEUE
-#cmakedefine USE_MSG_MORE
-#cmakedefine USE_MSG_NOSIGNAL
-#cmakedefine USE_POLL
-#cmakedefine USE_QUERY_ABORT
-#cmakedefine USE_SELECT
-
-#cmakedefine WITH_BENCHMARK
-#cmakedefine WITH_CUTTER
-#cmakedefine WITH_KYTEA
-#cmakedefine WITH_LIBMEMCACHED
-#cmakedefine WITH_MECAB
-
-#cmakedefine WORDS_BIGENDIAN
-
-#cmakedefine _FILE_OFFSET_BITS
-#cmakedefine _GNU_SOURCE
-#cmakedefine _ISOC99_SOURCE
-#cmakedefine _LARGE_FILES
-#cmakedefine _NETBSD_SOURCE
-#cmakedefine _XOPEN_SOURCE
-#cmakedefine _XPG4_2
-#cmakedefine __EXTENSIONS__
-
