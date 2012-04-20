bf3191f822585c09b188b0c30ca4fc3c7d115051
khayashi <hayashi@clear-code.com>
2012-04-20 18:45:08 +0900

    test: migrate a test from Cutter to grntest style
    The test checks that the approximate_type parameter(rectangle) of geo_in_circle() uses index.

--- test/unit/core/test-command-select-geo.c
+++ test/unit/core/test-command-select-geo.c
@@ -21,8 +21,6 @@
 
 #include "../lib/grn-assertions.h"
 
-void data_rectangle(void);
-void test_rectangle(gconstpointer data);
 void data_sphere(void);
 void test_sphere(gconstpointer data);
 void data_ellipsoid(void);
@@ -93,59 +91,6 @@ cut_teardown(void)
 }
 
 
-void
-data_rectangle(void)
-{
-#define ADD_DATA(label, type, use_index)                        \
-  gcut_add_datum(label,                                         \
-                 "approximate-type", G_TYPE_STRING, type,       \
-                 "use-index", G_TYPE_BOOLEAN, use_index,        \
-                 NULL)
-
-  ADD_DATA("full - use index", "rectangle", TRUE);
-  ADD_DATA("full - no index", "rectangle", FALSE);
-  ADD_DATA("abbreviation - use index", "rect", TRUE);
-  ADD_DATA("abbreviation - no index", "rect", FALSE);
-
-#undef ADD_DATA
-}
-
-void
-test_rectangle(gconstpointer data)
-{
-  gdouble yurakucho_latitude = 35.67487;
-  gdouble yurakucho_longitude = 139.76352;
-  gint distance = 3 * 1000;
-  const gchar *approximate_type;
-
-  approximate_type = gcut_data_get_string(data, "approximate-type");
-  cut_assert_equal_string(
-    "[[[7],"
-    "[[\"name\",\"ShortText\"],[\"_score\",\"Int32\"],"
-    "[\"location\",\"WGS84GeoPoint\"]],"
-    "[\"柳屋 たい焼き\",-2147483648,\"128467228x503222332\"],"
-    "[\"銀座 かずや\",280743810,\"128424629x503139222\"],"
-    "[\"たい焼き鉄次 大丸東京店\",810303031,\"128451283x503166852\"],"
-    "[\"たいやき神田達磨 八重洲店\",970517026,\"128453260x503174156\"],"
-    "[\"にしみや 甘味処\",1056698886,\"128418570x503188661\"],"
-    "[\"築地 さのきや\",1186376492,\"128397312x503174596\"],"
-    "[\"しげ田\",1530425643,\"128421454x503208983\"]"
-    "]]",
-    send_command(
-      cut_take_printf(
-        "select Shops "
-        "--sortby '+_score, +name' "
-        "--output_columns 'name, _score, location' "
-        "--filter 'geo_in_circle(location, \"%s\", %d, \"%s\")%s' "
-        "--scorer "
-          "'_score = geo_distance(location, \"%s\", \"%s\") * 1000 * 1000'",
-        grn_test_location_string(yurakucho_latitude, yurakucho_longitude),
-        distance,
-        approximate_type,
-        gcut_data_get_boolean(data, "use-index") ? "" : " > 0",
-        grn_test_location_string(yurakucho_latitude, yurakucho_longitude),
-        approximate_type)));
-}
 
 void
 data_sphere(void)
