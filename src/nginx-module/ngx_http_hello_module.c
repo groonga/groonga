09d0c6224873d1aec0aa9acc841fdf94def139b0
Ryo Onodera <onodera@clear-code.com>
2012-06-01 17:34:30 +0900

    Fix style: unwrap a function prototype

--- src/nginx-module/ngx_http_hello_module.c
+++ src/nginx-module/ngx_http_hello_module.c
@@ -20,8 +20,7 @@
 #include <ngx_core.h>
 #include <ngx_http.h>
 
-static char *ngx_http_hello(ngx_conf_t *cf, ngx_command_t *cmd,
-    void *conf);
+static char *ngx_http_hello(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
 
 static ngx_command_t ngx_http_hello_commands[] = {
     { ngx_string("hello"),
