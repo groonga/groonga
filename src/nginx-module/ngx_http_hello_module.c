e7954d83aba926dac230e22189ae0ba3bbfacb87
Ryo Onodera <onodera@clear-code.com>
2012-06-01 17:25:13 +0900

    Fix style: remove unneeded empty lines

--- src/nginx-module/ngx_http_hello_module.c
+++ src/nginx-module/ngx_http_hello_module.c
@@ -20,7 +20,6 @@
 #include <ngx_core.h>
 #include <ngx_http.h>
 
-
 static char *ngx_http_hello(ngx_conf_t *cf, ngx_command_t *cmd,
     void *conf);
 
@@ -35,9 +34,7 @@ static ngx_command_t ngx_http_hello_commands[] = {
     ngx_null_command
 };
 
-
 static u_char ngx_hello_string[] = "Hello, world!";
-
 
 static ngx_http_module_t ngx_http_hello_module_ctx = {
     NULL,                          /* preconfiguration */
@@ -53,7 +50,6 @@ static ngx_http_module_t ngx_http_hello_module_ctx = {
     NULL                           /* merge location configuration */
 };
 
-
 ngx_module_t ngx_http_hello_module = {
     NGX_MODULE_V1,
     &ngx_http_hello_module_ctx,    /* module context */
@@ -68,7 +64,6 @@ ngx_module_t ngx_http_hello_module = {
     NULL,                          /* exit master */
     NGX_MODULE_V1_PADDING
 };
-
 
 static ngx_int_t
 ngx_http_hello_handler(ngx_http_request_t *r)
@@ -133,7 +128,6 @@ ngx_http_hello_handler(ngx_http_request_t *r)
     return ngx_http_output_filter(r, &out);
 }
 
-
 static char *
 ngx_http_hello(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 {
@@ -141,6 +135,6 @@ ngx_http_hello(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 
     clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
     clcf->handler = ngx_http_hello_handler; /* handler to process the 'hello' directive */
-    
+
     return NGX_CONF_OK;
 }
