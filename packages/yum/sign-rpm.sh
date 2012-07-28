ca396157e6de47dab118c7ac0d745e115247662f
Kouhei Sutou <kou@clear-code.com>
2012-07-29 00:00:55 +0900

    yum: force to use SHA1 for digest

--- packages/yum/sign-rpm.sh
+++ packages/yum/sign-rpm.sh
@@ -30,6 +30,7 @@ echo "NOTE: YOU JUST ENTER! YOU DON'T NEED TO INPUT PASSWORD!"
 echo "      IT'S JUST FOR rpm COMMAND RESTRICTION!"
 run rpm \
     -D "_gpg_name ${GPG_UID}" \
+    -D "_gpg_digest_algo sha1" \
     -D "__gpg /usr/bin/gpg2" \
     -D "__gpg_check_password_cmd /bin/true true" \
     -D "__gpg_sign_cmd %{__gpg} gpg --batch --no-verbose --no-armor %{?_gpg_digest_algo:--digest-algo %{_gpg_digest_algo}} --no-secmem-warning -u \"%{_gpg_name}\" -sbo %{__signature_filename} %{__plaintext_filename}" \
