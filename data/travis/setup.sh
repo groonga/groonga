87ba06c7e1c0f74f6e5b3f59d75877f11bd112dd
Kouhei Sutou <kou@clear-code.com>
2012-08-14 11:44:17 +0900

    travis: indent

--- data/travis/setup.sh
+++ data/travis/setup.sh
@@ -14,25 +14,25 @@ if [ "$GROONGA_MASTER" = "yes" ]; then
     sudo make install > /dev/null
     cd ..
 else
-distribution=$(lsb_release --short --id | tr 'A-Z' 'a-z')
-code_name=$(lsb_release --short --codename)
-case $distribution in
-    debian)
-	component=main
-	;;
-    ubuntu)
-	component=universe
-	;;
-esac
-apt_url_base=http://packages.groonga.org
-cat <<EOF | sudo tee /etc/apt/sources.list.d/groonga.list
+    distribution=$(lsb_release --short --id | tr 'A-Z' 'a-z')
+    code_name=$(lsb_release --short --codename)
+    case $distribution in
+	debian)
+	    component=main
+	    ;;
+	ubuntu)
+	    component=universe
+	    ;;
+    esac
+    apt_url_base=http://packages.groonga.org
+    cat <<EOF | sudo tee /etc/apt/sources.list.d/groonga.list
 deb ${apt_url_base}/${distribution}/ ${code_name} ${component}
 deb-src ${apt_url_base}/${distribution}/ ${code_name} ${component}
 EOF
 
-sudo apt-get update
-sudo apt-get -y --allow-unauthenticated install groonga-keyring
-sudo apt-get -y purge zeromq
-sudo apt-get update
-sudo apt-get -y -V install groonga libgroonga-dev
+    sudo apt-get update
+    sudo apt-get -y --allow-unauthenticated install groonga-keyring
+    sudo apt-get -y purge zeromq
+    sudo apt-get update
+    sudo apt-get -y -V install groonga libgroonga-dev
 fi
