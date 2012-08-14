cd36c602f53f964d87af7d65e56a78bd5e14a1ec
Kouhei Sutou <kou@clear-code.com>
2012-08-14 11:44:00 +0900

    travis: support master use

--- data/travis/setup.sh
+++ data/travis/setup.sh
@@ -3,6 +3,17 @@
 set -e
 set -x
 
+if [ "$GROONGA_MASTER" = "yes" ]; then
+    sudo apt-get -y -V install autotools-dev pkg-config libmecab-dev \
+	libmsgpack-dev libevent-dev
+    git clone https://github.com/groonga/groonga.git
+    cd groonga
+    ./autogen.sh
+    ./configure --prefix=/usr --localstatedir=/var --with-debug
+    make -j$(grep '^processor' /proc/cpuinfo | wc -l) > /dev/null
+    sudo make install > /dev/null
+    cd ..
+else
 distribution=$(lsb_release --short --id | tr 'A-Z' 'a-z')
 code_name=$(lsb_release --short --codename)
 case $distribution in
@@ -24,3 +35,4 @@ sudo apt-get -y --allow-unauthenticated install groonga-keyring
 sudo apt-get -y purge zeromq
 sudo apt-get update
 sudo apt-get -y -V install groonga libgroonga-dev
+fi
