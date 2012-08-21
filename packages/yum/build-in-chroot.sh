ca4f4b4e5a77a61974dbd842a03f2cb5f76dfeb7
HAYASHI Kentaro <hayashi@clear-code.com>
2012-08-21 16:32:11 +0900

    yum: support to customize distribution_version for building CentOS rpm

--- packages/yum/build-in-chroot.sh
+++ packages/yum/build-in-chroot.sh
@@ -154,6 +154,9 @@ for architecture in $ARCHITECTURES; do
 		;;
 	    centos)
 		distribution_versions="5 6"
+		if [ ! -z "$DISTRIBUTION_VERSIONS" ]; then
+		    distribution_versions=$DISTRIBUTION_VERSIONS
+		fi
 		;;
 	esac
 	for distribution_version in $distribution_versions; do
