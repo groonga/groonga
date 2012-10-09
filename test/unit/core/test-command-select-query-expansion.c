3b1c4b7dcb8e753e078fdd5e608c678475592a03
Kouhei Sutou <kou@clear-code.com>
2012-10-09 11:43:33 +0900

    Move Cutter tests to grntest tests
    Tests for --query_expansion "(...)" is moved.

--- test/unit/core/test-command-select-query-expansion.c
+++ test/unit/core/test-command-select-query-expansion.c
@@ -186,29 +186,6 @@ data_scalar_and_vector(void)
 }
 
 void
-data_not_expand_paren(void)
-{
-  data_scalar_and_vector();
-}
-
-void
-test_not_expand_paren(gconstpointer data)
-{
-  cut_assert_equal_string(
-    "[[[2],"
-     "[[\"_id\",\"UInt32\"],"
-      "[\"_key\",\"Time\"],"
-      "[\"content\",\"Text\"]],"
-      "[4,1315926000.0,\"Start Ruby!\"],"
-      "[9,1316358000.0,\"Learning Ruby and groonga...\"]]]",
-    send_command(
-      cut_take_printf("select Diaries --sortby _id "
-                      "--match_columns content --query '(Ruby)' "
-                      "--query_expansion Synonyms.%s",
-                      gcut_data_get_string(data, "column-name"))));
-}
-
-void
 data_no_expand(void)
 {
   data_scalar_and_vector();
