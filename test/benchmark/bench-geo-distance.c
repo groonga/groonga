5ef864863429d8f78c5b23fa522934ae1b3584bb
HAYASHI Kentaro <hayashi@clear-code.com>
2012-10-31 11:07:33 +0900

    test: add benchmark test pattern from 3rd to 1st quadrant
    refs #1534

--- test/benchmark/bench-geo-distance.c
+++ test/benchmark/bench-geo-distance.c
@@ -193,6 +193,19 @@ bench_setup_rectangular_wgs84_1st_to_3rd_quadrant_short(gpointer user_data)
 }
 
 static void
+bench_setup_rectangular_wgs84_3rd_to_1st_quadrant_short(gpointer user_data)
+{
+  BenchmarkData *data = user_data;
+
+  bench_setup_common(user_data);
+  bench_setup_points(user_data,
+                     "-56880000x-172310000",
+                     "128452975x503157902",
+                     GRN_DB_WGS84_GEO_POINT);
+  data->geo_distance_proc = GET(data->context, "geo_distance");
+}
+
+static void
 bench_setup_rectangular_wgs84_1st_to_4th_quadrant_short(gpointer user_data)
 {
   BenchmarkData *data = user_data;
@@ -436,6 +449,8 @@ main(int argc, gchar **argv)
             rectangular_wgs84_3rd_to_2nd_quadrant_short);
   REGISTER("rectangular (WGS84 Tokyo to Cape Town)",
             rectangular_wgs84_1st_to_3rd_quadrant_short);
+  REGISTER("rectangular (WGS84 Cape Town to Tokyo)",
+            rectangular_wgs84_3rd_to_1st_quadrant_short);
   REGISTER("spherical (WGS84)", spherical_wgs84);
   REGISTER("spherical (TOKYO)", spherical_tgs);
   REGISTER("hubeny (WGS84)", hubeny_wgs84);
