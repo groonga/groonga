e2e1d01256dfb1410de8564f1308247163703667
HAYASHI Kentaro <hayashi@clear-code.com>
2012-11-01 14:04:22 +0900

    test: support to generate calculated distance from 2nd quadrant to 3rd one

--- test/command/tools/geo/generate-grntest-data.rb
+++ test/command/tools/geo/generate-grntest-data.rb
@@ -569,7 +569,7 @@ class GrnTestData
                                              @longitude_end.to_i,
                                              @latitude_end.to_i)
           (east_distance + west_distance).floor
-        when "1st_to_4th", "4th_to_1st"
+        when "1st_to_4th", "4th_to_1st", "2nd_to_3rd"
           longitude_delta = @longitude_end_degree - @longitude_start_degree
           latitude_delta = @latitude_end_degree - @latitude_start_degree
           if longitude_delta.to_f == 0
