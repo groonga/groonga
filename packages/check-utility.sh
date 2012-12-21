50db0667d0074ccd916b4b18a92635454e2edda2
HAYASHI Kentaro <hayashi@clear-code.com>
2012-12-21 11:02:33 +0900

    doc: drop Ubuntu 11.04 (Naty Narwhal)

--- packages/check-utility.sh
+++ packages/check-utility.sh
@@ -93,7 +93,7 @@ setup_rpm_architectures ()
 setup_codes ()
 {
     if [ -z "$CODES" ]; then
-	CODES="squeeze wheezy unstable lucid natty oneiric precise quantal"
+	CODES="squeeze wheezy unstable lucid oneiric precise quantal"
     fi
 }
 setup_deb_architectures ()
