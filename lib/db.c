8d3dc5ad153ad3d0abbc705f779fbb6307271e28
Kouhei Sutou <kou@clear-code.com>
2013-02-13 22:27:32 +0900

    Support Bool -> UInt8 cast

--- lib/db.c
+++ lib/db.c
@@ -4215,6 +4215,9 @@ grn_obj_cast_bool(grn_ctx *ctx, grn_obj *src, grn_obj *dest, grn_bool addp)
   case GRN_DB_INT8 :
     GRN_INT8_SET(ctx, dest, GRN_BOOL_VALUE(src));
     break;
+  case GRN_DB_UINT8 :
+    GRN_UINT8_SET(ctx, dest, GRN_BOOL_VALUE(src));
+    break;
   default :
     SRC2RECORD();
   }
