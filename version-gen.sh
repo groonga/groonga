f85d39d6866d031932f998f57adac60886aace3c
Kouhei Sutou <kou@clear-code.com>
2013-04-14 11:21:19 +0900

    Support grn_get_version() with "git clone --depth 1" repository

--- version-gen.sh
+++ version-gen.sh
@@ -14,6 +14,10 @@ then
 elif test -d .git -o -f .git
 then
     GRN_VN=`git describe --abbrev=7 HEAD 2>/dev/null`
+    if [ $? -ne 0 ]
+    then
+	GRN_VN=`cat base_version`
+    fi
 fi
 
 GRN_VN=`expr "$GRN_VN" : v*'\(.*\)'`
