5b31dae776377a1cac4b8c8647fb24a0fd584fc4
Kouhei Sutou <kou@clear-code.com>
2013-05-20 18:58:32 +0900

    Support evaluating vector as boolean
    Empty vector is evaluated as false. Otherwise the vector is evaluated
    as true.
    
    But is it OK? Empty array is evaluated as true in ECMAScript. Should
    groonga follow the behavior?

--- lib/db.h
+++ lib/db.h
@@ -392,6 +392,9 @@ grn_rc grn_db_obj_init(grn_ctx *ctx, grn_obj *db, grn_id id, grn_db_obj *obj);
       break;                                            \
     }                                                   \
     break;                                              \
+  case GRN_VECTOR :                                     \
+    result = grn_vector_size(ctx, v) > 0;               \
+    break;                                              \
   default :                                             \
     result = GRN_FALSE;                                 \
     break;                                              \
