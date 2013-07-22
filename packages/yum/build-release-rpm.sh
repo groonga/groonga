492a47d951738abc14c3dd1169a9bfa1767037a7
HAYASHI Kentaro <hayashi@clear-code.com>
2013-07-22 11:05:55 +0900

    rpm fedora: update supported release version from Fedora 18 to Fedora 19

--- packages/yum/build-release-rpm.sh
+++ packages/yum/build-release-rpm.sh
@@ -42,7 +42,7 @@ for distribution in ${DISTRIBUTIONS}; do
     case $distribution in
 	fedora)
 	    distribution_label=Fedora
-	    distribution_versions="18"
+	    distribution_versions="19"
 	    ;;
 	centos)
 	    distribution_label=CentOS
