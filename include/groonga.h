dd162ebcd9be1a2289485a2d8fd1eae68e661f59
whombx <whombx@motel-on-jupiter.sakuraweb.com>
2013-08-04 17:21:56 +0900

    doc: move all the grn_db_*() documents to Sphinx text from header file

--- include/groonga.h
+++ include/groonga.h
@@ -490,18 +490,6 @@ struct _grn_obj {
 
 #define GRN_OBJ_FIN(ctx,obj) (grn_obj_close((ctx), (obj)))
 
-/**
- * grn_db_create:
- * @path: 作成するdbを格納するファイルパス。NULLならtemporary dbとなる。
- * NULL以外のパスを指定した場合はpersistent dbとなる。
- * @optarg: 作成するdbの組み込み型の名前を変更する時に指定する。
- * optarg.builtin_type_namesには組み込み型の名前となるnul終端文字列の配列を指定する。
- * optarg.n_builtin_type_namesには、optarg.builtin_type_namesで指定する文字列の数を
- * 指定する。配列のoffsetはenum型grn_builtin_typeの値に対応する。
- *
- * 新たなdbを作成する。
- **/
-
 typedef struct _grn_db_create_optarg grn_db_create_optarg;
 
 struct _grn_db_create_optarg {
@@ -514,21 +502,7 @@ GRN_API grn_obj *grn_db_create(grn_ctx *ctx, const char *path, grn_db_create_opt
 #define GRN_DB_OPEN_OR_CREATE(ctx,path,optarg,db) \
   (((db) = grn_db_open((ctx), (path))) || (db = grn_db_create((ctx), (path), (optarg))))
 
-/**
- * grn_db_open:
- * @path: 開こうとするdbを格納するファイルパス。
- *
- * 既存のdbを開く。
- **/
 GRN_API grn_obj *grn_db_open(grn_ctx *ctx, const char *path);
-
-/**
- * grn_db_touch:
- * @db: 内容が変更されたdbを指定します。
- *
- * dbの内容の最終更新時刻を現在時刻にします。最終更新時刻は
- * キャッシュが有効かどうかの判断などに利用されます。
- **/
 GRN_API void grn_db_touch(grn_ctx *ctx, grn_obj *db);
 
 /**
