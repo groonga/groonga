55f49ff0912901bd1a94942e90de5193689fa95f
Kouhei Sutou <kou@clear-code.com>
2013-09-21 00:53:37 +0900

    Add "ruby" command by ruby/ruby plugin
    It evaluates Ruby expression and returns evaludated value.
    
    It is very experimental feature. The API will be changed!

--- lib/mrb.c
+++ lib/mrb.c
@@ -45,6 +45,49 @@ grn_ctx_impl_mrb_fin(grn_ctx *ctx)
     ctx->impl->mrb = NULL;
   }
 }
+
+mrb_value
+grn_mrb_eval(grn_ctx *ctx, const char *script, int script_length)
+{
+  mrb_state *mrb = ctx->impl->mrb;
+  int n;
+  mrb_value result;
+  struct mrb_parser_state *parser;
+
+  if (!mrb) {
+    return mrb_nil_value();
+  }
+
+  if (script_length < 0) {
+    script_length = strlen(script);
+  }
+  parser = mrb_parse_nstring(mrb, script, script_length, NULL);
+  n = mrb_generate_code(mrb, parser);
+  result = mrb_run(mrb,
+                   mrb_proc_new(mrb, mrb->irep[n]),
+                   mrb_top_self(mrb));
+  mrb_parser_free(parser);
+
+  return result;
+}
+
+grn_rc
+grn_mrb_to_grn(grn_ctx *ctx, mrb_value mrb_object, grn_obj *grn_object)
+{
+  grn_rc rc = GRN_SUCCESS;
+
+  switch (mrb_type(mrb_object)) {
+  case MRB_TT_FIXNUM :
+    grn_obj_reinit(ctx, grn_object, GRN_DB_INT32, 0);
+    GRN_INT32_SET(ctx, grn_object, mrb_fixnum(mrb_object));
+    break;
+  default :
+    rc = GRN_INVALID_ARGUMENT;
+    break;
+  }
+
+  return rc;
+}
 #else
 void
 grn_ctx_impl_mrb_init(grn_ctx *ctx)
