318087d54a7d9b525846494717f098918544ddc4
Kouhei Sutou <kou@clear-code.com>
2013-09-24 12:27:17 +0900

    Update mruby
    4728070 ->
    eeac4be

---
+++ vendor/mruby-eeac4be/src/re.h
@@ -0,0 +1,13 @@
+/*
+** re.h - Regexp class
+**
+** See Copyright Notice in mruby.h
+*/
+
+#ifndef RE_H
+#define RE_H
+
+#define REGEXP_CLASS          "Regexp"
+#define REGEXP_CLASS_CSTR_LEN 6
+
+#endif
