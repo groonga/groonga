564b29c7b0f671030b6c4e5c3fd715aaa01af3b1
wanabe <s.wanabe@gmail.com>
2013-10-04 01:40:05 +0900

    Move definition of SCAN_XXX, enum scan_stat and typedef of scan_info from expr.c into expr.h

--- lib/expr.h
+++ lib/expr.h
@@ -23,6 +23,21 @@
 extern "C" {
 #endif
 
+#define SCAN_ACCESSOR                  (0x01)
+#define SCAN_PUSH                      (0x02)
+#define SCAN_POP                       (0x04)
+#define SCAN_PRE_CONST                 (0x08)
+
+typedef enum {
+  SCAN_START = 0,
+  SCAN_VAR,
+  SCAN_COL1,
+  SCAN_COL2,
+  SCAN_CONST
+} scan_stat;
+
+typedef struct _grn_scan_info scan_info;
+
 #ifdef __cplusplus
 }
 #endif
