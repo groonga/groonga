2bd6da37b866b650050b7f8344d562f074f20aff
Kouhei Sutou <kou@clear-code.com>
2013-10-04 12:42:51 +0900

    travis: extract script code
    Because we can't use "&" in YAML.

---
+++ tools/travis-script.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+case "${BUILD_TOOLS}" in
+    autotools)
+	NO_RUBY=yes test/unit/run-test.sh
+	test/command/run-test.sh
+	# test/command/run-test.sh --interface http
+	# test/command/run-test.sh --interface http --testee groonga-httpd
+	;;
+    cmake)
+	test/command/run-test.sh
+	;;
+esac
