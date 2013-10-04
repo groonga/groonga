c90da8b66638ce1d2dfa6b8bdbcf16bc576bb910
Kouhei Sutou <kou@clear-code.com>
2013-10-04 11:12:49 +0900

    Unindent

--- lib/expr.c
+++ lib/expr.c
@@ -3952,14 +3952,13 @@ grn_scan_info_open(grn_ctx *ctx, int start)
     return NULL;
   }
 
-  {
-    GRN_INT32_INIT(&si->wv, GRN_OBJ_VECTOR);
-    GRN_PTR_INIT(&si->index, GRN_OBJ_VECTOR, GRN_ID_NIL);
-    si->logical_op = GRN_OP_OR;
-    si->flags = SCAN_PUSH;
-    si->nargs = 0;
-    si->start = start;
-  }
+  GRN_INT32_INIT(&si->wv, GRN_OBJ_VECTOR);
+  GRN_PTR_INIT(&si->index, GRN_OBJ_VECTOR, GRN_ID_NIL);
+  si->logical_op = GRN_OP_OR;
+  si->flags = SCAN_PUSH;
+  si->nargs = 0;
+  si->start = start;
+
   return si;
 }
 
