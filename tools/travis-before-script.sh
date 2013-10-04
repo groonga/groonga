1deeb8486f589e33e311b8dee28598f7384e5cc8
Kouhei Sutou <kou@clear-code.com>
2013-10-04 14:59:44 +0900

    travis: indent

--- tools/travis-before-script.sh
+++ tools/travis-before-script.sh
@@ -4,15 +4,15 @@ set -e
 
 case "${BUILD_TOOL}" in
     autotools)
-./autogen.sh
+	./autogen.sh
 
-configure_args=""
-#if [ "$CC" = "clang" ]; then
-    configure_args="${configure_args} --enable-debug"
-#fi
+	configure_args=""
+	#if [ "$CC" = "clang" ]; then
+	    configure_args="${configure_args} --enable-debug"
+	#fi
 
-./configure --with-ruby19 ${configure_args}
-;;
+	./configure --with-ruby19 ${configure_args}
+	;;
     cmake)
 	cmake . -DGRN_WITH_DEBUG=yes -DGRN_WITH_MRUBY=yes
 	;;
