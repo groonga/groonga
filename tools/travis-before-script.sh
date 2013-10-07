82eae6fecb393b0de76ce8c980bb4104fd0e9b2a
Kouhei Sutou <kou@clear-code.com>
2013-10-07 11:56:57 +0900

    travis: test mruby build

--- tools/travis-before-script.sh
+++ tools/travis-before-script.sh
@@ -10,10 +10,19 @@ case "${BUILD_TOOL}" in
 	#if [ "$CC" = "clang" ]; then
 	    configure_args="${configure_args} --enable-debug"
 	#fi
+	if [ "$ENABLE_MRUBY" = "yes" ]; then
+	    configure_args="${configure_args} --enable-mruby"
+	fi
 
 	./configure --with-ruby19 ${configure_args}
 	;;
     cmake)
-	cmake . -DGRN_WITH_DEBUG=yes -DGRN_WITH_MRUBY=yes
+	cmake_args=""
+	cmake_args="${cmake_args} -DGRN_WITH_DEBUG=yes"
+	if [ "$ENABLE_MRUBY" = "yes" ]; then
+	    cmake_args="${cmake_args} -DGRN_WITH_MRUBY=yes"
+	fi
+
+	cmake . ${cmake_args}
 	;;
 esac
