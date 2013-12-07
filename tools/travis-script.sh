fa91aba487b94acc872527909d1cd5b5d96d7a0b
Kouhei Sutou <kou@clear-code.com>
2013-12-07 20:47:31 +0900

    travis: disable HTTP server tests

--- tools/travis-script.sh
+++ tools/travis-script.sh
@@ -6,7 +6,7 @@ case "${BUILD_TOOL}" in
     autotools)
 	NO_RUBY=yes test/unit/run-test.sh
 	test/command/run-test.sh
-	test/command/run-test.sh --interface http
+	# test/command/run-test.sh --interface http
 	# test/command/run-test.sh --interface http --testee groonga-httpd
 	;;
     cmake)
