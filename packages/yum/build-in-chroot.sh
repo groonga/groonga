4013f7ebd06aaf46d808b6366a98df695f2f1b58
HAYASHI Kentaro <hayashi@clear-code.com>
2013-12-26 12:36:32 +0900

    rpm fedora: fix mismatch for output directory in i386 architecture

--- packages/yum/build-in-chroot.sh
+++ packages/yum/build-in-chroot.sh
@@ -94,6 +94,11 @@ build()
     build_user_dir=${base_dir}/home/${build_user}
     rpm_base_dir=${build_user_dir}/rpmbuild
     rpm_dir=${rpm_base_dir}/RPMS/${architecture}
+    if [ $distribution = "fedora" ]; then
+	if [ $architecture = "i386" ]; then
+	    rpm_dir=${rpm_base_dir}/RPMS/i686
+	fi
+    fi
     srpm_dir=${rpm_base_dir}/SRPMS
     pool_base_dir=${DESTINATION}${distribution}/${distribution_version}
     if test "${HAVE_DEVELOPMENT_BRANCH}" = "yes"; then
