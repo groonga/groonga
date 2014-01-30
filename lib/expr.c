groonga/groonga

commit 33efe2bd2fd6e87d2d4e209f08c4a03573e4f049

Fix a bug that an object in grn_expr is used after it is freed
It is caused when you specify column name including pseudo column name
such as _key by GRN_BULK. Groonga server users will not do the
usage. Rroonga users may use the usage. For example:

    expression.parse("true",
                     :default_column => "_id", # <- HERE
                     :syntax => :script)

lib/expr.c
@@ -1127,7 +1127,6 @@ grn_expr_append_obj(grn_ctx *ctx, grn_obj *expr, grn_obj *obj, grn_operator op, 
               obj = col;
               type = col->header.type;
               domain = grn_obj_get_range(ctx, col);
-              grn_obj_unlink(ctx, col);
             }
           } else {
             domain = grn_obj_get_range(ctx, obj);

commit 24b0264c1629416ba76182ff540d85c487e9d465

Fix a memory leak introduced in 33efe2bd2fd6e87d2d4e209f08c4a03573e4f049

lib/expr.c
@@ -1127,6 +1127,7 @@ grn_expr_append_obj(grn_ctx *ctx, grn_obj *expr, grn_obj *obj, grn_operator op, 
               obj = col;
               type = col->header.type;
               domain = grn_obj_get_range(ctx, col);
+              GRN_PTR_PUT(ctx, &(e->objs), col);
             }
           } else {
             domain = grn_obj_get_range(ctx, obj);