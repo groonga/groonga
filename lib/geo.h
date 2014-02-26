b4f2ee6118db2ae22cb6c3b28300f77cabe86fe0
Sebastian Wiedenroth <wiedi@frubar.net>
2014-02-26 14:49:06 +0100

    Need MIN/MAX macros on SunOS too

--- lib/geo.h
+++ lib/geo.h
@@ -24,7 +24,7 @@
 #include "ii.h"
 #include "db.h"
 
-#ifdef WIN32
+#if defined(WIN32) || defined(__sun)
 # define _USE_MATH_DEFINES
 # ifndef MAX
 #  define MAX(a, b) ((a) > (b) ? (a) : (b))
@@ -33,7 +33,7 @@
 # ifndef MIN
 #  define MIN(a, b) ((a) < (b) ? (a) : (b))
 # endif
-#endif /* WIN32 */
+#endif /* WIN32 or __sun */
 #include <math.h>
 
 #ifdef __cplusplus
