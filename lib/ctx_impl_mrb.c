89c4ed4b1584f555c9cfbbece682126d6281608d
Kouhei Sutou <kou@clear-code.com>
2014-05-28 22:45:34 +0900

    mrb: bind variable size column

--- lib/ctx_impl_mrb.c
+++ lib/ctx_impl_mrb.c
@@ -24,6 +24,7 @@
 #include "mrb/mrb_obj.h"
 #include "mrb/mrb_column.h"
 #include "mrb/mrb_fixed_size_column.h"
+#include "mrb/mrb_variable_size_column.h"
 #include "mrb/mrb_expr.h"
 #include "mrb/mrb_accessor.h"
 #include "mrb/mrb_index_info.h"
@@ -41,6 +42,7 @@ grn_ctx_impl_mrb_init_bindings(grn_ctx *ctx)
   grn_mrb_obj_init(ctx);
   grn_mrb_column_init(ctx);
   grn_mrb_fixed_size_column_init(ctx);
+  grn_mrb_variable_size_column_init(ctx);
   grn_mrb_expr_init(ctx);
   grn_mrb_accessor_init(ctx);
   grn_mrb_index_info_init(ctx);
