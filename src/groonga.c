497ed33ab261306e91f4158fd9990352bbe920f3
Kouhei Sutou <kou@clear-code.com>
2014-06-02 09:41:09 +0900

    Add compatible API for Visual C++

--- src/groonga.c
+++ src/groonga.c
@@ -48,6 +48,12 @@
 # include <sys/sysctl.h>
 #endif /* HAVE_SYS_SYSCTL_H */
 
+#ifndef HAVE_STRNCASECMP
+# ifdef HAVE__STRNICMP
+#  define strncasecmp(s1,s2,n) _strnicmp(s1,s2,n)
+# endif /* HAVE__STRNICMP */
+#endif /* HAVE_STRNCASECMP */
+
 #ifndef USE_MSG_NOSIGNAL
 # ifdef MSG_NOSIGNAL
 #  undef MSG_NOSIGNAL
