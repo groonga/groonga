d3a73fb84dcc4f4b286c5f1a7b0072472c2a6359
Kouhei Sutou <kou@clear-code.com>
2014-09-13 00:14:50 +0900

    mruby: fix a bug that flags isn't copied

--- lib/mrb/scripts/scan_info_builder.rb
+++ lib/mrb/scripts/scan_info_builder.rb
@@ -293,6 +293,7 @@ module Groonga
     def create_between_data(data, next_data)
       between_data = ScanInfoData.new(data.start)
       between_data.end = next_data.end
+      between_data.flags = data.flags
       between_data.op = Operator::CALL
       between_data.logical_op = data.logical_op
       between_data.args = create_between_data_args(data, next_data)
