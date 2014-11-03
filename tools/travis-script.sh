5d205ada8ac9b91c8e0c0a8a1a7322d8302bd65e
Kouhei Sutou <kou@clear-code.com>
2014-11-03 16:39:45 +0900

    travis: run query optimizer tests with mruby enabled build

--- tools/travis-script.sh
+++ tools/travis-script.sh
@@ -6,6 +6,9 @@ case "${BUILD_TOOL}" in
   autotools)
     test/unit/run-test.sh
     test/command/run-test.sh
+    if [ "${ENABLE_MRUBY" = "yes" ]; then
+      test/query_optimizer/run-test.rb
+    fi
     # test/command/run-test.sh --interface http
     # test/command/run-test.sh --interface http --testee groonga-httpd
     ;;
