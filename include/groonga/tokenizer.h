e2b455172da18147ccc7a067137aae2555126caf
Kouhei Sutou <kou@clear-code.com>
2014-11-17 22:40:45 +0900

    Extract grn_token declarations

--- include/groonga/tokenizer.h
+++ include/groonga/tokenizer.h
@@ -18,9 +18,8 @@
 #ifndef GROONGA_TOKENIZER_H
 #define GROONGA_TOKENIZER_H
 
-#include <stddef.h>
-
 #include <groonga/plugin.h>
+#include <groonga/token.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -28,23 +27,6 @@ extern "C" {
 
 #define GRN_TOKENIZER_TOKENIZED_DELIMITER_UTF8     "\xEF\xBF\xBE"
 #define GRN_TOKENIZER_TOKENIZED_DELIMITER_UTF8_LEN 3
-
-/*
-  grn_token_mode describes propose for tokenization.
-
-  `GRN_TOKEN_GET`: Tokenization for search.
-
-  `GRN_TOKEN_ADD`: Tokenization for adding token to index.
-
-  `GRN_TOKEN_DEL`: Tokenization for deleting token from index.
-
-  @since 4.0.7
- */
-typedef enum {
-  GRN_TOKEN_GET = 0,
-  GRN_TOKEN_ADD,
-  GRN_TOKEN_DEL
-} grn_token_mode;
 
 /*
   grn_tokenizer_charlen() returns the length (#bytes) of the first character
@@ -163,64 +145,6 @@ GRN_PLUGIN_EXPORT void grn_tokenizer_token_init(grn_ctx *ctx, grn_tokenizer_toke
 GRN_PLUGIN_EXPORT void grn_tokenizer_token_fin(grn_ctx *ctx, grn_tokenizer_token *token);
 
 /*
- * grn_token_status is a flag set for tokenizer status codes.
- * If a document or query contains no tokens, push an empty string with
- * GRN_TOKEN_LAST as a token.
- *
- * @since 4.0.7
- */
-typedef unsigned int grn_token_status;
-
-/*
- * GRN_TOKEN_CONTINUE means that the next token is not the last one.
- *
- * @since 4.0.7
- */
-#define GRN_TOKEN_CONTINUE           (0)
-/*
- * GRN_TOKEN_LAST means that the next token is the last one.
- *
- * @since 4.0.7
- */
-#define GRN_TOKEN_LAST               (0x01L<<0)
-/*
- * GRN_TOKEN_OVERLAP means that ...
- *
- * @since 4.0.7
- */
-#define GRN_TOKEN_OVERLAP            (0x01L<<1)
-/*
- * GRN_TOKEN_UNMATURED means that ...
- *
- * @since 4.0.7
- */
-#define GRN_TOKEN_UNMATURED          (0x01L<<2)
-/*
- * GRN_TOKEN_REACH_END means that ...
- *
- * @since 4.0.7
- */
-#define GRN_TOKEN_REACH_END          (0x01L<<3)
-/*
- * GRN_TOKEN_SKIP means that the token is skipped
- *
- * @since 4.0.7
- */
-#define GRN_TOKEN_SKIP               (0x01L<<4)
-/*
- * GRN_TOKEN_SKIP_WITH_POSITION means that the token and postion is skipped
- *
- * @since 4.0.7
- */
-#define GRN_TOKEN_SKIP_WITH_POSITION (0x01L<<5)
-/*
- * GRN_TOKEN_FORCE_PREIX that the token is used common prefix search
- *
- * @since 4.0.7
- */
-#define GRN_TOKEN_FORCE_PREFIX       (0x01L<<6)
-
-/*
  * grn_tokenizer_status is a flag set for tokenizer status codes.
  * If a document or query contains no tokens, push an empty string with
  * GRN_TOKENIZER_TOKEN_LAST as a token.
@@ -286,21 +210,6 @@ typedef grn_token_status grn_tokenizer_status;
  */
 #define GRN_TOKENIZER_CONTINUE GRN_TOKENIZER_TOKEN_CONTINUE
 #define GRN_TOKENIZER_LAST     GRN_TOKENIZER_TOKEN_LAST
-
-typedef struct _grn_token grn_token;
-
-GRN_PLUGIN_EXPORT grn_obj *grn_token_get_data(grn_ctx *ctx,
-                                              grn_token *token);
-GRN_PLUGIN_EXPORT grn_rc grn_token_set_data(grn_ctx *ctx,
-                                            grn_token *token,
-                                            const char *str_ptr,
-                                            int str_length);
-GRN_PLUGIN_EXPORT grn_tokenizer_status grn_token_get_status(grn_ctx *ctx,
-                                                            grn_token *token);
-GRN_PLUGIN_EXPORT grn_rc grn_token_set_status(grn_ctx *ctx,
-                                              grn_token *token,
-                                              grn_token_status status);
-
 
 /*
   grn_tokenizer_token_push() pushes the next token into `token'. Note that
