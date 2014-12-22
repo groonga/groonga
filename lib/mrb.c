34bbe84d6669578c74216f29de701bd062191a5d
Kouhei Sutou <kou@clear-code.com>
2014-12-22 23:59:10 +0900

    mrb: raise error on load

--- lib/mrb.c
+++ lib/mrb.c
@@ -126,7 +126,7 @@ grn_mrb_load(grn_ctx *ctx, const char *path)
     SERR(message);
     exception = mrb_exc_new(mrb, E_LOAD_ERROR,
                             ctx->errbuf, strlen(ctx->errbuf));
-    mrb->exc = mrb_obj_ptr(exception);
+    mrb_exc_raise(mrb, exception);
     return mrb_nil_value();
   }
 
