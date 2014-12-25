0a9bd525fbbcd50d6452a83e99dc0b8f61edcd26
Kouhei Sutou <kou@clear-code.com>
2014-12-25 22:08:05 +0900

    grndb: add --action

--- lib/mrb/scripts/command/grndb.rb
+++ lib/mrb/scripts/command/grndb.rb
@@ -25,22 +25,51 @@ module Groonga
           return false
         end
 
+        succeeded = false
         database = Groonga::Database.open(rest[0])
         begin
-          database.recover
+          succeeded = run_action(slop, database)
         ensure
           database.close
         end
-
-        true
+        succeeded
       end
 
       private
+      ACTIONS = [:recover, :check]
+
       def create_slop
         slop = Slop.new
         slop.banner = "Usage: #{File.basename(@command)} [options] DB_PATH"
         slop.on("-h", "--help", "Display this help message.", :tail => true)
+        slop.on("--action=",
+                "Action to do. Available actions: #{format_actions}",
+                :default => ACTIONS.first,
+                :as => lambda {|value| action_value(value)})
         slop
+      end
+
+      def format_actions
+        "[#{ACTIONS.join(", ")}]"
+      end
+
+      def action_value(value)
+        action = value.to_sym
+        case action
+        when *ACTIONS
+          action
+        else
+          message = "action must be one of #{format_actions}: #{value.inspect}"
+          raise Slop::InvalidArgumentError, message
+        end
+      end
+
+      def run_action(slop, database)
+        case slop[:action]
+        when :recover
+          database.recover
+          true
+        end
       end
     end
   end
