16be2a0bd658246614d7e15ae5bdac951dbf41ca
Kouhei Sutou <kou@clear-code.com>
2014-12-26 00:25:20 +0900

    mrb: remove debug prints

--- lib/mrb/scripts/table_cursor.rb
+++ lib/mrb/scripts/table_cursor.rb
@@ -3,8 +3,6 @@ module Groonga
     def each
       loop do
         id = self.next
-        p [id]
-        p Groonga::ID::NIL
         return if id == Groonga::ID::NIL
         yield(id)
       end
