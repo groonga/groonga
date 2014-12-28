196e10ad4b4b24e12c346842dc4cc80fb3415b1c
Kouhei Sutou <kou@clear-code.com>
2014-12-28 21:17:33 +0900

    mrb: bind Groonga::Type

--- lib/ctx_impl_mrb.c
+++ lib/ctx_impl_mrb.c
@@ -37,6 +37,7 @@
 #include "mrb/mrb_fixed_size_column.h"
 #include "mrb/mrb_variable_size_column.h"
 #include "mrb/mrb_index_column.h"
+#include "mrb/mrb_type.h"
 #include "mrb/mrb_expr.h"
 #include "mrb/mrb_accessor.h"
 #include "mrb/mrb_procedure.h"
@@ -107,6 +108,7 @@ grn_ctx_impl_mrb_init_bindings(grn_ctx *ctx)
   grn_mrb_fixed_size_column_init(ctx);
   grn_mrb_variable_size_column_init(ctx);
   grn_mrb_index_column_init(ctx);
+  grn_mrb_type_init(ctx);
   grn_mrb_expr_init(ctx);
   grn_mrb_accessor_init(ctx);
   grn_mrb_procedure_init(ctx);
