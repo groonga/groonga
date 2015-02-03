5e4bb759948f7b80774ffc22824f0f0d837e7d69
Kouhei Sutou <kou@clear-code.com>
2015-02-03 13:06:21 +0900

    mrb: add Database#each_table

--- lib/mrb/scripts/database.rb
+++ lib/mrb/scripts/database.rb
@@ -15,5 +15,23 @@ module Groonga
         cursor.close
       end
     end
+
+    def each_table(options={})
+      context = Context.instance
+      min = options[:prefix]
+      flags =
+        TableCursorFlags::ASCENDING |
+        TableCursorFlags::BY_KEY
+      flags |= TableCursorFlags::PREFIX if min
+      cursor = TableCursor.open(self, :min => min, :flags => flags)
+      begin
+        cursor.each do |id|
+          object = context[id]
+          yield(object) if object.is_a?(Table)
+        end
+      ensure
+        cursor.close
+      end
+    end
   end
 end
