89ee3b968ba602472862f9fc71722351ed9301a7
Kouhei Sutou <kou@clear-code.com>
2015-02-03 16:07:58 +0900

    mrb: add missing nil initialization

--- plugins/sharding/search.rb
+++ plugins/sharding/search.rb
@@ -18,11 +18,30 @@ module Groonga
           raise InvalidArgument, "[logical_count] logical_table is missing"
         end
 
+        filter = input[:filter]
+
         total = 0
         context.database.each_table(:prefix => "#{logical_table}_") do |table|
-          total += table.size
+          total += count_n_records(table, filter)
         end
         output(total)
+      end
+
+      private
+      def count_n_records(table, filter)
+        return table.size if filter.nil?
+
+        expression = nil
+        filtered_table = nil
+        begin
+          expression = Expression.create(table)
+          expression.parse(filter)
+          filtered_table = table.select(expression)
+          filtered_table.size
+        ensure
+          filtered_table.close if filtered_table
+          expression.close if expression
+        end
       end
     end
   end
