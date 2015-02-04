448804a72c9d37ee06f48ee856bfb59fc5b5116c
Kouhei Sutou <kou@clear-code.com>
2015-02-04 18:30:34 +0900

    logical_count: support partial range

--- plugins/sharding/search.rb
+++ plugins/sharding/search.rb
@@ -13,39 +13,99 @@ module Groonga
                ])
 
       def run_body(input)
+        logical_table = input[:logical_table]
+        if logical_table.nil?
+          raise InvalidArgument, "[logical_count] logical_table is missing"
+        end
+        shard_key = input[:shard_key]
+        if shard_key.nil?
+          raise InvalidArgument, "[logical_count] shard_key is missing"
+        end
         filter = input[:filter]
 
         total = 0
-        each_table(input) do |table|
-          total += count_n_records(table, filter)
+        target_range = TargetRange.new(input)
+        each_table(logical_table) do |table, shard_range|
+          total += count_n_records(table, filter,
+                                   shard_key, shard_range, target_range)
         end
         output(total)
       end
 
       private
-      def each_table(input)
-        logical_table = input[:logical_table]
-        if logical_table.nil?
-          raise InvalidArgument, "[logical_count] logical_table is missing"
-        end
-
+      def each_table(logical_table)
         prefix = "#{logical_table}_"
-        target_range = TargetRange.new(input)
         context.database.each_table(:prefix => prefix) do |table|
-          shard_key = table.name[prefix.size..-1]
-          next unless target_range.include?(shard_key)
-          yield(table)
+          shard_range_raw = table.name[prefix.size..-1]
+
+          next unless /\A(\d{4})(\d{2})(\d{2})\z/ =~ shard_range_raw
+          shard_range = ShardRange.new($1.to_i, $2.to_i, $3.to_i)
+
+          yield(table, shard_range)
         end
       end
 
-      def count_n_records(table, filter)
-        return table.size if filter.nil?
+      def count_n_records(table, filter, shard_key, shard_range, target_range)
+        cover_type = target_range.cover_type(shard_range)
+        case cover_type
+        when :none
+          return 0
+        when :all
+          return table.size if filter.nil?
+          filtered_count_n_records(table, filter)
+        when :partial_min
+          filtered_count_n_records(table, filter) do |expression|
+            expression.append_constant(shard_key, Operator::PUSH, 1)
+            expression.append_operator(Operator::GET_VALUE, 1)
+            expression.append_constant(target_range.min, Operator::PUSH, 1)
+            if target_range.min_border == :include
+              expression.append_operator(Operator::GREATER_EQUAL, 2)
+            else
+              expression.append_operator(Operator::GREATER, 2)
+            end
+          end
+        when :partial_max
+          filtered_count_n_records(table, filter) do |expression|
+            expression.append_constant(shard_key, Operator::PUSH, 1)
+            expression.append_operator(Operator::GET_VALUE, 1)
+            expression.append_constant(target_range.max, Operator::PUSH, 1)
+            if target_range.max_border == :include
+              expression.append_operator(Operator::LESS_EQUAL, 2)
+            else
+              expression.append_operator(Operator::LESS, 2)
+            end
+          end
+        when :partial_min_and_max
+          filtered_count_n_records(table, filter) do |expression|
+            expression.append_object(context["between"], Operator::PUSH, 1)
+            expression.append_constant(shard_key, Operator::PUSH, 1)
+            expression.append_operator(Operator::GET_VALUE, 1)
+            expression.append_constant(target_range.min, Operator::PUSH, 1)
+            expression.append_constant(target_range.min_border,
+                                       Operator::PUSH, 1)
+            expression.append_constant(target_range.max, Operator::PUSH, 1)
+            expression.append_constant(target_range.max_border,
+                                       Operator::PUSH, 1)
+            expression.append_operator(Operator::CALL, 5)
+          end
+        end
+      end
 
+      def filtered_count_n_records(table, filter)
         expression = nil
         filtered_table = nil
+
         begin
           expression = Expression.create(table)
-          expression.parse(filter)
+          if block_given?
+            yield(expression)
+            if filter
+              expression.parse(filter)
+              expression.append_operator(Operator::AND, 2)
+            end
+          else
+            expression.parse(filter)
+          end
           filtered_table = table.select(expression)
           filtered_table.size
         ensure
@@ -54,23 +114,58 @@ module Groonga
         end
       end
 
+      class ShardRange
+        attr_reader :year, :month, :day
+        def initialize(year, month, day)
+          @year = year
+          @month = month
+          @day = day
+        end
+      end
+
       class TargetRange
+        attr_reader :min, :min_border
+        attr_reader :max, :max_border
         def initialize(input)
           @input = input
+          @shard_key = input[:shard_key]
           @min = parse_value(:min)
           @min_border = parse_border(:min_border)
           @max = parse_value(:max)
           @max_border = parse_border(:max_border)
         end
 
-        def include?(shard_key)
-          if /\A(\d{4})(\d{2})(\d{2})\z/ =~ shard_key
-            year = $1.to_i
-            month = $2.to_i
-            day = $3.to_i
-            in_range?(year, month, day)
+        def cover_type(shard_range)
+          return :all if @min.nil? and @max.nil?
+
+          if @min and @max
+            return :none unless in_min?(shard_range)
+            return :none unless in_max?(shard_range)
+            min_partial_p = in_min_partial?(shard_range)
+            max_partial_p = in_max_partial?(shard_range)
+            if min_partial_p and max_partial_p
+              :partial_min_and_max
+            elsif min_partial_p
+              :partial_min
+            elsif max_partial_p
+              :partial_max
+            else
+              :all
+            end
+          elsif @min
+            return :none unless in_min?(shard_range)
+            if in_min_partial?(shard_range)
+              :partial_min
+            else
+              :all
+            end
           else
-            false
+            return :none unless in_max?(shard_range)
+            if in_max_partial?(shard_range)
+              :partial_max
+            else
+              :all
+            end
           end
         end
 
@@ -99,26 +194,41 @@ module Groonga
           end
         end
 
-        def in_range?(year, month, day)
-          in_min?(year, month, day) and in_max?(year, month, day)
+        def in_min?(shard_range)
+          base_time = Time.local(shard_range.year,
+                                 shard_range.month,
+                                 shard_range.day + 1)
+          @min < base_time
         end
 
-        def in_min?(year, month, day)
-          return true if @min.nil?
+        def in_min_partial?(shard_range)
+          return false unless @min.year == shard_range.year
+          return false unless @min.month == shard_range.month
+          return false unless @min.day == shard_range.day
 
-          min_base_time = Time.local(year, month, day + 1)
-          @min < min_base_time
+          return true if @min_border == :exclude
+
+          @min.hour != 0 and
+            @min.min != 0 and
+            @min.sec != 0 and
+            @min.usec != 0
         end
 
-        def in_max?(year, month, day)
-          return true if @max.nil?
-
-          max_base_time = Time.local(year, month, day)
+        def in_max?(shard_range)
+          max_base_time = Time.local(shard_range.year,
+                                     shard_range.month,
+                                     shard_range.day)
           if @max_border == :include
             @max >= max_base_time
           else
             @max > max_base_time
           end
+        end
+
+        def in_max_partial?(shard_range)
+          @max.year == shard_range.year and
+            @max.month == shard_range.month and
+            @max.day == shard_range.day
         end
       end
     end
