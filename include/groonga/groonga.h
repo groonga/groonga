5bfa57f0e1becafea1de488acfe815bf615743f1
Kouhei Sutou <kou@clear-code.com>
2015-02-08 19:19:25 +0900

    Extract compare operation codes as functions
    It's for reusing these operations in plugin without creating grn_expr.

--- include/groonga/groonga.h
+++ include/groonga/groonga.h
@@ -714,6 +714,12 @@ GRN_API const char *grn_operator_to_string(grn_operator op);
 GRN_API grn_bool grn_operator_exec_equal(grn_ctx *ctx, grn_obj *x, grn_obj *y);
 GRN_API grn_bool grn_operator_exec_not_equal(grn_ctx *ctx,
                                              grn_obj *x, grn_obj *y);
+GRN_API grn_bool grn_operator_exec_less(grn_ctx *ctx, grn_obj *x, grn_obj *y);
+GRN_API grn_bool grn_operator_exec_greater(grn_ctx *ctx, grn_obj *x, grn_obj *y);
+GRN_API grn_bool grn_operator_exec_less_equal(grn_ctx *ctx,
+                                              grn_obj *x, grn_obj *y);
+GRN_API grn_bool grn_operator_exec_greater_equal(grn_ctx *ctx,
+                                                 grn_obj *x, grn_obj *y);
 
 struct _grn_table_group_result {
   grn_obj *table;
