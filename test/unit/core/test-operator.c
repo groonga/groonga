8e56d1193b459ccfa6731258dd7b21c44b68744b
Kouhei Sutou <kou@clear-code.com>
2015-03-10 15:50:02 +0900

    test: add tests for grn_operator_exec_less_equal()

--- test/unit/core/test-operator.c
+++ test/unit/core/test-operator.c
@@ -41,6 +41,10 @@ void data_exec_greater_true(void);
 void test_exec_greater_true(gconstpointer data);
 void data_exec_greater_false(void);
 void test_exec_greater_false(gconstpointer data);
+void data_exec_less_equal_true(void);
+void test_exec_less_equal_true(gconstpointer data);
+void data_exec_less_equal_false(void);
+void test_exec_less_equal_false(gconstpointer data);
 
 static gchar *tmp_directory;
 
@@ -375,4 +379,71 @@ test_exec_greater_false(gconstpointer data)
   set_one(&rhs, rhs_type);
 
   cut_assert_false(grn_operator_exec_greater(context, &lhs, &rhs));
+}
+
+void
+data_exec_less_equal_true(void)
+{
+#define ADD_DATA(lhs_type, rhs_type)                            \
+  gcut_add_datum(lhs_type " <= " rhs_type,                      \
+                 "lhs_type", G_TYPE_STRING, lhs_type,           \
+                 "rhs_type", G_TYPE_STRING, rhs_type,           \
+                 NULL)
+
+  ADD_DATA("int32", "int32");
+  ADD_DATA("text", "text");
+  ADD_DATA("text", "int32");
+  ADD_DATA("int32", "text");
+
+#undef ADD_DATA
+}
+
+void
+test_exec_less_equal_true(gconstpointer data)
+{
+  const gchar *lhs_type;
+  const gchar *rhs_type;
+
+  lhs_type = gcut_data_get_string(data, "lhs_type");
+  rhs_type = gcut_data_get_string(data, "rhs_type");
+
+  set_one(&lhs, lhs_type);
+
+  set_one(&rhs, rhs_type);
+  cut_assert_true(grn_operator_exec_less_equal(context, &lhs, &rhs));
+
+  set_two(&rhs, rhs_type);
+  cut_assert_true(grn_operator_exec_less_equal(context, &lhs, &rhs));
+}
+
+void
+data_exec_less_equal_false(void)
+{
+#define ADD_DATA(lhs_type, rhs_type)                            \
+  gcut_add_datum(lhs_type " <= " rhs_type,                      \
+                 "lhs_type", G_TYPE_STRING, lhs_type,           \
+                 "rhs_type", G_TYPE_STRING, rhs_type,           \
+                 NULL)
+
+  ADD_DATA("int32", "int32");
+  ADD_DATA("text", "text");
+  ADD_DATA("text", "int32");
+  ADD_DATA("int32", "text");
+
+#undef ADD_DATA
+}
+
+void
+test_exec_less_equal_false(gconstpointer data)
+{
+  const gchar *lhs_type;
+  const gchar *rhs_type;
+
+  lhs_type = gcut_data_get_string(data, "lhs_type");
+  rhs_type = gcut_data_get_string(data, "rhs_type");
+
+  set_two(&lhs, lhs_type);
+  set_one(&rhs, rhs_type);
+
+  cut_assert_false(grn_operator_exec_less_equal(context, &lhs, &rhs));
 }
