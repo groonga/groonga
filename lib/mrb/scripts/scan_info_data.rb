55fe0b932489c24747d589a080fba63d3a2170bc
Kouhei Sutou <kou@clear-code.com>
2015-04-01 12:50:29 +0900

    mrb: fix "\\" handling

--- lib/mrb/scripts/scan_info_data.rb
+++ lib/mrb/scripts/scan_info_data.rb
@@ -124,6 +124,9 @@ module Groonga
             return false
           when "k", "g", "1", "2", "3", "4", "5", "6", "7", "8", "9"
             return false
+          when "\\"
+            previous_char = nil
+            next
           end
         else
           case char
