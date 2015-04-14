effec70e47dd643ef51cf123101f4dd11c6f3049
Kouhei Sutou <kou@clear-code.com>
2015-04-14 22:32:11 +0900

    windows: show real error for lseek()

--- lib/ii.c
+++ lib/ii.c
@@ -7386,7 +7386,7 @@ grn_ii_buffer_fetch(grn_ctx *ctx, grn_ii_buffer *ii_buffer,
         }
       }
       if (grn_lseek(ii_buffer->tmpfd, block->head, SEEK_SET) != block->head) {
-        SERR("grn_lseek");
+        ERRNO_ERR("grn_lseek");
         return;
       }
       if (read(ii_buffer->tmpfd, block->buffer, bytesize) != bytesize) {
