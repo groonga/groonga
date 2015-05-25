456eced55006634415e21bca80cb5df48f7c066c
HAYASHI Kentaro <hayashi@clear-code.com>
2015-05-25 13:52:52 +0900

    travis: clone master branch only
    It reduces a cost of clone.
    
    Before:
    
      git clone --depth 1 --recursive git@github.com:groonga/groonga groonga.depth1  6.27s user 2.24s system 16% cpu 52.319 total
    
    After:
    
      git clone --depth 1 --recursive --branch master git@github.com:groonga/groonga  6.18s user 2.04s system 24% cpu 33.450 total

--- data/travis/setup.sh
+++ data/travis/setup.sh
@@ -21,7 +21,7 @@ set -e
 if [ "$GROONGA_MASTER" = "yes" ]; then
     sudo apt-get install -qq -y -V autotools-dev pkg-config libmecab-dev \
 	libmsgpack-dev libevent-dev
-    git clone --recursive --depth 1 https://github.com/groonga/groonga.git
+    git clone --recursive --depth 1 --branch master https://github.com/groonga/groonga.git
     cd groonga
     ./autogen.sh
     ./configure --prefix=/usr --localstatedir=/var --enable-debug
