4e311b55b4abe8bf71bb0c2a7e1c151fdd89acaf
Kouhei Sutou <kou@clear-code.com>
2015-07-16 12:19:20 +0900

    sharding: fix wrong min include detection for month range type

--- plugins/sharding/logical_enumerator.rb
+++ plugins/sharding/logical_enumerator.rb
@@ -270,10 +270,7 @@ module Groonga
 
           return true if @min_border == :exclude
 
-          not (@min.hour == 0 and
-               @min.min  == 0 and
-               @min.sec  == 0 and
-               @min.usec == 0)
+          shard_range.min_time != @min
         end
 
         def in_max?(shard_range)
