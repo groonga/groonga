b8fb765b5df2436e1006537b13454c1ac1beb628
Kouhei Sutou <kou@clear-code.com>
2015-10-07 18:06:57 +0900

    mrb: add column value read support to Record

--- lib/mrb/scripts/record.rb
+++ lib/mrb/scripts/record.rb
@@ -8,5 +8,31 @@ module Groonga
         "@id=#{@id.inspect}, @table=#{@table.inspect}>"
       end
     end
+
+    def [](name)
+      column = find_column(name)
+      if column.nil?
+        raise InvalidArgument, "unknown column: <#{absolute_column_name(name)}>"
+      end
+      column[@id]
+    end
+
+    def method_missing(name, *args, &block)
+      return super unless args.empty?
+
+      column = find_column(name)
+      return super if column.nil?
+
+      column[@id]
+    end
+
+    private
+    def absolute_column_name(name)
+      "#{@table.name}.#{name}"
+    end
+
+    def find_column(name)
+      Context.instance[absolute_column_name(name)]
+    end
   end
 end
