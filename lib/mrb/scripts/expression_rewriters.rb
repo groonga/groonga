ef3521733d24a176f9bcf96e08f3522102d27451
Kouhei Sutou <kou@clear-code.com>
2016-01-11 00:12:42 +0900

    mrb: use class instance variable instead of class variable
    Because the current mruby has a bug of class variable.

--- lib/mrb/scripts/expression_rewriters.rb
+++ lib/mrb/scripts/expression_rewriters.rb
@@ -1,10 +1,10 @@
 module Groonga
   module ExpressionRewriters
-    @@rewriters = {}
+    @rewriters = {}
 
     class << self
       def register(name, rewriter_class)
-        @@rewriters[name] = rewriter_class
+        @rewriters[name] = rewriter_class
       end
 
       def classes
@@ -16,11 +16,11 @@ module Groonga
         rewriters_table.collect do |id|
           record = Record.new(rewriters_table, id)
           name = record.key
-          rewriter = @@rewriters[name]
+          rewriter = @rewriters[name]
           if rewriter.nil?
             plugin_name = record.plugin_name.value
             require plugin_name
-            rewriter = @@rewriters[name]
+            rewriter = @rewriters[name]
             raise "unknown rewriter: <#{name}>:<#{plugin_name}>" if rewriter.nil?
           end
           rewriter
