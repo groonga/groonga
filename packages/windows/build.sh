1914fc8cae182493052ba98a9ad1c0b7121f116e
Kouhei Sutou <kou@clear-code.com>
2016-01-11 15:46:35 +0900

    windows: use Debian Jessie to build Windows binaries
    Because MinGW on sid may be broken.

---
+++ packages/windows/build.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+LANG=C
+
+run()
+{
+  "$@"
+  if test $? -ne 0; then
+    echo "Failed $@"
+    exit 1
+  fi
+}
+
+run sudo dpkg --add-architecture i386
+run sudo apt update
+run sudo apt install -V -y \
+    build-essential \
+    devscripts \
+    autoconf \
+    libtool \
+    pkg-config \
+    mingw-w64 \
+    wine \
+    wine-binfmt \
+    rsync \
+    ruby
+
+run sudo gem install rake
+
+run cd /vagrant
+run rsync -avz tmp/download/ /tmp/download/
+. tmp/env.sh
+
+for architecture in ${ARCHITECTURES}; do
+  run rake build                                \
+      TMP_DIR="/tmp"                            \
+      VERSION="${VERSION}"                      \
+      SOURCE="${SOURCE}"                        \
+      DEBUG_BUILD="${DEBUG_BUILD}"              \
+      ARCHITECTURE="${architecture}"
+done
