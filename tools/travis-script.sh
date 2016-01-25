cb89e5117a390f67f5dfadf9ac67ace16a23cca7
Kouhei Sutou <kou@clear-code.com>
2016-01-25 15:42:59 +0900

    travis: run command line test

--- tools/travis-script.sh
+++ tools/travis-script.sh
@@ -20,6 +20,7 @@ case "${BUILD_TOOL}" in
     # TODO: Re-enable me on OS X
     if [ "${TRAVIS_OS_NAME}" = "linux" -a "${ENABLE_MRUBY}" = "yes" ]; then
       test/mruby/run-test.rb
+      test/command_line/run-test.rb
     fi
     test/command/run-test.sh ${command_test_options} --interface http
     mkdir -p ${prefix}/var/log/groonga/httpd
