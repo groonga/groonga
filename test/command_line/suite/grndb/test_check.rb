c4be11e4e52627c612131f05d4d8bb054f1ad957
Kouhei Sutou <kou@clear-code.com>
2016-01-25 15:10:54 +0900

    test grndb check: add a test for locked table case

--- test/command_line/suite/grndb/test_check.rb
+++ test/command_line/suite/grndb/test_check.rb
@@ -11,4 +11,15 @@ class TestGrnDBCheck < GroongaTestCase
 Database is locked. It may be broken. Re-create the database.
     MESSAGE
   end
+
+  def test_locked_table
+    groonga("table_create", "Users", "TABLE_HASH_KEY", "ShortText")
+    groonga("lock_acquire", "Users")
+    error = assert_raise(CommandRunner::Error) do
+      grndb("check")
+    end
+    assert_equal(<<-MESSAGE, error.error_output)
+[Users] Table is locked. It may be broken. (1) Truncate the table (truncate Users) or clear lock of the table (lock_clear Users) and (2) load data again.
+    MESSAGE
+  end
 end
