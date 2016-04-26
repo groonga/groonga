d64e2c5533304a9bf4b49dfcee8cda808d5b2c85
Kouhei Sutou <kou@clear-code.com>
2016-04-26 11:36:43 +0900

    Include Groonga internal header first

--- src/suggest/groonga_suggest_create_dataset.c
+++ src/suggest/groonga_suggest_create_dataset.c
@@ -15,13 +15,13 @@
   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+/* For grn_str_getopt() */
+#include <grn_str.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <groonga.h>
-
-/* For grn_str_getopt() */
-#include <grn_str.h>
 
 typedef enum {
   MODE_NONE,
