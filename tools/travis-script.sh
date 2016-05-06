6b1a97aff607591c81ce37e10971689d1fcd5d6a
Kouhei Sutou <kou@clear-code.com>
2016-05-06 15:44:36 +0900

    travis: fix a typo

--- tools/travis-script.sh
+++ tools/travis-script.sh
@@ -13,7 +13,7 @@ export COLUMNS=79
 if [ "${TRAVIS_OS_NAME}" = "osx" ]; then
   memory_fs_size=$[1024 * 1024] # 1MiB
   byte_per_sector=512
-  n_sectors=$[${memory_fs_size} / ${byte_per_sesctor}]
+  n_sectors=$[${memory_fs_size} / ${byte_per_sector}]
   memory_fs_device_path=$(hdid -nomount ram://${n_sectors})
   newfs_hfs ${memory_fs_device_path}
   mkdir -p test/command/tmp
