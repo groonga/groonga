7c0ea8c64c214857603a10fd5ad03658a465a36b
Kouhei Sutou <kou@clear-code.com>
2016-05-08 22:12:06 +0900

    travis: disable memory file system on OS X temporary
    It may be slow...

--- tools/travis-script.sh
+++ tools/travis-script.sh
@@ -13,15 +13,15 @@ set -x
 
 export COLUMNS=79
 
-if [ "${TRAVIS_OS_NAME}" = "osx" ]; then
-  memory_fs_size=$[10 * 1024 * 1024] # 10MiB
-  byte_per_sector=512
-  n_sectors=$[${memory_fs_size} / ${byte_per_sector}]
-  memory_fs_device_path=$(hdid -nomount ram://${n_sectors})
-  newfs_hfs ${memory_fs_device_path}
-  mkdir -p test/command/tmp
-  mount -t hfs ${memory_fs_device_path} test/command/tmp
-fi
+# if [ "${TRAVIS_OS_NAME}" = "osx" ]; then
+#   memory_fs_size=$[10 * 1024 * 1024] # 10MiB
+#   byte_per_sector=512
+#   n_sectors=$[${memory_fs_size} / ${byte_per_sector}]
+#   memory_fs_device_path=$(hdid -nomount ram://${n_sectors})
+#   newfs_hfs ${memory_fs_device_path}
+#   mkdir -p test/command/tmp
+#   mount -t hfs ${memory_fs_device_path} test/command/tmp
+# fi
 
 case "${BUILD_TOOL}" in
   autotools)
