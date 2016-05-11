2aec4c6983e4ae459f2b455f8c76e2b03ef20e23
Kouhei Sutou <kou@clear-code.com>
2016-05-11 11:32:02 +0900

    travis: try memory file system on OS X again

--- tools/travis-script.sh
+++ tools/travis-script.sh
@@ -13,6 +13,17 @@ set -x
 
 export COLUMNS=79
 
+if [ "${TRAVIS_OS_NAME}" = "osx" ]; then
+  memory_fs_size=$[50 * 1024 * 1024] # 50MiB
+  byte_per_sector=512
+  n_sectors=$[${memory_fs_size} / ${byte_per_sector}]
+  memory_fs_device_path=$(hdid -nomount ram://${n_sectors})
+  newfs_hfs ${memory_fs_device_path}
+  mkdir -p test/command/tmp
+  mount -t hfs ${memory_fs_device_path} test/command/tmp
+  df -h
+fi
+
 case "${BUILD_TOOL}" in
   autotools)
     # TODO: Re-enable me on OS X
