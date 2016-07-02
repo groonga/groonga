028159a2f56486569134c448e88ac64ebfbac3d1
Kouhei Sutou <kou@clear-code.com>
2016-07-02 22:16:20 +0900

    example dictionary: use double quote for string literal

--- examples/dictionary/edict/edict-import.sh
+++ examples/dictionary/edict/edict-import.sh
@@ -17,9 +17,9 @@ else
 fi
 
 if type gzcat > /dev/null 2>&1; then
-    zcat='gzcat'
+    zcat="gzcat"
 else
-    zcat='zcat'
+    zcat="zcat"
 fi
 
 if $zcat $edict_gz | ${base_dir}/edict2grn.rb | groonga $1 > /dev/null; then
