875d3d0e2658640ffbdaaf67dad6db61f0ed05af
Kouhei Sutou <kou@clear-code.com>
2016-08-26 16:53:44 +0900

    logical_table_remove: start implementing "--force yes"
    It's not completed yet.

--- plugins/sharding/logical_table_remove.rb
+++ plugins/sharding/logical_table_remove.rb
@@ -10,10 +10,12 @@ module Groonga
                  "max",
                  "max_border",
                  "dependent",
+                 "force",
                ])
 
       def run_body(input)
         @dependent = (input[:dependent] == "yes")
+        @force = (input[:force] == "yes")
 
         enumerator = LogicalEnumerator.new("logical_table_remove", input)
 
@@ -37,38 +39,52 @@ module Groonga
         end
         table = shard.table
 
+        if cover_type == :all or (table.nil? and @force)
+          remove_table(shard, table)
+          return
+        end
+
         expression_builder = RangeExpressionBuilder.new(shard_key,
                                                         target_range,
                                                         nil)
-
         case cover_type
-        when :all
-          remove_table(shard)
         when :partial_min
           remove_records(table) do |expression|
             expression_builder.build_partial_min(expression)
           end
-          remove_table(shard) if table.empty?
+          remove_table(shard, table) if table.empty?
         when :partial_max
           remove_records(table) do |expression|
             expression_builder.build_partial_max(expression)
           end
-          remove_table(shard) if table.empty?
+          remove_table(shard, table) if table.empty?
         when :partial_min_and_max
           remove_records(table) do |expression|
             expression_builder.build_partial_min_and_max(expression)
           end
-          remove_table(shard) if table.empty?
+          remove_table(shard, table) if table.empty?
         end
       end
 
-      def remove_table(shard)
-        table = shard.table
+      def remove_table(shard, table)
+        if table.nil? and @force
+          Context.instance.clear_error
+        end
+
         shard_suffix = shard.range_data.to_suffix
 
         referenced_table_ids = []
         if @dependent
-          table.columns.each do |column|
+          if table
+            columns = table.columns
+          else
+            prefix = "#{shard.table_name}."
+            column_names = context.database.each_name(:prefix => prefix)
+            columns = column_names.collect do |column_name|
+              context[column_name]
+            end
+          end
+          columns.each do |column|
             range = column.range
             if range.is_a?(Table)
               referenced_table_ids << range.id
@@ -82,9 +98,27 @@ module Groonga
           end
         end
 
-        table.remove(:dependent => @dependent)
+        if table.nil?
+          if @force
+            remove_table_force(shard)
+          else
+            message = "[logical_table_remove] table is broken: " +
+                      "<#{shard.table_name}>"
+            raise InvalidArgument, message
+          end
+        else
+          options = {:dependent => @dependent}
+          if @force
+            begin
+              table.remove(options)
+            rescue
+              remove_table_force(shard)
+            end
+          else
+            table.remove(options)
+          end
+        end
 
-        context = Groonga::Context.instance
         referenced_table_ids.each do |referenced_table_id|
           referenced_table = context[referenced_table_id]
           next if referenced_table.nil?
@@ -92,6 +126,25 @@ module Groonga
             referenced_table.remove(:dependent => @dependent)
           end
         end
+      end
+
+      def remove_table_force(shard)
+        database = context.database
+
+        prefix = "#{shard.table_name}."
+        database.each_raw(:prefix => prefix) do |id, cursor|
+          column = context[id]
+          if column.nil?
+            column_name = cursor.key
+            Object.remove_force(column_name)
+          else
+            column.remove(:dependent => @dependent)
+          end
+        end
+
+        # TODO: Remove objects that refers the table
+
+        Object.remove_force(shard.table_name)
       end
 
       def remove_records(table)
