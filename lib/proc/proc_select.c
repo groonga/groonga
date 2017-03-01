1ef28f75334e04839b4ce95e50938b8b2f5cc9d2
Kouhei Sutou <kou@clear-code.com>
2017-03-01 17:27:05 +0900

    select: use columns[LABEL](N) for query log format

--- lib/proc/proc_select.c
+++ lib/proc/proc_select.c
@@ -1391,10 +1391,10 @@ grn_select_apply_columns(grn_ctx *ctx,
     grn_obj_close(ctx, expression);
 
     GRN_QUERY_LOG(ctx, GRN_QUERY_LOG_SIZE,
-                  ":", "columns(%d)[%.*s]",
-                  grn_table_size(ctx, table),
+                  ":", "columns[%.*s](%d)",
                   (int)(column_data->label.length),
-                  column_data->label.value);
+                  column_data->label.value,
+                  grn_table_size(ctx, table));
   }
 
   grn_hash_cursor_close(ctx, columns_cursor);
