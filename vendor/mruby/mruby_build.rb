f513ec0a18579d36c9a928c0f512456a5f17dde2
Kouhei Sutou <kou@clear-code.com>
2018-04-09 17:54:19 +0900

    mruby: update file list

--- vendor/mruby/mruby_build.rb
+++ vendor/mruby/mruby_build.rb
@@ -51,11 +51,6 @@ mruby_env_dir = "#{mruby_build_dir}/mrbgems/mruby-env"
 FileUtils.mkdir_p("mruby-env/")
 FileUtils.cp_r("#{mruby_env_dir}/src/", "mruby-env/")
 
-mruby_io_dir = "#{mruby_build_dir}/mrbgems/mruby-io"
-FileUtils.mkdir_p("mruby-io/")
-FileUtils.cp_r("#{mruby_io_dir}/include/", "mruby-io/")
-FileUtils.cp_r("#{mruby_io_dir}/src/", "mruby-io/")
-
 mruby_file_stat_dir = "#{mruby_build_dir}/mrbgems/mruby-file-stat"
 FileUtils.mkdir_p("mruby-file-stat/")
 FileUtils.cp_r("#{mruby_file_stat_dir}/src/", "mruby-file-stat/")
