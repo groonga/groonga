098aed475c7eb79066f656105416a56cafff08d9
Kouhei Sutou <kou@clear-code.com>
2018-04-09 17:50:13 +0900

    mruby: use the latest mruby

--- lib/mrb.c
+++ lib/mrb.c
@@ -207,7 +207,7 @@ grn_mrb_load(grn_ctx *ctx, const char *path)
     {
       struct RProc *proc;
       proc = mrb_generate_code(mrb, parser);
-      proc->target_class = mrb->object_class;
+      MRB_PROC_SET_TARGET_CLASS(proc, mrb->object_class);
       result = mrb_toplevel_run(mrb, proc);
     }
     mrb_parser_free(parser);
