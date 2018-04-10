25bd382a9185b528cd32b4f18915fe0c38d36b43
Kouhei Sutou <kou@clear-code.com>
2018-04-10 14:44:13 +0900

    test: support running command line tests on out-of-source build

--- test/command_line/run-test.rb
+++ test/command_line/run-test.rb
@@ -4,10 +4,16 @@ require "rbconfig"
 require "pathname"
 require "fileutils"
 
-base_dir_path       = Pathname(__FILE__).expand_path.dirname
-source_top_dir_path = base_dir_path.parent.parent
-build_top_dir_path  = Pathname($0).expand_path.dirname.parent.parent
-build_base_dir_path = build_top_dir_path + "test/command_line"
+source_base_dir_path = Pathname(__dir__).expand_path
+source_top_dir_path = source_base_dir_path.parent.parent
+if ENV["BUILD_DIR"]
+  build_base_dir_path = Pathname(ENV["BUILD_DIR"]).expand_path
+else
+  build_base_dir_path  = Pathname($0).expand_path.dirname
+end
+build_top_dir_path = build_base_dir_path.parent.parent
+
+ENV["BASE_DIR"] ||= build_base_dir_path.to_s
 
 groonga_install_prefix = nil
 if (ARGV[0] || "").start_with?("--groonga-install-prefix=")
@@ -28,10 +34,18 @@ else
     (build_top_dir_path + "src").to_s,
     ENV["PATH"],
   ].join(File::PATH_SEPARATOR)
+  if build_top_dir_path != source_top_dir_path
+    Dir.glob(source_top_dir_path + "plugins/**/*.rb") do |source_rb|
+      relative_path = Pathname(source_rb).relative_path_from(source_top_dir_path)
+      build_rb = build_top_dir_path + relative_path
+      FileUtils.mkdir_p(build_rb.dirname)
+      FileUtils.cp(source_rb, build_rb)
+    end
+  end
   ENV["GRN_PLUGINS_DIR"]      = (build_top_dir_path + "plugins").to_s
-  ENV["GRN_RUBY_SCRIPTS_DIR"] = (build_top_dir_path + "lib/mrb/scripts").to_s
+  ENV["GRN_RUBY_SCRIPTS_DIR"] = (source_top_dir_path + "lib/mrb/scripts").to_s
   ENV["GRN_RUBY_LOAD_PATH"] = [
-    (build_top_dir_path + "vendor/groonga-log-source/lib").to_s,
+    (source_top_dir_path + "vendor/groonga-log-source/lib").to_s,
     ENV["GRN_RUBY_LOAD_PATH"],
   ].compact.join(File::PATH_SEPARATOR)
 end
@@ -42,4 +56,4 @@ require_relative "helper"
 
 ARGV.unshift("--max-diff-target-string-size=5000")
 
-exit(Test::Unit::AutoRunner.run(true, (base_dir_path + "suite").to_s))
+exit(Test::Unit::AutoRunner.run(true, (source_base_dir_path + "suite").to_s))
