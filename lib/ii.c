1e2648dd4d5d518376d22fbf0d9f534324c90c01
Kouhei Sutou <kou@clear-code.com>
2018-11-01 16:55:02 +0900

    ii: fix a crash bug on warning log
    c->cinfo may be NULL when the chunk isn't split.

--- lib/ii.c
+++ lib/ii.c
@@ -5472,10 +5472,12 @@ grn_ii_cursor_next_internal(grn_ctx *ctx, grn_ii_cursor *c,
                   if (decoded_size == 0) {
                     GRN_LOG(ctx, GRN_LOG_WARNING,
                             "[ii][cursor][next][chunk][last] "
-                            "chunk(%d) is changed by another thread "
-                            "while decoding: %p",
-                            c->cinfo[c->curr_chunk].segno,
-                            c);
+                            "failed to decode the last chunk: "
+                            "Another tread might change "
+                            "the chunk while decoding: <%p>: <%d>: <%d>",
+                            c,
+                            c->curr_chunk,
+                            c->cinfo ? c->cinfo[c->curr_chunk].segno : -1);
                     c->pc.rid = GRN_ID_NIL;
                     break;
                   }
