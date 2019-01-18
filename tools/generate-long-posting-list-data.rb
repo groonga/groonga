94485f1661f43fb3fe37ec8c572b220390c3e5c3
Kouhei Sutou <kou@clear-code.com>
2019-01-18 17:48:30 +0900

    Make the number of records customizable

--- tools/generate-long-posting-list-data.rb
+++ tools/generate-long-posting-list-data.rb
@@ -1,6 +1,15 @@
 #!/usr/bin/env ruby
 
 require "json"
+require "optparse"
+
+n_records = 100000
+parser = OptionParser.new
+parser.on("--n-records=N", Integer,
+          "[#{n_records}]") do |n|
+  n_records = n
+end
+parser.parse!
 
 puts(<<-COMMANDS)
 table_create Data TABLE_NO_KEY
@@ -16,9 +25,9 @@ puts(<<-LOAD)
 load --table Data
 [
 LOAD
-100000.times do
+n_records.times do
   puts({"value" => "X"}.to_json)
 end
 puts(<<-LOAD)
 ]
-LAOD
+LOAD
