68376a894f86dc8ddb14761bb4c702adc6496fd7
Kouhei Sutou <kou@clear-code.com>
2019-04-26 10:21:10 +0900

    Add a mruby test runner

---
+++ test/mruby/run-test.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+export SOURCE_DIR="`dirname $0`"
+if test -z "$BUILD_DIR"; then
+  BUILD_DIR="$SOURCE_DIR"
+fi
+export BUILD_DIR
+
+source_top_dir="$SOURCE_DIR/../.."
+source_top_dir=$(cd -P "$source_top_dir" 2>/dev/null || cd "$source_top_dir"; pwd)
+
+build_top_dir="$BUILD_DIR/../.."
+build_top_dir=$(cd -P "$build_top_dir" 2>/dev/null || cd "$build_top_dir"; pwd)
+
+. "${build_top_dir}/config.sh"
+
+case $(uname) in
+  Linux|*BSD)
+    LD_LIBRARY_PATH="$build_top_dir/lib/.libs:$LD_LIBRARY_PATH"
+    export LD_LIBRARY_PATH
+    ;;
+  Darwin)
+    DYLD_LIBRARY_PATH="$build_top_dir/lib/.libs:$DYLD_LIBRARY_PATH"
+    export DYLD_LIBRARY_PATH
+    ;;
+  *)
+    :
+    ;;
+esac
+
+if test -z "$RUBY"; then
+  exit 1
+fi
+
+"$RUBY" "${SOURCE_DIR}/run-test.rb" "$@"
