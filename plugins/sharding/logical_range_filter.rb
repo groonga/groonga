ed9c14e97250c31660d7f59afa535f2aa7437c15
Sutou Kouhei <kou@clear-code.com>
2019-06-06 17:52:56 +0900

    Fold long lines

--- plugins/sharding/logical_range_filter.rb
+++ plugins/sharding/logical_range_filter.rb
@@ -964,6 +964,8 @@ module Groonga
                 compute_max_n_unmatched_records(data_table.size,
                                                 options[:limit])
               options[:max_n_unmatched_records] = max_n_unmatched_records
+              query_log_prefix =
+                "filter(#{n_matched_records})[#{@shard.table_name}]"
               if @filter
                 create_expression(data_table) do |expression|
                   expression.parse(@filter)
@@ -972,14 +974,12 @@ module Groonga
                     n_matched_records = index_cursor.select(result_set, options)
                   end
                 end
-                query_logger.log(:size, ":",
-                                 "filter(#{n_matched_records})[#{@shard.table_name}]: #{@filter}")
+                query_logger.log(:size, ":", "#{query_log_prefix}: #{@filter}")
               else
                 IndexCursor.open(table_cursor, range_index) do |index_cursor|
                   n_matched_records = index_cursor.select(result_set, options)
                 end
-                query_logger.log(:size, ":",
-                                 "filter(#{n_matched_records})[#{@shard.table_name}]")
+                query_logger.log(:size, ":", query_log_prefix)
               end
               if n_matched_records == -1
                 result_set.close
