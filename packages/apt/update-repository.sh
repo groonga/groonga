ddb41d7066c33b89e3d01ba4c749f640a26c0f99
Sutou Kouhei <kou@clear-code.com>
2019-07-31 09:51:16 +0900

    packages apt: add support for buster

--- packages/apt/update-repository.sh
+++ packages/apt/update-repository.sh
@@ -109,7 +109,7 @@ EOF
 
 for code_name in ${CODES}; do
     case ${code_name} in
-	jessie|stretch|unstable)
+	jessie|stretch|buster|unstable)
 	    distribution=debian
 	    component=main
 	    ;;
