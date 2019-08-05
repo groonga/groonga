f4c4a4d2b2cd65ff28846e6dbd11e7972322e00e
Sutou Kouhei <kou@clear-code.com>
2019-08-05 14:54:50 +0900

    Fix indent

--- autogen.sh
+++ autogen.sh
@@ -2,25 +2,25 @@
 
 ./version-gen.sh
 
-case `uname -s` in
-Darwin)
-        homebrew_aclocal=/usr/local/share/aclocal
-        if [ -d $homebrew_aclocal ]; then
-          ACLOCAL_PATH="$ACLOCAL_PATH $homebrew_aclocal"
-        fi
-        gettext_prefix=/usr/local/Cellar/gettext
-        if [ -d $gettext_prefix ]; then
-          gettext_aclocal=$(ls $gettext_prefix/*/share/aclocal | \
-                               gsort --version-sort | \
-                               tail -n 1)
-          if [ -d $gettext_aclocal ]; then
-            ACLOCAL_PATH="$ACLOCAL_PATH $gettext_aclocal"
-          fi
-        fi
-	;;
-FreeBSD)
-	ACLOCAL_PATH="$ACLOCAL_PATH /usr/local/share/aclocal/"
-	;;
+case $(uname -s) in
+  Darwin)
+    homebrew_aclocal=/usr/local/share/aclocal
+    if [ -d $homebrew_aclocal ]; then
+      ACLOCAL_PATH="$ACLOCAL_PATH $homebrew_aclocal"
+    fi
+    gettext_prefix=/usr/local/Cellar/gettext
+    if [ -d $gettext_prefix ]; then
+      gettext_aclocal=$(ls $gettext_prefix/*/share/aclocal | \
+                          gsort --version-sort | \
+                          tail -n 1)
+      if [ -d $gettext_aclocal ]; then
+        ACLOCAL_PATH="$ACLOCAL_PATH $gettext_aclocal"
+      fi
+    fi
+    ;;
+  FreeBSD)
+    ACLOCAL_PATH="$ACLOCAL_PATH /usr/local/share/aclocal/"
+    ;;
 esac
 
 if [ ! -e vendor/mruby-source/.git ]; then
