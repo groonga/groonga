eb1064e2e9e5f6c161a2bcea973ebd3d16eabd0e
Kentaro Hayashi <hayashi@clear-code.com>
2019-08-05 09:58:16 +0900

    packages apt: drop oldoldstable

--- packages/apt/update-repository.sh
+++ packages/apt/update-repository.sh
@@ -109,7 +109,7 @@ EOF
 
 for code_name in ${CODES}; do
     case ${code_name} in
-	jessie|stretch|buster|unstable)
+	stretch|buster|unstable)
 	    distribution=debian
 	    component=main
 	    ;;
