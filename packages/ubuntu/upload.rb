6445e5ffe5f69e86f7091193d85422c42f117b88
Kentaro Hayashi <hayashi@clear-code.com>
2019-08-26 15:29:07 +0900

    packages ubuntu: make more robust archive detection
    It supports tar.xz and so on.

--- packages/ubuntu/upload.rb
+++ packages/ubuntu/upload.rb
@@ -112,10 +112,11 @@ allow_unsigned_uploads = 0
 
   def upload(ubuntu_code_name, ubuntu_version)
     in_temporary_directory do
+      archive_basename, archive_suffix = @source_archive.to_s.split(".tar.")
       FileUtils.cp(@source_archive.to_s,
-                   "#{@package}_#{@version}.orig.tar.gz")
+                   "#{@package}_#{@version}.orig.tar.#{archive_suffix}")
       run_command("tar", "xf", @source_archive.to_s)
-      directory_name = "#{@package}-#{@version}"
+      directory_name = File.basename(archive_basename)
       Dir.chdir(directory_name) do
         FileUtils.cp_r(@debian_directory.to_s, "debian")
         if ubuntu_version
