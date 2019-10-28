23d2b43772cc4c0764f19850469ccd567537eeee
Sutou Kouhei <kou@clear-code.com>
2019-10-28 16:25:16 +0900

    Use $0

--- tools/exec-index-column-diff.sh
+++ tools/exec-index-column-diff.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 if [ $# -ne 3 ]; then
-  echo \
-    "Usage: ./exec-index-column-diff.sh database_dump groonga_path database_path"
-  echo \
-    "Example: ./exec-index-column-diff.sh dump.grn /usr/local/groonga/bin/groonga ~/database/db"
+  echo "Usage: $0 database_dump groonga_path database_path"
+  echo "Example: $0 dump.grn /usr/local/groonga/bin/groonga ~/database/db"
   exit 1
 fi
 
