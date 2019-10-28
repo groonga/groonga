56dd8b57236c3263b62f400089a8586d2f53897a
Yasuhiro Horimoto <horimoto@clear-code.com>
2019-10-28 16:09:53 +0900

    Add new lines

--- tools/exec-index-column-diff.sh
+++ tools/exec-index-column-diff.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-grep -w COLUMN_INDEX $1 | awk '{ print "index_column_diff --table "$2" --name "$3 }' > index_column_diff.query
+grep -w COLUMN_INDEX $1 | \
+  awk '{ print "index_column_diff --table "$2" --name "$3 }' \
+  > index_column_diff.query
 
-$2 --log-path groonga.log --log-level debug --query-log-path query.log $3 < index_column_diff.query > index_column_diff_result.log
+$2 \
+  --log-path groonga.log \
+  --log-level debug \
+  --query-log-path query.log \
+  $3 \
+  < index_column_diff.query \
+  > index_column_diff_result.log
 
 rm index_column_diff.query
