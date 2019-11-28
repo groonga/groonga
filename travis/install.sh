9893dee03715e619d1ba45d097ba359827c67c09
Sutou Kouhei <kou@clear-code.com>
2019-11-28 12:05:49 +0900

    travis: enable Apache Arrow

--- travis/install.sh
+++ travis/install.sh
@@ -31,11 +31,20 @@ case "${TRAVIS_OS_NAME}" in
       --location \
       https://raw.github.com/clear-code/cutter/master/data/travis/setup.sh | sh
     if [ "${ENABLE_DOCUMENT}" = "yes" ]; then
-      sudo apt-get install -qq -y \
+      sudo apt install -qq -y \
            python3-pip
       sudo pip3 install setuptools
       sudo pip3 install Sphinx
     fi
+    sudo wget -O /usr/share/keyrings/apache-arrow-keyring.gpg \
+         https://apache.bintray.com/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/apache-arrow-keyring.gpg
+    sudo tee /etc/apt/sources.list.d/apache-arrow.list <<APT_LINE
+deb [arch=amd64 signed-by=/usr/share/keyrings/apache-arrow-keyring.gpg] https://dl.bintray.com/apache/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/ $(lsb_release --codename --short) main
+deb-src [signed-by=/usr/share/keyrings/apache-arrow-keyring.gpg] https://dl.bintray.com/apache/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/ $(lsb_release --codename --short) main
+APT_LINE
+    sudo apt update -qq
+    sudo apt install -qq -y \
+         libarrow-dev
     ;;
   osx)
     if [ "${ENABLE_DOCUMENT}" = "yes" ]; then
