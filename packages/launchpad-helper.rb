eed84b382442c5c8f932ef215202cf37d0d79d9a
Sutou Kouhei <kou@clear-code.com>
2019-11-28 12:09:04 +0900

    packages: add missing require

--- packages/launchpad-helper.rb
+++ packages/launchpad-helper.rb
@@ -14,6 +14,8 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
+require "pathname"
+
 require_relative "../vendor/apache-arrow-source/dev/tasks/linux-packages/package-task"
 
 module LaunchpadHelper
