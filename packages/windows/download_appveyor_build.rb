f1a258dc4766d6f74de1037a4333064dc1f592d8
Sutou Kouhei <kou@clear-code.com>
2020-04-06 16:51:23 +0900

    packages windows: improve the target build detection for master

--- packages/windows/download_appveyor_build.rb
+++ packages/windows/download_appveyor_build.rb
@@ -21,40 +21,62 @@ def download(url, local_path)
   end
 end
 
-client = Octokit::Client.new
-
-groonga_repository = "groonga/groonga"
-
 appveyor_url = "https://ci.appveyor.com/"
-appveyor_info = nil
-last_appveyor_info = nil
-client.statuses(groonga_repository, tag).each do |status|
-  next unless status.target_url.start_with?(appveyor_url)
+appveyor_account = "groonga"
+appveyor_project = "groonga"
 
-  match_data = /\/([^\/]+?)\/([^\/]+?)\/builds\/(\d+)\z/.match(status.target_url)
-  last_appveyor_info = {
-    account: match_data[1],
-    project: match_data[2],
-    build_id: match_data[3],
-  }
-  unless status.state == "pending"
-    appveyor_info = last_appveyor_info
-    break
+build_version = nil
+if tag == "master"
+  n_builds = 50
+  build_history = Veyor.project_history(account: appveyor_account,
+                                        project: appveyor_project,
+                                        limit: n_builds)
+  build_history["builds"].each do |build|
+    if build["status"] == "success"
+      build_version = build["buildNumber"]
+      break
+    end
   end
-end
-appveyor_info ||= last_appveyor_info
-if appveyor_info.nil?
-  raise "No AppVeyor build"
+  if build_version.nil?
+    raise "No AppVeyor success build in the last #{n_builds} builds"
+  end
+else
+  appveyor_info = nil
+  last_appveyor_info = nil
+  groonga_repository = "groonga/groonga"
+
+  client = Octokit::Client.new
+  client.statuses(groonga_repository, tag).each do |status|
+    pp status
+    next unless status.target_url.start_with?(appveyor_url)
+
+    match_data = /\/([^\/]+?)\/([^\/]+?)\/builds\/(\d+)\z/.match(status.target_url)
+    last_appveyor_info = {
+      account: match_data[1],
+      project: match_data[2],
+      build_id: match_data[3],
+    }
+    pp [status.state, last_appveyor_info]
+    unless status.state == "pending"
+      appveyor_info = last_appveyor_info
+      break
+    end
+  end
+  appveyor_info ||= last_appveyor_info
+  if appveyor_info.nil?
+    raise "No AppVeyor build"
+  end
+
+  start_build = appveyor_info[:build_id].to_i + 1
+  build_history = Veyor.project_history(account: appveyor_account,
+                                        project: appveyor_project,
+                                        start_build: start_build,
+                                        limit: 1)
+  build_version = build_history["builds"][0]["buildNumber"]
 end
 
-start_build = appveyor_info[:build_id].to_i + 1
-build_history = Veyor.project_history(account: appveyor_info[:account],
-                                      project: appveyor_info[:project],
-                                      start_build: start_build,
-                                      limit: 1)
-build_version = build_history["builds"][0]["buildNumber"]
-project = Veyor.project(account: appveyor_info[:account],
-                        project: appveyor_info[:project],
+project = Veyor.project(account: appveyor_account,
+                        project: appveyor_project,
                         version: build_version)
 downloaded = false
 project["build"]["jobs"].each do |job|
@@ -73,6 +95,6 @@ end
 
 unless downloaded
   build_id = project["build"]["buildId"]
-  url = "#{appveyor_url}project/#{groonga_repository}/builds/#{build_id}"
+  url = "#{appveyor_url}project/#{appveyor_account}/#{appveyor_project}/builds/#{build_id}"
   raise "No artifacts on AppVeyor: #{url}"
 end
