3dc844086d2f398ceb8f6915404e0cfe54fe9824
Horimoto Yasuhiro <horimoto@clear-code.com>
2020-04-16 17:24:45 +0900

    github-actions: add tests

--- packages/apt/test.sh
+++ packages/apt/test.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
 apt install /repositories/debian/pool/stretch/main/g/groonga/*.deb
+groonga --version
+git clone https://github.com/groonga/groonga.git
+cd groonga
+GROONGA=/usr/bin/groonga test/unit/run-test.sh
+GROONGA=/usr/bin/groonga test/command/run-test.sh
+GROONGA=/usr/bin/groonga test/command_line/run-test.sh
