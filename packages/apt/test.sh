4d736ef0c7d997acae7582005888e1157d3be1db
Horimoto Yasuhiro <horimoto@clear-code.com>
2020-04-16 17:48:45 +0900

    github-actions: fix wrong paths

--- packages/apt/test.sh
+++ packages/apt/test.sh
@@ -4,6 +4,8 @@ apt install /repositories/debian/pool/stretch/main/g/groonga/*.deb
 groonga --version
 git clone https://github.com/groonga/groonga.git
 cd groonga
-GROONGA=/usr/bin/groonga test/unit/run-test.sh
-GROONGA=/usr/bin/groonga test/command/run-test.sh
-GROONGA=/usr/bin/groonga test/command_line/run-test.sh
+sh autogen.sh
+./condifure
+GROONGA=/usr/bin/groonga /groonga/test/unit/run-test.sh
+GROONGA=/usr/bin/groonga /groonga/test/command/run-test.sh
+GROONGA=/usr/bin/groonga /groonga/test/command_line/run-test.sh
