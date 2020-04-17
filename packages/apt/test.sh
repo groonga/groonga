e54043fbc4dc535abbca5c8ed9ce57c26e0af3a5
Horimoto Yasuhiro <horimoto@clear-code.com>
2020-04-17 17:24:15 +0900

    github-ci:add a cutter

--- packages/apt/test.sh
+++ packages/apt/test.sh
@@ -3,9 +3,10 @@
 apt install /repositories/debian/pool/stretch/main/g/groonga/*.deb
 groonga --version
 git clone https://github.com/groonga/groonga.git
+git clone https://github.com/clear-code/cutter.git
 cd groonga
 sh autogen.sh
-./configure
-GROONGA=/usr/bin/groonga /groonga/test/unit/run-test.sh
+./configure --with-cutter-source-path=../cutter/cutter
 GROONGA=/usr/bin/groonga /groonga/test/command/run-test.sh
 GROONGA=/usr/bin/groonga /groonga/test/command_line/run-test.sh
+GROONGA=/usr/bin/groonga /groonga/test/unit/run-test.sh
