d678cfd8dbab119ee623a54578e0d115c0466d61
Horimoto Yasuhiro <horimoto@clear-code.com>
2020-04-22 16:19:23 +0900

    github-actions: test

--- packages/apt/test.sh
+++ packages/apt/test.sh
@@ -4,4 +4,7 @@ apt install /repositories/debian/pool/stretch/main/g/groonga/*.deb
 groonga --version
 git clone https://github.com/groonga/groonga.git
 cd groonga
+sh autogen.sh
+./configure --prefix=/tmp/local --with-ruby
+make
 GROONGA=/usr/bin/groonga /groonga/test/command/run-test.sh
