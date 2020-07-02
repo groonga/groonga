0d26c51977836da649c59a4f28c2d0795f11d430
Sutou Kouhei <kou@clear-code.com>
2020-07-02 12:37:30 +0900

    aggregator_sd: add
    Implemented by Welford's online algorithm.

--- lib/aggregators.c
+++ lib/aggregators.c
@@ -20,6 +20,8 @@
 #include "grn_aggregators.h"
 #include "grn_bulk.h"
 
+#include <math.h>
+
 static void *
 aggregator_sum_init(grn_ctx *ctx, grn_aggregator_data *data)
 {
@@ -208,6 +210,182 @@ aggregator_mean_fin(grn_ctx *ctx, grn_aggregator_data *data)
   return ctx->rc;
 }
 
+typedef struct {
+  uint32_t n_values;
+  double mean;
+  double m2;
+} aggregator_sd_value;
+
+typedef struct {
+  grn_hash *values;
+  bool unbiased;
+} aggregator_sd_data;
+
+static void *
+aggregator_sd_init(grn_ctx *ctx, grn_aggregator_data *data)
+{
+  grn_obj *args = grn_aggregator_data_get_args(ctx, data);
+  size_t n_args = GRN_PTR_VECTOR_SIZE(args);
+  if (!(1 <= n_args && n_args <= 2)) {
+    GRN_PLUGIN_ERROR(ctx,
+                     GRN_INVALID_ARGUMENT,
+                     "aggregator_sd(): wrong number of arguments "
+                     "(%" GRN_FMT_SIZE " for 1..2)",
+                     n_args);
+    return NULL;
+  }
+
+  grn_obj *target = GRN_PTR_VALUE_AT(args, 0);
+  if (!grn_obj_is_number_family_scalar_column(ctx, target)) {
+    grn_obj inspected;
+    GRN_TEXT_INIT(&inspected, 0);
+    grn_inspect_limited(ctx, &inspected, target);
+    GRN_PLUGIN_ERROR(ctx,
+                     GRN_INVALID_ARGUMENT,
+                     "aggregator_sd(): target must be numeric column: %.*s",
+                     (int)GRN_TEXT_LEN(&inspected),
+                     GRN_TEXT_VALUE(&inspected));
+    GRN_OBJ_FIN(ctx, &inspected);
+    return NULL;
+  }
+
+  bool unbiased = false;
+  if (n_args >= 2) {
+    grn_obj *options = GRN_PTR_VALUE_AT(args, 1);
+    grn_rc rc = grn_proc_options_parse(ctx,
+                                       options,
+                                       "[aggregator_sd]",
+                                       "unbiased",
+                                       GRN_PROC_OPTION_VALUE_BOOL,
+                                       &unbiased,
+                                       false);
+    if (rc != GRN_SUCCESS) {
+      return NULL;
+    }
+  }
+
+  aggregator_sd_data *sd_data = GRN_MALLOCN(aggregator_sd_data, 1);
+  if (!sd_data) {
+    GRN_PLUGIN_ERROR(ctx,
+                     GRN_NO_MEMORY_AVAILABLE,
+                     "aggregator_sd(): failed to allocate: %s",
+                     ctx->errbuf);
+    return NULL;
+  }
+
+  sd_data->values =
+    grn_hash_create(ctx,
+                    NULL,
+                    sizeof(grn_id),
+                    sizeof(aggregator_sd_value),
+                    GRN_OBJ_TABLE_HASH_KEY);
+  if (!sd_data->values) {
+    GRN_PLUGIN_ERROR(ctx,
+                     GRN_NO_MEMORY_AVAILABLE,
+                     "aggregator_sd(): failed to create hash table: %s",
+                     ctx->errbuf);
+    GRN_FREE(sd_data);
+    return NULL;
+  }
+  sd_data->unbiased = unbiased;
+
+  return sd_data;
+}
+
+static grn_rc
+aggregator_sd_update(grn_ctx *ctx, grn_aggregator_data *data)
+{
+  aggregator_sd_data *sd_data =
+    grn_aggregator_data_get_user_data(ctx, data);
+  grn_id group_id = grn_aggregator_data_get_group_id(ctx, data);
+  void *value;
+  int added;
+  grn_id value_id = grn_hash_add(ctx,
+                                 sd_data->values,
+                                 &group_id,
+                                 sizeof(grn_id),
+                                 &value,
+                                 &added);
+  if (value_id == GRN_ID_NIL) {
+    GRN_PLUGIN_ERROR(ctx,
+                     GRN_INVALID_ARGUMENT,
+                     "aggregator_sd(): failed to add ID: <%u>: %s",
+                     group_id,
+                     ctx->errbuf);
+    return ctx->rc;
+  }
+
+  aggregator_sd_value *sd_value = value;
+  if (added) {
+    sd_value->n_values = 0;
+    sd_value->mean = 0.0;
+    sd_value->m2 = 0.0;
+  }
+
+  grn_obj *args = grn_aggregator_data_get_args(ctx, data);
+  grn_obj *target = GRN_PTR_VALUE_AT(args, 0);
+  grn_obj current_value;
+  GRN_VOID_INIT(&current_value);
+  grn_id source_id = grn_aggregator_data_get_source_id(ctx, data);
+  grn_obj_get_value(ctx, target, source_id, &current_value);
+  double current_value_float = grn_bulk_get_float(ctx, &current_value);
+  GRN_OBJ_FIN(ctx, &current_value);
+
+  const double mean_previous = sd_value->mean;
+  const double m2_previous = sd_value->m2;
+  sd_value->n_values++;
+  sd_value->mean +=
+    (current_value_float - mean_previous) / sd_value->n_values;
+  /* Welfold's online algorithm */
+  sd_value->m2 =
+    m2_previous +
+    ((current_value_float - mean_previous) *
+     (current_value_float - sd_value->mean));
+
+  return ctx->rc;
+}
+
+static grn_rc
+aggregator_sd_fin(grn_ctx *ctx, grn_aggregator_data *data)
+{
+  aggregator_sd_data *sd_data =
+    grn_aggregator_data_get_user_data(ctx, data);
+
+  grn_obj *output_column = grn_aggregator_data_get_output_column(ctx, data);
+  grn_obj sd_float;
+  GRN_FLOAT_INIT(&sd_float, 0);
+  GRN_HASH_EACH_BEGIN(ctx, sd_data->values, cursor, id) {
+    void *key;
+    unsigned int key_size;
+    void *value;
+    grn_hash_cursor_get_key_value(ctx, cursor, &key, &key_size, &value);
+    grn_id group_id = *((grn_id *)key);
+    aggregator_sd_value *sd_value = value;
+    double sd;
+    if (sd_data->unbiased) {
+      if (sd_value->n_values > 1) {
+        sd = sqrt(sd_value->m2 / (sd_value->n_values - 1));
+      } else {
+        sd = NAN;
+      }
+    } else {
+      sd = sqrt(sd_value->m2 / sd_value->n_values);
+    }
+    GRN_FLOAT_SET(ctx, &sd_float, sd);
+    grn_obj_set_value(ctx,
+                      output_column,
+                      group_id,
+                      &sd_float,
+                      GRN_OBJ_SET);
+  } GRN_HASH_EACH_END(ctx, cursor);
+  GRN_OBJ_FIN(ctx, &sd_float);
+
+  grn_hash_close(ctx, sd_data->values);
+  GRN_FREE(sd_data);
+
+  return ctx->rc;
+}
+
 grn_rc
 grn_db_init_builtin_aggregators(grn_ctx *ctx)
 {
@@ -222,6 +400,12 @@ grn_db_init_builtin_aggregators(grn_ctx *ctx)
                         aggregator_mean_init,
                         aggregator_mean_update,
                         aggregator_mean_fin);
+
+  grn_aggregator_create(ctx,
+                        "aggregator_sd", -1,
+                        aggregator_sd_init,
+                        aggregator_sd_update,
+                        aggregator_sd_fin);
 
   return GRN_SUCCESS;
 }
