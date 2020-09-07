c504f8d7d430b4feeb85a77ed4ff6725a5e2a93a
Horimoto Yasuhiro <horimoto@clear-code.com>
2020-09-07 17:22:21 +0900

    Add a tool for upload packages from GitHub release

--- packages/packages-groonga-org-package-task.rb
+++ packages/packages-groonga-org-package-task.rb
@@ -1,4 +1,5 @@
 # Copyright(C) 2014-2020  Sutou Kouhei <kou@clear-code.com>
+# Copyright(C) 2020  Horimoto Yasuhiro <horimoto@clear-code.com>
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -17,15 +18,18 @@
 require_relative "../vendor/apache-arrow-source/dev/tasks/linux-packages/package-task"
 require_relative "launchpad-helper"
 require_relative "repository-helper"
+require_relative "github-release-helper"
 
 class PackagesGroongaOrgPackageTask < PackageTask
   include LaunchpadHelper
   include RepositoryHelper
+  include GitHubReleaseHelper
 
   def define
     super
     define_release_tasks
     define_ubuntu_tasks
+    define_upload_task
   end
 
   private
@@ -75,6 +79,58 @@ class PackagesGroongaOrgPackageTask < PackageTask
         desc "Release #{target_namespace} packages"
         task :release do
           release(target_namespace) if __send__("enable_#{target_namespace}?")
+        end
+      end
+      task target_namespace => ["#{target_namespace}:release"]
+    end
+  end
+
+  def download_packages(target_namespace)
+    package_archives = []
+    if target_namespace.equal?(:yum)
+      yum_targets_default.each do |target|
+        download_url = "#{release_base_url}/#{release_version}/#{target}.tar.gz"
+        package_archives << "#{target}.tar.gz"
+        sh("wget", "#{download_url}")
+      end
+    elsif target_namespace.equal?(:apt)
+      apt_targets_default.each do |target|
+        if target.end_with?("i386")
+          download_url = "#{release_base_url}/#{release_version}/#{target}.tar.gz"
+          package_archives << "#{target}.tar.gz"
+        else
+          download_url = "#{release_base_url}/#{release_version}/#{target}-amd64.tar.gz"
+          package_archives << "#{target}-amd64.tar.gz"
+        end
+        sh("wget", "#{download_url}")
+      end
+    end
+    unless package_archives.empty?
+      package_archives.each do |archive|
+        sh("tar", "-zxvf", "#{archive}")
+      end
+    end
+  end
+
+  def upload_packages(target_namespace)
+    sh("rync",
+       "-av",
+       "packages/#{target_namespace.to_s}/repositories/",
+       "#{repository_rsync_base_path}/")
+  end
+
+  def upload(target_namespace)
+    download_packages(target_namespace)
+    upload_packages(target_namespace)
+    sh("rm", "-rf", "packages")
+  end
+
+  def define_upload_task
+    [:apt, :yum].each do |target_namespace|
+      namespace target_namespace do
+        desc "Upload #{target_namespace} packages"
+        task :upload_packages do
+          upload(target_namespace) if __send__("enable_#{target_namespace}?")
         end
       end
       task target_namespace => ["#{target_namespace}:release"]
