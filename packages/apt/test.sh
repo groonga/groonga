58b39202011cd184e681b7d5a7a0c225571f9a51
Sutou Kouhei <kou@clear-code.com>
2021-03-25 06:08:12 +0900

    Don't run fragile tests

--- packages/apt/test.sh
+++ packages/apt/test.sh
@@ -61,10 +61,12 @@ export TZ=Asia/Tokyo
 
 grntest_options=()
 grntest_options+=(--base-directory=command)
-grntest_options+=(--n-retries=3)
+grntest_options+=(--n-retries=2)
 grntest_options+=(--n-workers=$(nproc))
 grntest_options+=(--reporter=mark)
 grntest_options+=(command/suite)
 grntest "${grntest_options[@]}"
-grntest "${grntest_options[@]}" --interface http
-grntest "${grntest_options[@]}" --interface http --testee groonga-httpd
+if [ "${architecture}" != "arm64" ]; then
+  grntest "${grntest_options[@]}" --interface http
+  grntest "${grntest_options[@]}" --interface http --testee groonga-httpd
+fi
