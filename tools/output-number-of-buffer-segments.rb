e2273898490c01b39621a408e7c9f18edae9cbb3
Horimoto Yasuhiro <horimoto@clear-code.com>
2021-06-03 14:53:30 +0900

    Add check for the status of command execution and stdout

--- tools/output-number-of-buffer-segments.rb
+++ tools/output-number-of-buffer-segments.rb
@@ -2,6 +2,7 @@
 
 require "open3"
 require "json"
+require "date"
 
 if ARGV.size() == 0
   puts "Usage: $0 DATABASE_PATH"
@@ -10,6 +11,14 @@ if ARGV.size() == 0
 end
 
 DATABASE_PATH = ARGV[0]
+ERROR_LOG_NAME = "output_n_buffer_segments.error"
+
+def output_error(status, stdout, stderr)
+  File.open(ERROR_LOG_NAME, 'a') do |file|
+    file.puts("#{Time.now}: #{status.inspect}, #{stdout.chomp}, #{stderr.chomp}")
+  end
+end
+
 stdout, stderr, status = Open3.capture3("groonga",
                                         DATABASE_PATH,
                                         "dump",
@@ -17,15 +26,24 @@ stdout, stderr, status = Open3.capture3("groonga",
                                         "--dump_schema", "no",
                                         "--dump_records", "no",
                                         "--dump_configs", "no")
-stdout.each_line do |line|
-  index_name = "#{line.split()[1]}.#{line.split()[2]}"
-  stdout, stderr, status = Open3.capture3("groonga",
-                                          DATABASE_PATH,
-                                          "object_inspect",
-                                          index_name)
-  unix_time_when_command_executed = JSON.parse(stdout)[0][1]
-  n_buffer_segments = JSON.parse(stdout)[1]["value"]["statistics"]["n_buffer_segments"]
-  File.open(index_name, 'a') do |file|
-    file.puts("#{unix_time_when_command_executed}, #{n_buffer_segments}")
+if (status.success?)
+  stdout.each_line do |line|
+    index_name = "#{line.split()[1]}.#{line.split()[2]}"
+    stdout, stderr, status = Open3.capture3("groonga",
+                                            DATABASE_PATH,
+                                            "object_inspect",
+                                            index_name)
+    if (status.success?)
+      unix_time_when_command_executed = JSON.parse(stdout)[0][1]
+      n_buffer_segments = JSON.parse(stdout)[1]["value"]["statistics"]["n_buffer_segments"]
+      File.open(index_name, 'a') do |file|
+        file.puts("#{unix_time_when_command_executed}, #{n_buffer_segments}")
+      end
+    else
+      output_error(status, stdout, stderr)
+      exit
+    end
   end
+else
+  output_error(status, stdout, stderr)
 end
