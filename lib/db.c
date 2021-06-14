4be0cfb0c80dbf727b1663019194f41a8c8c109a
Sutou Kouhei <kou@clear-code.com>
2021-06-14 17:43:59 +0900

    grn_table_setoperation_or: value_src may not full grn_rset_recinfo
    It may have only grn_rest_recinfo::score and
    grn_rest_recinfo::n_subrecs. The following code also touches
    grn_rset_recinfo::subrecs:
    
        grn_rset_recinfo ri_src = *((grn_rset_recinfo *)value_src)

--- lib/db.c
+++ lib/db.c
@@ -4326,9 +4326,17 @@ grn_table_setoperation_or(grn_ctx *ctx,
                                               &added);
       if (id_dest != GRN_ID_NIL) {
         if (added) {
-          grn_rset_recinfo ri_src = *((grn_rset_recinfo *)value_src);
-          ri_src.score *= data->weight_factor;
-          grn_memcpy(value_dest, &ri_src, data->value_size);
+          /* grn_rset_recinfo::score is double */
+          if (data->value_size >= sizeof(double)) {
+            grn_rset_recinfo *ri_src = value_src;
+            double score = ri_src->score * data->weight_factor;
+            grn_memcpy(value_dest, &score, sizeof(double));
+            grn_memcpy(((uint8_t *)value_dest) + sizeof(double),
+                       ((uint8_t *)value_src) + sizeof(double),
+                       data->value_size - sizeof(double));
+          } else {
+            grn_memcpy(value_dest, value_src, data->value_size);
+          }
         } else {
           grn_rset_recinfo *ri_dest = value_dest;
           grn_rset_recinfo *ri_src = value_src;
