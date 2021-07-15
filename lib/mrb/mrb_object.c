cdca52618f6e20903034a049510be907a63a4efb
Horimoto Yasuhiro <horimoto@clear-code.com>
2021-07-15 10:39:29 +0900

    mrb: add Groonga::Object#push/pop_open_space
    There are used closing object in database.

--- lib/mrb/mrb_object.c
+++ lib/mrb/mrb_object.c
@@ -255,6 +255,32 @@ grn_mrb_object_is_closed(mrb_state *mrb, mrb_value self)
 }
 
 mrb_value
+grn_mrb_object_push_open_space(mrb_state *mrb, mrb_value self)
+{
+  grn_ctx *ctx = (grn_ctx *)mrb->ud;
+  grn_bool is_close_opened_object_mode = (grn_thread_get_limit() == 1);
+
+  if (is_close_opened_object_mode) {
+    grn_ctx_push_temporary_open_space(ctx);
+  }
+
+  return mrb_nil_value();
+}
+
+mrb_value
+grn_mrb_object_pop_open_space(mrb_state *mrb, mrb_value self)
+{
+  grn_ctx *ctx = (grn_ctx *)mrb->ud;
+  grn_bool is_close_opened_object_mode = (grn_thread_get_limit() == 1);
+
+  if (is_close_opened_object_mode) {
+    grn_ctx_pop_temporary_open_space(ctx);
+  }
+
+  return mrb_nil_value();
+}
+
+mrb_value
 grn_mrb_object_get_domain_id(mrb_state *mrb, mrb_value self)
 {
   grn_obj *object;
@@ -371,6 +397,11 @@ grn_mrb_object_init(grn_ctx *ctx)
   mrb_define_method(mrb, klass, "remove", object_remove, MRB_ARGS_OPT(1));
   mrb_define_method(mrb, klass, "closed?",
                     grn_mrb_object_is_closed, MRB_ARGS_NONE());
+
+  mrb_define_method(mrb, klass, "push_open_space",
+                    grn_mrb_object_push_open_space, MRB_ARGS_NONE());
+  mrb_define_method(mrb, klass, "pop_open_space",
+                    grn_mrb_object_pop_open_space, MRB_ARGS_NONE());
 
   mrb_define_method(mrb, klass, "domain_id", grn_mrb_object_get_domain_id,
                     MRB_ARGS_NONE());
