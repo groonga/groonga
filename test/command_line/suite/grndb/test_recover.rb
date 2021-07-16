c921fb0901168aec00aa15d57fa04e2eb52caf91
Horimoto Yasuhiro <horimoto@clear-code.com>
2021-07-16 12:36:51 +0900

    test grndb recover: add tests for "use_temporary_open_space" option

--- test/command_line/suite/grndb/test_recover.rb
+++ test/command_line/suite/grndb/test_recover.rb
@@ -179,7 +179,7 @@ object corrupt: <#{recover_error_message}>(-55)
       FileUtils.touch(additional_path)
       result = grndb("recover",
                      "--force-truncate",
-                     "--log-level", "info")
+                     "--log-level", "dump")
       message = <<-MESSAGE
 [Users] Truncated broken object: <#{@table_path}>
 [Users] Removed broken object related file: <#{additional_path}>
@@ -187,14 +187,20 @@ object corrupt: <#{recover_error_message}>(-55)
       assert_equal([
                      "",
                      message,
-                     expected_groonga_log("info", <<-MESSAGES),
+                     expected_groonga_log("dump", <<-MESSAGES),
 |i| Recovering database: <#{@database_path}>
+|-| [io][open] <#{@table_path}>
+|-| [io][close] <#{@table_path}>
 #{windows? ? "|i| [io][open] open existing file: <#{@table_path}>" : ""}
 |i| [io][remove] removed path: <#{@table_path}>
+|-| [io][open] <#{@table_path}>
 #{windows? ? "|i| [io][open] create new file: <#{@table_path}>" : ""}
 #{prepend_tag("|i| ", message).chomp}
+|-| [io][close] <#{@table_path}>
+|-| [io][open] <#{@table_path}>
 #{windows? ? "|i| [io][open] open existing file: <#{@table_path}>" : ""}
 |i| Recovered database: <#{@database_path}>
+|-| [io][close] <#{@table_path}>
                      MESSAGES
                    ],
                    [
@@ -365,16 +371,20 @@ object corrupt: <#{recover_error_message}>(-55)
     _id, _name, path, *_ = JSON.parse(groonga("table_list").output)[1][1]
 
     remove_groonga_log
-    result = grndb("recover", "--force-lock-clear", "--log-level", "info")
+    result = grndb("recover", "--force-lock-clear", "--log-level", "dump")
     assert_equal([
                    "",
                    "",
-                   expected_groonga_log("info", <<-MESSAGES),
+                   expected_groonga_log("dump", <<-MESSAGES),
 |i| Recovering database: <#{@database_path}>
+|-| [io][open] <#{path}>
 #{windows? ? "|i| [io][open] open existing file: <#{path}>" : ""}
 |i| [Users] Clear locked object: <#{path}>
+|-| [io][close] <#{path}>
+|-| [io][open] <#{path}>
 #{windows? ? "|i| [io][open] open existing file: <#{path}>" : ""}
 |i| Recovered database: <#{@database_path}>
+|-| [io][close] <#{path}>
                    MESSAGES
                  ],
                  [
