groonga/groonga

commit 1a184946c927d0d1384e9194421d3a968451cb37

Optimize nested index search
This uses existing records in the search target result set with AND
instead of empty result set with OR. This will reduce search range and
improve performance.

This approach is always used in grn_table_selector_select() for
SCAN_PUSH and OR.

lib/accessor.c
@@ -1,6 +1,6 @@
 /*
   Copyright(C) 2018  Brazil
-  Copyright(C) 2019-2020  Sutou Kouhei <kou@clear-code.com>
+  Copyright(C) 2019-2022  Sutou Kouhei <kou@clear-code.com>
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
@@ -591,21 +591,28 @@ grn_accessor_execute(grn_ctx *ctx,
   }
 
   int depth = 0;
-  grn_accessor *a;
-  for (a = (grn_accessor *)accessor; a->next; a = a->next) {
-    depth++;
+  grn_accessor *last_a = NULL;
+  grn_accessor *second_to_last_a = NULL;
+  {
+    grn_accessor *a;
+    for (last_a = a = (grn_accessor *)accessor;
+         a->next;
+         last_a = a = a->next) {
+      depth++;
+      second_to_last_a = a;
+    }
   }
 
   grn_index_datum index_data;
   unsigned int n_index_datum;
   grn_obj *index;
   n_index_datum = grn_column_find_index_data(ctx,
-                                             a->obj,
+                                             last_a->obj,
                                              execute_op,
                                              &index_data,
                                              1);
   if (n_index_datum == 0) {
-    index = (grn_obj *)a;
+    index = (grn_obj *)last_a;
   } else {
     index = index_data.index;
   }
@@ -620,22 +627,44 @@ grn_accessor_execute(grn_ctx *ctx,
                  execute_data);
   } else {
     grn_obj *base_table = NULL;
-    if (grn_obj_is_table(ctx, a->obj)) {
-      base_table = a->obj;
+    if (grn_obj_is_table(ctx, last_a->obj)) {
+      base_table = last_a->obj;
     } else {
-      base_table = grn_ctx_at(ctx, a->obj->header.domain);
+      base_table = grn_ctx_at(ctx, last_a->obj->header.domain);
     }
 
     grn_obj *base_res =
       grn_table_create(ctx, NULL, 0, NULL,
                        GRN_OBJ_TABLE_HASH_KEY|GRN_OBJ_WITH_SUBREC,
                        base_table, NULL);
+    grn_operator base_logical_op = GRN_OP_OR;
+    if (second_to_last_a &&
+        grn_obj_is_result_set(ctx, second_to_last_a->obj) &&
+        second_to_last_a->obj->header.domain == base_res->header.domain) {
+      GRN_LOG(ctx, GRN_REPORT_INDEX_LOG_LEVEL,
+              "[accessor][execute][initial] <%u>",
+              grn_table_size(ctx, second_to_last_a->obj));
+      GRN_TABLE_EACH_BEGIN(ctx, second_to_last_a->obj, cursor, id) {
+        void *key;
+        uint32_t key_size = grn_table_cursor_get_key(ctx, cursor, &key);
+        grn_hash_add(ctx,
+                     (grn_hash *)base_res,
+                     key,
+                     key_size,
+                     NULL,
+                     NULL);
+      } GRN_TABLE_EACH_END(ctx, cursor);
+      base_logical_op = GRN_OP_AND;
+    }
+    int original_flags = ctx->flags;
+    ctx->flags &= ~GRN_CTX_TEMPORARY_DISABLE_II_RESOLVE_SEL_AND;
     rc = execute(ctx,
                  index,
                  execute_op,
                  base_res,
-                 GRN_OP_OR,
+                 base_logical_op,
                  execute_data);
+    ctx->flags = original_flags;
     if (rc == GRN_SUCCESS) {
       rc = grn_accessor_resolve(ctx,
                                 accessor,

test/command/suite/select/column/stage/initial/index.expected
@@ -75,6 +75,7 @@ select Items   --column[price_with_tax].stage initial   --column[price_with_tax]
     ]
   ]
 ]
+#|i| [accessor][execute][initial] <6>
 #|i| [table-selector][select][index][range] <Prices.item_index>
 #|i| [accessor][resolve][table] (temporary) -> <Items> -> <ShortText>
 log_level --level notice

test/command/suite/select/columns/stage/initial/index.expected
@@ -75,6 +75,7 @@ select Items   --columns[price_with_tax].stage initial   --columns[price_with_ta
     ]
   ]
 ]
+#|i| [accessor][execute][initial] <6>
 #|i| [table-selector][select][index][range] <Prices.item_index>
 #|i| [accessor][resolve][table] (temporary) -> <Items> -> <ShortText>
 log_level --level notice

test/command/suite/select/function/query/options/max_n_enough_filtered_records/slice.expected
@@ -1,0 +1,96 @@
+table_create Products TABLE_NO_KEY
+[[0,0.0,0.0],true]
+column_create Products name COLUMN_SCALAR ShortText
+[[0,0.0,0.0],true]
+table_create Terms TABLE_PAT_KEY ShortText --normalizer NormalizerAuto
+[[0,0.0,0.0],true]
+column_create Terms products_name COLUMN_INDEX Products name
+[[0,0.0,0.0],true]
+load --table Products
+[
+["name"],
+["Groonga"],
+["Mroonga"],
+["Rroonga"],
+["PGroonga"],
+["Ruby"],
+["PostgreSQL"]
+]
+[[0,0.0,0.0],6]
+log_level --level info
+[[0,0.0,0.0],true]
+select   --table Products   --filter 'name @ "r"'   --slices[slice].filter 'query("name", "name:Ruby", {"max_n_enough_filtered_records": 10})'
+[
+  [
+    0,
+    0.0,
+    0.0
+  ],
+  [
+    [
+      [
+        2
+      ],
+      [
+        [
+          "_id",
+          "UInt32"
+        ],
+        [
+          "name",
+          "ShortText"
+        ]
+      ],
+      [
+        3,
+        "Rroonga"
+      ],
+      [
+        5,
+        "Ruby"
+      ]
+    ],
+    {
+      "slice": [
+        [
+          1
+        ],
+        [
+          [
+            "_id",
+            "UInt32"
+          ],
+          [
+            "name",
+            "ShortText"
+          ]
+        ],
+        [
+          5,
+          "Ruby"
+        ]
+      ]
+    }
+  ]
+]
+#|i| [object][search][index][key][exact] <Terms.products_name>
+#|i| grn_ii_sel > (r)
+#|i| exact: 0
+#|i| [ii][select] n=1 (r)
+#|i| unsplit: 2
+#|i| hits=2
+#|i| [table-selector][select][index][selector][no-index][query] <>
+#|i| [accessor][execute][initial] <2>
+#|i| [table-selector][select][index-not-used][equal] <Terms.products_name>: enough filtered: 33.33%(2/6) < 100.00% && 2 <= 10
+#>select --filter "name @ \"r\"" --slices[slice].filter "query(\"name\", \"name:Ruby\", {\"max_n_enough_filtered_records\": 10})" --table "Products"
+#:000000000000000 filter(2): Products.name match "r"
+#:000000000000000 select(2)
+#:000000000000000 filter(1): #<accessor _key(anonymous).name(Products)> equal "Ruby"
+#:000000000000000 slices[slice].filter(1): query("name", "name:Ruby", (nil))
+#:000000000000000 slices[slice](1)
+#:000000000000000 output(2)
+#:000000000000000 slices[slice].output(1)
+#:000000000000000 send(0)
+#<000000000000000 rc=0
+log_level --level notice
+[[0,0.0,0.0],true]

test/command/suite/select/function/query/options/max_n_enough_filtered_records/slice.test
@@ -1,0 +1,31 @@
+#$GRN_TABLE_SELECT_ENOUGH_FILTERED_RATIO=1.0
+
+table_create Products TABLE_NO_KEY
+column_create Products name COLUMN_SCALAR ShortText
+
+table_create Terms TABLE_PAT_KEY ShortText --normalizer NormalizerAuto
+column_create Terms products_name COLUMN_INDEX Products name
+
+load --table Products
+[
+["name"],
+["Groonga"],
+["Mroonga"],
+["Rroonga"],
+["PGroonga"],
+["Ruby"],
+["PostgreSQL"]
+]
+
+#@add-important-log-levels info
+#@add-ignore-log-pattern /\A\[io\]/
+log_level --level info
+#@collect-query-log true
+select \
+  --table Products \
+  --filter 'name @ "r"' \
+  --slices[slice].filter 'query("name", "name:Ruby", {"max_n_enough_filtered_records": 10})'
+#@collect-query-log false
+log_level --level notice
+#@remove-ignore-log-pattern /\A\[io\]/
+#@remove-important-log-levels info

test/command/suite/select/slices/filtered/equal.expected
@@ -80,6 +80,7 @@ select Users   --filter '_id > 1'   --slices[name].filter 'name == "bob"'   --sl
   ]
 ]
 #|i| [table-selector][select][index][range][id] <Users>
+#|i| [accessor][execute][initial] <3>
 #|i| [table-selector][select][index][equal] <Names.users_name>
 #|i| [accessor][resolve][table] (temporary) -> <Users>
 log_level --level notice

commit d89f38e5e439b08a99e27b973b3adb89fa04aa06

grn_accessor_execute: fix a bug that returns wrong result
We can't use the optimization introduced by
1a184946c927d0d1384e9194421d3a968451cb37 with GRN_OP_NOT_EQUAL.

lib/accessor.c
@@ -641,7 +641,8 @@ grn_accessor_execute(grn_ctx *ctx,
                        GRN_OBJ_TABLE_HASH_KEY|GRN_OBJ_WITH_SUBREC,
                        base_table, NULL);
     grn_operator base_logical_op = GRN_OP_OR;
-    if (second_to_last_a &&
+    if (execute_op != GRN_OP_NOT_EQUAL &&
+        second_to_last_a &&
         grn_obj_is_result_set(ctx, second_to_last_a->obj) &&
         second_to_last_a->obj->header.domain == base_res->header.domain) {
       GRN_LOG(ctx, GRN_REPORT_INDEX_LOG_LEVEL,

test/command/suite/select/drilldowns/filter/index/not_equal/and.expected
@@ -1,0 +1,104 @@
+table_create Tags TABLE_PAT_KEY ShortText
+[[0,0.0,0.0],true]
+column_create Tags is_stop_word COLUMN_SCALAR Bool
+[[0,0.0,0.0],true]
+table_create Booleans TABLE_PAT_KEY Bool
+[[0,0.0,0.0],true]
+column_create Booleans index COLUMN_INDEX Tags is_stop_word
+[[0,0.0,0.0],true]
+table_create Memos TABLE_HASH_KEY ShortText
+[[0,0.0,0.0],true]
+column_create Memos tag COLUMN_SCALAR Tags
+[[0,0.0,0.0],true]
+load --table Memos
+[
+{"_key": "Groonga is fast!", "tag": "Groonga"},
+{"_key": "Mroonga is fast!", "tag": "Mroonga"},
+{"_key": "Groonga sticker!", "tag": "Groonga"},
+{"_key": "Rroonga is fast!", "tag": "Rroonga"}
+]
+[[0,0.0,0.0],4]
+load --table Tags
+[
+{"_key": "Mroonga", "is_stop_word": true}
+]
+[[0,0.0,0.0],1]
+log_level --level info
+[[0,0.0,0.0],true]
+select Memos   --drilldowns[tag].keys tag   --drilldowns[tag].filter '_key != "Groonga" && is_stop_word != true'   --drilldowns[tag].output_columns _key,_nsubrecs,is_stop_word
+[
+  [
+    0,
+    0.0,
+    0.0
+  ],
+  [
+    [
+      [
+        4
+      ],
+      [
+        [
+          "_id",
+          "UInt32"
+        ],
+        [
+          "_key",
+          "ShortText"
+        ],
+        [
+          "tag",
+          "Tags"
+        ]
+      ],
+      [
+        1,
+        "Groonga is fast!",
+        "Groonga"
+      ],
+      [
+        2,
+        "Mroonga is fast!",
+        "Mroonga"
+      ],
+      [
+        3,
+        "Groonga sticker!",
+        "Groonga"
+      ],
+      [
+        4,
+        "Rroonga is fast!",
+        "Rroonga"
+      ]
+    ],
+    {
+      "tag": [
+        [
+          1
+        ],
+        [
+          [
+            "_key",
+            "ShortText"
+          ],
+          [
+            "_nsubrecs",
+            "Int32"
+          ],
+          [
+            "is_stop_word",
+            "Bool"
+          ]
+        ],
+        [
+          "Rroonga",
+          1,
+          false
+        ]
+      ]
+    }
+  ]
+]
+log_level --level notice
+[[0,0.0,0.0],true]

test/command/suite/select/drilldowns/filter/index/not_equal/and.test
@@ -1,0 +1,32 @@
+table_create Tags TABLE_PAT_KEY ShortText
+column_create Tags is_stop_word COLUMN_SCALAR Bool
+
+table_create Booleans TABLE_PAT_KEY Bool
+column_create Booleans index COLUMN_INDEX Tags is_stop_word
+
+table_create Memos TABLE_HASH_KEY ShortText
+column_create Memos tag COLUMN_SCALAR Tags
+
+load --table Memos
+[
+{"_key": "Groonga is fast!", "tag": "Groonga"},
+{"_key": "Mroonga is fast!", "tag": "Mroonga"},
+{"_key": "Groonga sticker!", "tag": "Groonga"},
+{"_key": "Rroonga is fast!", "tag": "Rroonga"}
+]
+
+load --table Tags
+[
+{"_key": "Mroonga", "is_stop_word": true}
+]
+
+log_level --level info
+#@add-important-log-levels info
+#@add-ignore-log-pattern /\A\[io\]/
+select Memos \
+  --drilldowns[tag].keys tag \
+  --drilldowns[tag].filter '_key != "Groonga" && is_stop_word != true' \
+  --drilldowns[tag].output_columns _key,_nsubrecs,is_stop_word
+#@remove-ignore-log-pattern /\A\[io\]/
+#@remove-important-log-levels info
+log_level --level notice