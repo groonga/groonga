49dc06b00163fe5c9da68038c5e17ed59064b4cf
Sutou Kouhei <kou@clear-code.com>
2022-11-17 14:32:13 +0900

    ii: use grn_obj_unref()

--- lib/ii.c
+++ lib/ii.c
@@ -6141,9 +6141,7 @@ grn_ii_close(grn_ctx *ctx, grn_ii *ii)
       rc = sub_rc;
     }
   }
-  if (grn_enable_reference_count) {
-    grn_obj_unlink(ctx, ii->lexicon);
-  }
+  grn_obj_unref(ctx, ii->lexicon);
   GRN_FREE(ii);
   /*
   {
