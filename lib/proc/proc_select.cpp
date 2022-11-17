77033c823a293f051b28ed30818b81737eb81f5f
Sutou Kouhei <kou@clear-code.com>
2022-11-17 14:25:20 +0900

    select: use grn_obj_unref()

--- lib/proc/proc_select.cpp
+++ lib/proc/proc_select.cpp
@@ -3743,9 +3743,7 @@ grn_select_apply_adjuster_execute_adjust(grn_ctx *ctx,
     grn_obj_search(ctx, index, value, table, GRN_OP_ADJUST, &options);
   }
 
-  if (grn_enable_reference_count) {
-    grn_obj_unlink(ctx, index);
-  }
+  grn_obj_unref(ctx, index);
 }
 
 static void
