36de421433279e290bbc576a3de2f70c35eda9d3
Sutou Kouhei <kou@clear-code.com>
2023-01-06 16:21:19 +0900

    select drilldowns.key_vector_expansion: power_set -> POWER_SET
    "power_set" is deprecated. Use "POWER_SET" instead.

--- lib/proc/proc_select.cpp
+++ lib/proc/proc_select.cpp
@@ -1967,7 +1967,10 @@ namespace {
         result.flags |= calc_types;
       }
 
-      if (GRN_RAW_STRING_EQUAL_CSTRING(key_vector_expansion, "power_set")) {
+      /* "power_set" is only for backward compatibility. It's deprecated.
+       * Use "POWER_SET" instead. */
+      if (GRN_RAW_STRING_EQUAL_CSTRING(key_vector_expansion, "power_set") ||
+          GRN_RAW_STRING_EQUAL_CSTRING(key_vector_expansion, "POWER_SET")) {
         result.flags |= GRN_TABLE_GROUP_KEY_VECTOR_EXPANSION_POWER_SET;
       }
 
