104575b371ff19a21fc6509a2bc0542e840dbe2f
Takashi Hashida <hashida@clear-code.com>
2023-01-17 16:27:23 +0900

    Add generators for Facebook and Twitter Make parametes script options

--- doc/create-announcement-article.rb
+++ doc/create-announcement-article.rb
@@ -1,7 +1,4 @@
-release_date = "2023-01-07"
-groonga_previous_version = "12.1.0"
-groonga_version = "12.1.1"
-groonga_org_repository = "/home/t-hashida/gitdir/groonga.org"
+require 'optparse'
 
 class ArticleGenerator
   attr_reader :output_file_path
@@ -10,12 +7,19 @@ class ArticleGenerator
     @groonga_version = groonga_version
     @groonga_previous_version = groonga_previous_version
     @groonga_version_in_link = groonga_version.gsub(".", "-")
+    @release_date = release_date
+    @release_date_in_link = release_date.gsub("-", "/")
   end
 
   def get_article
     template = get_template
     changes = get_changes
-    template % {groonga_version:@groonga_version, groonga_version_in_link:@groonga_version_in_link, changes: changes }
+    template % {
+        groonga_version:@groonga_version, 
+        groonga_version_in_link:@groonga_version_in_link, 
+        changes: changes,
+        link_prefix: @link_prefix
+      }
   end
 
   def enumerate_changes
@@ -48,260 +52,352 @@ class ArticleGenerator
   end
 end
 
-class EnArticleGenerator < ArticleGenerator
+class MarkdownArticleGenerator < ArticleGenerator
+  def get_changes
+    changes = super
+    changes.gsub(/(\[.+?\])\((.+?).md(.*?\))/, "\\1(#{@link_prefix}/\\2.html\\3")
+  end
+end
+
+class MarkdownEnArticleGenerator < MarkdownArticleGenerator
   def initialize(release_date, groonga_version, groonga_previous_version, groonga_org_repository)
     super(release_date, groonga_version, groonga_previous_version, groonga_org_repository)
     @input_file_path = "./locale/en/markdown/news.md"
     @start_headline = "## Release #{groonga_version}"
     @finish_headline = "## Release #{groonga_previous_version}"
   end
+
+  def get_template
+    <<-"TEMPLATE"
+## Groonga %<groonga_version>s has been released
+
+[Groonga %<groonga_version>s](%<link_prefix>s/news.html#release-%<groonga_version_in_link>s) has been released!
+
+How to install: [Install](%<link_prefix>s/install.html)
+
+### Changes
+
+Here are important changes in this release:
+
+%<changes>s
+
+### Known Issues
+
+* Currently, Groonga has a bug that there is possible that data is corrupt when we execute many additions, delete, and update data to vector column.
+
+* `*<` and `*>` only valid when we use `query()` the right side of filter condition.
+
+  If we specify as below, `*<` and `*>` work as `&&`.
+
+  * `'content @ "Groonga" *< content @ "Mroonga"'`
+
+* Groonga may not return records that should match caused by `GRN_II_CURSOR_SET_MIN_ENABLE`.
+
+### Conclusion
+
+Please refert to the following news for more details.
+
+[News Release %<groonga_version>s](%<link_prefix>s/news.html#release-%<groonga_version_in_link>s)
+
+Let's search by Groonga!
+    TEMPLATE
+  end
 end
 
-class JaArticleGenerator < ArticleGenerator
+class MarkdownJaArticleGenerator < MarkdownArticleGenerator
   def initialize(release_date, groonga_version, groonga_previous_version, groonga_org_repository)
     super(release_date, groonga_version, groonga_previous_version, groonga_org_repository)
     @input_file_path = "./locale/ja/markdown/news.md"
     @start_headline = "## #{groonga_version}"
     @finish_headline = "## #{groonga_previous_version}"
   end
-end
-
-class BlogEnArticleGenerator < EnArticleGenerator
-  def initialize(release_date, groonga_version, groonga_previous_version, groonga_org_repository)
-    super(release_date, groonga_version, groonga_previous_version, groonga_org_repository)
-    @output_file_path = "#{groonga_org_repository}/en/_posts/#{release_date}-groonga-#{groonga_version}.md"
-  end
-
-  def get_changes
-    changes = super
-    changes.gsub(/(\[.+?\])\((.+?).md(.*?\))/, "\\1(/docs/\\2.html\\3")
-  end
 
   def get_template
     <<-"TEMPLATE"
+## Groonga %<groonga_version>sリリース
+
+[Groonga %<groonga_version>s](%<link_prefix>s/news.html#release-%<groonga_version_in_link>s)をリリースしました！
+
+それぞれの環境毎のインストール方法: [インストール](%<link_prefix>s/install.html)
+
+### 変更内容
+    
+主な変更点は以下の通りです。
+
+%<changes>s
+
+### 既知の問題
+
+* 現在Groongaには、ベクターカラムに対してデータを大量に追加、削除、更新した際にデータが破損することがある問題があります。
+
+* `*<` と `*>` は、filter条件の右辺に `query()` を使う時のみ有効です。もし、以下のように指定した場合、 `*<` と `*>` は `&&` として機能します。
+
+  ``
+  'content @ "Groonga" *< content @ "Mroonga"'
+  ``
+
+* `GRN_II_CURSOR_SET_MIN_ENABLE` が原因でマッチするはずのレコードを返さないことがあります。
+
+### さいごに
+
+詳細については、以下のお知らせ、リリース自慢会も参照してください。
+
+[お知らせ %<groonga_version>sリリース](%<link_prefix>s/news.html#release-%<groonga_version_in_link>s)
+
+[リリース自慢会](https://www.youtube.com/playlist?list=PLKb0MEIU7gvRxTDecELqAOzOsa21dSwtU)
+
+リリース自慢会は、リリースノートを見ながら、Groongaの新機能・バグ修正を開発者が自慢する会です。
+
+毎月リリースされている最新バージョンについて、開発者的に「これ！」という機能や、「ここをがんばった！」ということを紹介しています。
+Liveチャットでコメントも受け付けていますので、気になっていることを質問したり、気になっていたあの機能について聞いたりすることも可能です。
+
+それでは、Groongaでガンガン検索してください！
+    TEMPLATE
+  end
+end
+
+class BlogEnArticleGenerator < MarkdownEnArticleGenerator
+  def initialize(release_date, groonga_version, groonga_previous_version, groonga_org_repository)
+    super(release_date, groonga_version, groonga_previous_version, groonga_org_repository)
+    @output_file_path = "#{groonga_org_repository}/en/_posts/#{release_date}-groonga-#{groonga_version}.md"
+    @link_prefix = "/docs"
+  end
+
+  def get_template
+    template_base = super
+    prefix = <<"PREFIX"
 ---
 layout: post.en
 title: Groonga %<groonga_version>s has been released
 description: Groonga %<groonga_version>s has been released!
 ---
 
-## Groonga %<groonga_version>s has been released
+PREFIX
 
-[Groonga %<groonga_version>s](/docs/news.html#release-%<groonga_version_in_link>s) has been released!
-
-How to install: [Install](/docs/install.html)
-
-### Changes
-
-Here are important changes in this release:
-
-%<changes>s
-
-### Known Issues
-
-* Currently, Groonga has a bug that there is possible that data is corrupt when we execute many additions, delete, and update data to vector column.
-
-* `*<` and `*>` only valid when we use `query()` the right side of filter condition.
-
-  If we specify as below, `*<` and `*>` work as `&&`.
-
-  * `'content @ "Groonga" *< content @ "Mroonga"'`
-
-* Groonga may not return records that should match caused by `GRN_II_CURSOR_SET_MIN_ENABLE`.
-
-### Conclusion
-
-Please refert to the following news for more details.
-
-[News Release %<groonga_version>s](/docs/news.html#release-%<groonga_version_in_link>s)
-
-Let's search by Groonga!
-    TEMPLATE
+    prefix + template_base
   end
 end
 
-class BlogJaArticleGenerator < JaArticleGenerator
+class BlogJaArticleGenerator < MarkdownJaArticleGenerator
   def initialize(release_date, groonga_version, groonga_previous_version, groonga_org_repository)
     super(release_date, groonga_version, groonga_previous_version, groonga_org_repository)
     @output_file_path = "#{groonga_org_repository}/ja/_posts/#{release_date}-groonga-#{groonga_version}.md"
-  end
-
-  def get_changes
-    changes = super
-    changes.gsub(/(\[.+?\])\((.+?).md(.*?\))/, "\\1(/ja/docs/\\2.html\\3")
+    @link_prefix = "/ja/docs"
   end
 
   def get_template
-    <<-"TEMPLATE"
+    template_base = super
+    prefix = <<"PREFIX"
 ---
 layout: post.ja
 title: Groonga %<groonga_version>sリリース
 description: Groonga %<groonga_version>sをリリースしました！
 ---
 
-## Groonga %<groonga_version>sリリース
+PREFIX
 
-[Groonga %<groonga_version>s](/ja/docs/news.html#release-%<groonga_version_in_link>s)をリリースしました！
-
-それぞれの環境毎のインストール方法: [インストール](/ja/docs/install.html)
-
-### 変更内容
-    
-主な変更点は以下の通りです。
-
-%<changes>s
-
-### 既知の問題
-
-* 現在Groongaには、ベクターカラムに対してデータを大量に追加、削除、更新した際にデータが破損することがある問題があります。
-
-* `*<` と `*>` は、filter条件の右辺に `query()` を使う時のみ有効です。もし、以下のように指定した場合、 `*<` と `*>` は `&&` として機能します。
-
-  ``
-  'content @ "Groonga" *< content @ "Mroonga"'
-  ``
-
-* `GRN_II_CURSOR_SET_MIN_ENABLE` が原因でマッチするはずのレコードを返さないことがあります。
-
-### さいごに
-
-詳細については、以下のお知らせ、リリース自慢会も参照してください。
-
-[お知らせ %<groonga_version>sリリース](/ja/docs/news.html#release-%<groonga_version_in_link>s)
-
-[リリース自慢会](https://www.youtube.com/playlist?list=PLKb0MEIU7gvRxTDecELqAOzOsa21dSwtU)
-
-リリース自慢会は、リリースノートを見ながら、Groongaの新機能・バグ修正を開発者が自慢する会です。
-
-毎月リリースされている最新バージョンについて、開発者的に「これ！」という機能や、「ここをがんばった！」ということを紹介しています。
-Liveチャットでコメントも受け付けていますので、気になっていることを質問したり、気になっていたあの機能について聞いたりすることも可能です。
-
-それでは、Groongaでガンガン検索してください！
-    TEMPLATE
+    prefix + template_base
   end
 end
 
-class DiscussionsEnArticleGenerator < EnArticleGenerator
+class DiscussionsEnArticleGenerator < MarkdownEnArticleGenerator
   def initialize(release_date, groonga_version, groonga_previous_version, groonga_org_repository)
     super(release_date, groonga_version, groonga_previous_version, groonga_org_repository)
     @output_file_path = "./tmp/discussions-en-#{release_date}-groonga-#{groonga_version}.md"
-  end
-
-  def get_changes
-    changes = super
-    changes.gsub(/(\[.+?\])\((.+?).md(.*?\))/, "\\1(https://groonga.org/docs/\\2.html\\3")
+    @link_prefix = "https://groonga.org/docs"
   end
 
   def output_file
     Dir.mkdir("./tmp") unless Dir.exist?("./tmp")
     super
   end
+end
+
+class DiscussionsJaArticleGenerator < MarkdownJaArticleGenerator
+  def initialize(release_date, groonga_version, groonga_previous_version, groonga_org_repository)
+    super(release_date, groonga_version, groonga_previous_version, groonga_org_repository)
+    @output_file_path = "./tmp/discussions-ja-#{release_date}-groonga-#{groonga_version}.md"
+    @link_prefix = "https://groonga.org/ja/docs"
+  end
+
+  def output_file
+    Dir.mkdir("./tmp") unless Dir.exist?("./tmp")
+    super
+  end
+end
+
+class FacebookArticleGenerator < ArticleGenerator
+  def output_file
+    Dir.mkdir("./tmp") unless Dir.exist?("./tmp")
+    super
+  end
+
+  def get_article
+    template = get_template
+    changes = get_changes
+    template % {
+        groonga_version:@groonga_version, 
+        groonga_version_in_link:@groonga_version_in_link, 
+        changes: changes,
+        release_date_in_link: @release_date_in_link
+      }
+  end
+
+  def get_changes
+    changes = ""
+    # The first line is "======", which is meaningless and should be skip.
+    first = true
+    enumerate_changes do |line|
+      if first
+        first = false
+        next
+      end
+      changes << line
+    end
+    changes.gsub(/^\R\R/, "\n").strip
+  end
+end
+
+class FacebookEnArticleGenerator < FacebookArticleGenerator
+  def initialize(release_date, groonga_version, groonga_previous_version, groonga_org_repository)
+    super(release_date, groonga_version, groonga_previous_version, groonga_org_repository)
+    @input_file_path = "./locale/en/text/news.txt"
+    @output_file_path = "./tmp/facebook-en-#{release_date}-groonga-#{groonga_version}.txt"
+    @start_headline = "Release #{groonga_version}"
+    @finish_headline = "Release #{groonga_previous_version}"
+  end
 
   def get_template
     <<-"TEMPLATE"
-## Groonga %<groonga_version>s has been released
-
-[Groonga %<groonga_version>s](https://groonga.org/docs/news.html#release-%<groonga_version_in_link>s) has been released!
-
-How to install: [Install](https://groonga.org/docs/install.html)
-
-### Changes
-
-Here are important changes in this release:
+Hi,
+Groonga %<groonga_version>s has been released!
+    
+https://groonga.org/docs/news.html#release-%<groonga_version_in_link>s
+https://groonga.org/en/blog/%<release_date_in_link>s/groonga-%<groonga_version_in_link>s.html
+    
+Install: https://groonga.org/docs/install.html
+    
+Characteristics: https://groonga.org/docs/characteristic.html
+    
+The topics in this release:
 
 %<changes>s
 
-### Known Issues
+Known Issues
+============
 
 * Currently, Groonga has a bug that there is possible that data is corrupt when we execute many additions, delete, and update data to vector column.
 
-* `*<` and `*>` only valid when we use `query()` the right side of filter condition.
+* "*<" and "*>" only valid when we use "query()" the right side of filter condition.
+  If we specify as below, "*<" and "*>" work as &&.
 
-  If we specify as below, `*<` and `*>` work as `&&`.
+    'content @ "Groonga" *< content @ "Mroonga"'
 
-  * `'content @ "Groonga" *< content @ "Mroonga"'`
-
-* Groonga may not return records that should match caused by `GRN_II_CURSOR_SET_MIN_ENABLE`.
-
-### Conclusion
-
-Please refert to the following news for more details.
-
-[News Release %<groonga_version>s](https://groonga.org/docs/news.html#release-%<groonga_version_in_link>s)
-
-Let's search by Groonga!
+* Groonga may not return records that should match caused by "GRN_II_CURSOR_SET_MIN_ENABLE".
     TEMPLATE
   end
 end
 
-class DiscussionsJaArticleGenerator < JaArticleGenerator
+class FacebookJaArticleGenerator < FacebookArticleGenerator
   def initialize(release_date, groonga_version, groonga_previous_version, groonga_org_repository)
     super(release_date, groonga_version, groonga_previous_version, groonga_org_repository)
-    @output_file_path = "./tmp/discussions-ja-#{release_date}-groonga-#{groonga_version}.md"
-  end
-
-  def get_changes
-    changes = super
-    changes.gsub(/(\[.+?\])\((.+?).md(.*?\))/, "\\1(https://groonga.org/ja/docs/\\2.html\\3")
-  end
-
-  def output_file
-    Dir.mkdir("./tmp") unless Dir.exist?("./tmp")
-    super
+    @input_file_path = "./locale/ja/text/news.txt"
+    @output_file_path = "./tmp/facebook-ja-#{release_date}-groonga-#{groonga_version}.txt"
+    @start_headline = "#{groonga_version}"
+    @finish_headline = "#{groonga_previous_version}"
   end
 
   def get_template
     <<-"TEMPLATE"
-## Groonga %<groonga_version>sリリース
+Groonga %<groonga_version>s をリリースしました！
 
-[Groonga %<groonga_version>s](https://groonga.org/ja/docs/news.html#release-%<groonga_version_in_link>s)をリリースしました！
+    https://groonga.org/ja/docs/news.html#release-%<groonga_version_in_link>s
 
-それぞれの環境毎のインストール方法: [インストール](https://groonga.org/ja/docs/install.html)
+変更点一覧:
 
-### 変更内容
-    
+    https://groonga.org/ja/blog/%<release_date_in_link>s/groonga-%<groonga_version_in_link>s.html
+
+変更内容
+========
+
 主な変更点は以下の通りです。
 
 %<changes>s
 
-### 既知の問題
+既知の問題
+==========
 
 * 現在Groongaには、ベクターカラムに対してデータを大量に追加、削除、更新した際にデータが破損することがある問題があります。
 
-* `*<` と `*>` は、filter条件の右辺に `query()` を使う時のみ有効です。もし、以下のように指定した場合、 `*<` と `*>` は `&&` として機能します。
+* "*<" と "*>" は、filter条件の右辺に "query()" を使う時のみ有効です。もし、以下のように指定した場合、 "*<" と "*>" は "&&" として機能します。
 
-  ``
   'content @ "Groonga" *< content @ "Mroonga"'
-  ``
 
-* `GRN_II_CURSOR_SET_MIN_ENABLE` が原因でマッチするはずのレコードを返さないことがあります。
-
-### さいごに
-
-詳細については、以下のお知らせ、リリース自慢会も参照してください。
-
-[お知らせ %<groonga_version>sリリース](https://groonga.org/ja/docs/news.html#release-%<groonga_version_in_link>s)
-
-[リリース自慢会](https://www.youtube.com/playlist?list=PLKb0MEIU7gvRxTDecELqAOzOsa21dSwtU)
-
-リリース自慢会は、リリースノートを見ながら、Groongaの新機能・バグ修正を開発者が自慢する会です。
-
-毎月リリースされている最新バージョンについて、開発者的に「これ！」という機能や、「ここをがんばった！」ということを紹介しています。
-Liveチャットでコメントも受け付けていますので、気になっていることを質問したり、気になっていたあの機能について聞いたりすることも可能です。
-
-それでは、Groongaでガンガン検索してください！
+* "GRN_II_CURSOR_SET_MIN_ENABLE" が原因でマッチするはずのレコードを返さないことがあります。
     TEMPLATE
   end
 end
 
-generator_list = [
-  BlogEnArticleGenerator.new(release_date, groonga_version, groonga_previous_version, groonga_org_repository),
-  BlogJaArticleGenerator.new(release_date, groonga_version, groonga_previous_version, groonga_org_repository),
-  DiscussionsEnArticleGenerator.new(release_date, groonga_version, groonga_previous_version, groonga_org_repository),
-  DiscussionsJaArticleGenerator.new(release_date, groonga_version, groonga_previous_version, groonga_org_repository)
+class TwitterArticleBaseGenerator < ArticleGenerator
+  def get_article
+    template = get_template
+    template % {
+        groonga_version:@groonga_version, 
+        release_date: @release_date,
+        release_date_in_link: @release_date_in_link
+      }
+  end
+end
+
+class TwitterEnArticleBaseGenerator < TwitterArticleBaseGenerator
+  def initialize(release_date, groonga_version, groonga_previous_version, groonga_org_repository)
+    super(release_date, groonga_version, groonga_previous_version, groonga_org_repository)
+    @output_file_path = "./tmp/twitter-en-#{release_date}-groonga-#{groonga_version}-base.txt"
+  end
+
+  def get_template
+    "Groonga %<groonga_version>s has been released!(%<release_date>s) " + 
+    "https://groonga.org/en/blog/%<release_date_in_link>s/groonga-%<groonga_version>s.html"
+  end
+end
+
+class TwitterJaArticleBaseGenerator < TwitterArticleBaseGenerator
+  def initialize(release_date, groonga_version, groonga_previous_version, groonga_org_repository)
+    super(release_date, groonga_version, groonga_previous_version, groonga_org_repository)
+    @output_file_path = "./tmp/twitter-ja-#{release_date}-groonga-#{groonga_version}-base.txt"
+  end
+
+  def get_template
+    "Groonga %<groonga_version>sをリリースしました！(%<release_date>s) " +
+    "https://groonga.org/ja/blog/%<release_date_in_link>s/groonga-%<groonga_version>s.html"
+  end
+end
+
+option = {}
+OptionParser.new do |opt|
+  opt.on('--release-date VALUE', "YYYY-MM-DD") { |v| option[:release_date] = v }
+  opt.on('--groonga_version VALUE', "e.g. 12.1.1") { |v| option[:groonga_version] = v }
+  opt.on('--groonga_previous_version VALUE', "e.g. 12.1.0") { |v| option[:groonga_previous_version] = v }
+  opt.on('--groonga_org_repository VALUE', "e.g. $HOME/work/groonga.org") { |v| option[:groonga_org_repository] = v }
+  opt.parse!(ARGV)
+end
+
+generator_class_list = [
+  BlogEnArticleGenerator,
+  BlogJaArticleGenerator,
+  DiscussionsEnArticleGenerator,
+  DiscussionsJaArticleGenerator,
+  FacebookEnArticleGenerator,
+  FacebookJaArticleGenerator,
+  TwitterEnArticleBaseGenerator,
+  TwitterJaArticleBaseGenerator
 ]
 
-generator_list.each do |generator|
+generator_class_list.each do |generator_class|
+  generator = generator_class.new(option[:release_date],
+                                  option[:groonga_version], 
+                                  option[:groonga_previous_version],
+                                  option[:groonga_org_repository])
   generator.output_file
   puts generator.output_file_path
 end
