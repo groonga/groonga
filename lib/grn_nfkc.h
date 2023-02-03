441d8002fb3734365e81284c59e6fa677a231d17
Takashi Hashida <hashida@clear-code.com>
2023-02-03 14:00:36 +0900

    NormalizerNFKC:  add `unify_katakana_z_sounds` option (#1502)
    When `unify_katakana_z_sounds` is specified, `NormalizerNFKC*` normalize
    characters as below.
    
    ズァ -> ザ
    ズィ -> ジ
    ズェ -> ゼ
    ズォ -> ゾ
    
    
    Usage: 
    
    ```
    normalize \
      'NormalizerNFKC130("unify_katakana_z_sounds", true, \
                         "report_source_offset", true)' \
      "ズァズィズェズォ" \
      WITH_CHECKS|WITH_TYPES
    ```

--- lib/grn_nfkc.h
+++ lib/grn_nfkc.h
@@ -44,6 +44,7 @@ typedef struct {
   grn_bool unify_middle_dot;
   grn_bool unify_katakana_v_sounds;
   grn_bool unify_katakana_bu_sound;
+  grn_bool unify_katakana_z_sounds;
   grn_bool unify_katakana_wo_sound;
   grn_bool unify_katakana_di_sound;
   grn_bool unify_katakana_g_sounds;
