a51e423b7b2adfe7d583ebb27590c7d8017e1053
Takashi Hashida <hashida@clear-code.com>
2023-02-21 19:00:54 +0900

    *ONP: skip if "interval == 0" That case means these are a same word at that context.

--- lib/ii.c
+++ lib/ii.c
@@ -11793,9 +11793,10 @@ grn_ii_select_data_check_near_element_intervals(grn_ctx *ctx,
       int32_t max_element_interval =
         GRN_INT32_VALUE_AT(data->max_element_intervals, i_interval);
       int32_t interval = pos - previous_pos;
-      if ((max_element_interval >= 0 &&
+      if (interval != 0 &&
+          ((max_element_interval >= 0 &&
            interval > max_element_interval) ||
-          interval < data->min_interval) {
+          interval < data->min_interval)) {
         return false;
       }
       previous_pos = pos;
