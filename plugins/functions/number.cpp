cbcc5fe39975a62dfb32173d1af28e779f77d102
Sutou Kouhei <kou@clear-code.com>
2024-01-21 21:40:36 +0900

    number_round: add

--- plugins/functions/number.cpp
+++ plugins/functions/number.cpp
@@ -177,6 +177,137 @@ namespace {
     return classed_number;
 #undef FUNCTION_NAME
   }
+
+  template <typename TYPE>
+  std::enable_if_t<std::is_integral_v<TYPE>, TYPE>
+  round_raw(TYPE value, int32_t n_digits)
+  {
+    if (n_digits >= 0) {
+      return value;
+    }
+    auto scale = std::pow(10, -n_digits);
+    return std::round(value / scale) * scale;
+  }
+
+  template <typename TYPE>
+  std::enable_if_t<std::is_floating_point_v<TYPE>, TYPE>
+  round_raw(TYPE value, int32_t n_digits)
+  {
+    if (n_digits == 0) {
+      return std::round(value);
+    }
+    if (n_digits > 0) {
+      auto scale = std::pow(10, n_digits);
+      return std::round(value * scale) / scale;
+    } else {
+      auto scale = std::pow(10, -n_digits);
+      return std::round(value / scale) * scale;
+    }
+  }
+
+  template <typename TYPE>
+  void
+  round(grn_ctx *ctx, grn_obj *value, int32_t n_digits_raw, grn_obj *rounded)
+  {
+    TYPE value_raw = grn::bulk::get<TYPE>(ctx, value, 0);
+    TYPE rounded_raw = round_raw<TYPE>(value_raw, n_digits_raw);
+    grn::bulk::set(ctx, rounded, rounded_raw);
+  }
+
+  grn_obj *
+  func_number_round(grn_ctx *ctx,
+                    int n_args,
+                    grn_obj **args,
+                    grn_user_data *user_data)
+  {
+#define FUNCTION_NAME "round"
+    if (n_args == 0 || n_args > 2) {
+      GRN_PLUGIN_ERROR(ctx,
+                       GRN_INVALID_ARGUMENT,
+                       "%s(): wrong number of arguments (%d for 1..2)",
+                       FUNCTION_NAME,
+                       n_args);
+      return grn_plugin_proc_alloc(ctx, user_data, GRN_DB_VOID, 0);
+    }
+
+    grn_obj *target = args[0];
+    if (!grn_obj_is_number_family_bulk(ctx, target)) {
+      grn_obj inspected;
+      GRN_TEXT_INIT(&inspected, 0);
+      grn_inspect(ctx, &inspected, target);
+      GRN_PLUGIN_ERROR(ctx,
+                       GRN_INVALID_ARGUMENT,
+                       "%s(): target value must be a number family value: %.*s",
+                       FUNCTION_NAME,
+                       (int)GRN_TEXT_LEN(&inspected),
+                       GRN_TEXT_VALUE(&inspected));
+      GRN_OBJ_FIN(ctx, &inspected);
+      return grn_plugin_proc_alloc(ctx, user_data, GRN_DB_VOID, 0);
+    }
+
+    int32_t n_digits = 0;
+    if (n_args >= 2) {
+      grn_obj *n_digits_object = args[1];
+      n_digits = grn_plugin_proc_get_value_int32(ctx,
+                                                 n_digits_object,
+                                                 n_digits,
+                                                 FUNCTION_NAME "(): n_digits");
+      if (ctx->rc != GRN_SUCCESS) {
+        return grn_plugin_proc_alloc(ctx, user_data, GRN_DB_VOID, 0);
+      }
+    }
+
+    auto rounded =
+      grn_plugin_proc_alloc(ctx, user_data, target->header.domain, 0);
+    switch (target->header.domain) {
+    case GRN_DB_UINT8:
+      round<uint8_t>(ctx, target, n_digits, rounded);
+      break;
+    case GRN_DB_INT8:
+      round<int8_t>(ctx, target, n_digits, rounded);
+      break;
+    case GRN_DB_UINT16:
+      round<uint16_t>(ctx, target, n_digits, rounded);
+      break;
+    case GRN_DB_INT16:
+      round<int16_t>(ctx, target, n_digits, rounded);
+      break;
+    case GRN_DB_UINT32:
+      round<uint32_t>(ctx, target, n_digits, rounded);
+      break;
+    case GRN_DB_INT32:
+      round<int32_t>(ctx, target, n_digits, rounded);
+      break;
+    case GRN_DB_UINT64:
+      round<uint64_t>(ctx, target, n_digits, rounded);
+      break;
+    case GRN_DB_INT64:
+      round<int64_t>(ctx, target, n_digits, rounded);
+      break;
+    case GRN_DB_FLOAT32:
+      round<float>(ctx, target, n_digits, rounded);
+      break;
+    case GRN_DB_FLOAT:
+      round<double>(ctx, target, n_digits, rounded);
+      break;
+    default:
+      {
+        grn_obj inspected;
+        GRN_TEXT_INIT(&inspected, 0);
+        grn_inspect(ctx, &inspected, target);
+        GRN_PLUGIN_ERROR(
+          ctx,
+          GRN_FUNCTION_NOT_IMPLEMENTED,
+          "%s(): unsupported type: %s",
+          FUNCTION_NAME,
+          grn_type_id_to_string_builtin(ctx, target->header.domain));
+        GRN_OBJ_FIN(ctx, &inspected);
+      }
+      break;
+    }
+    return rounded;
+#undef FUNCTION_NAME
+  }
 } // namespace
 
 extern "C" grn_rc
@@ -195,6 +326,16 @@ GRN_PLUGIN_REGISTER(grn_ctx *ctx)
                   -1,
                   GRN_PROC_FUNCTION,
                   func_number_classify,
+                  NULL,
+                  NULL,
+                  0,
+                  NULL);
+
+  grn_proc_create(ctx,
+                  "number_round",
+                  -1,
+                  GRN_PROC_FUNCTION,
+                  func_number_round,
                   NULL,
                   NULL,
                   0,
