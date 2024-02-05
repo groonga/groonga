e43345e41b587d566760ea44044ed04460491857
github-action-benchmark <github@users.noreply.github.com>
2024-02-05 18:24:09 +0900

    add Benchmark (googlecpp) benchmark result for 40177b47121d6fa276f4fad7142fc68e7989a50c

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1707125049249,
+  "repoUrl": "https://github.com/groonga/groonga",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "kou@clear-code.com",
+            "name": "Sutou Kouhei",
+            "username": "kou"
+          },
+          "committer": {
+            "email": "kou@clear-code.com",
+            "name": "Sutou Kouhei",
+            "username": "kou"
+          },
+          "distinct": true,
+          "id": "40177b47121d6fa276f4fad7142fc68e7989a50c",
+          "message": "ci benchmark: remove a needless option",
+          "timestamp": "2024-02-05T18:17:58+09:00",
+          "tree_id": "7eba45cecf008062f2e21fcc07078242e650d861",
+          "url": "https://github.com/groonga/groonga/commit/40177b47121d6fa276f4fad7142fc68e7989a50c"
+        },
+        "date": 1707125047570,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "short_text",
+            "value": 0.02649119700004121,
+            "unit": "s/iter",
+            "extra": "iterations: 5\ncpu: 0.0014410000000001366 s\nthreads: undefined"
+          }
+        ]
+      }
+    ]
+  }
+}
