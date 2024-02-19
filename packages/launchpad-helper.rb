864ddf042186ad9718ad55cc873d6b1e2d90cf64
Sutou Kouhei <kou@clear-code.com>
2024-02-19 11:46:07 +0900

    packages launchpad: fallback to Git information

--- packages/launchpad-helper.rb
+++ packages/launchpad-helper.rb
@@ -102,7 +102,12 @@ allow_unsigned_uploads = 0
         minor_version = ENV["UBUNTU_MINOR_VERSION"] || "1"
         version_suffix = "ubuntu#{version}.#{minor_version}"
         deb_version = "#{detect_current_deb_version}.#{version_suffix}"
-        sh("dch",
+        env = {
+          "DEBFULLNAME" => packager_name,
+          "DEBEMAIL" => packager_email,
+        }
+        sh(env,
+           "dch",
            "--distribution", code_name,
            "--newversion", deb_version,
            "Build for #{code_name}.")
