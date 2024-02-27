41d59a36922093b53fac114ff365a094ba71639c
Sutou Kouhei <kou@clear-code.com>
2024-02-27 17:18:54 +0900

    clang-format: add include/groonga/token.h

--- include/groonga/token.h
+++ include/groonga/token.h
@@ -20,7 +20,7 @@
 
 #ifdef __cplusplus
 extern "C" {
-#endif  /* __cplusplus */
+#endif /* __cplusplus */
 
 /*
  * grn_tokenize_mode describes propose for tokenization.
@@ -72,137 +72,118 @@ typedef uint32_t grn_token_status;
  *
  * @since 4.0.8
  */
-#define GRN_TOKEN_CONTINUE           (0)
+#define GRN_TOKEN_CONTINUE (0)
 /*
  * GRN_TOKEN_LAST means that the next token is the last one.
  *
  * @since 4.0.8
  */
-#define GRN_TOKEN_LAST               (0x01L<<0)
+#define GRN_TOKEN_LAST (0x01L << 0)
 /*
  * GRN_TOKEN_OVERLAP means that ...
  *
  * @since 4.0.8
  */
-#define GRN_TOKEN_OVERLAP            (0x01L<<1)
+#define GRN_TOKEN_OVERLAP (0x01L << 1)
 /*
  * GRN_TOKEN_UNMATURED means that ...
  *
  * @since 4.0.8
  */
-#define GRN_TOKEN_UNMATURED          (0x01L<<2)
+#define GRN_TOKEN_UNMATURED (0x01L << 2)
 /*
  * GRN_TOKEN_REACH_END means that ...
  *
  * @since 4.0.8
  */
-#define GRN_TOKEN_REACH_END          (0x01L<<3)
+#define GRN_TOKEN_REACH_END (0x01L << 3)
 /*
  * GRN_TOKEN_SKIP means that the token is skipped
  *
  * @since 4.0.8
  */
-#define GRN_TOKEN_SKIP               (0x01L<<4)
+#define GRN_TOKEN_SKIP (0x01L << 4)
 /*
  * GRN_TOKEN_SKIP_WITH_POSITION means that the token and postion is skipped
  *
  * @since 4.0.8
  */
-#define GRN_TOKEN_SKIP_WITH_POSITION (0x01L<<5)
+#define GRN_TOKEN_SKIP_WITH_POSITION (0x01L << 5)
 /*
  * GRN_TOKEN_FORCE_PREFIX means that the token is used common prefix search
  *
  * @since 4.0.8
  */
-#define GRN_TOKEN_FORCE_PREFIX       (0x01L<<6)
+#define GRN_TOKEN_FORCE_PREFIX (0x01L << 6)
 /*
  * GRN_TOKEN_KEEP_ORIGINAL means that the original token of the token is
  * also used. This is for token filter.
  *
  * @since 11.0.3
  */
-#define GRN_TOKEN_KEEP_ORIGINAL      (0x01L<<7)
+#define GRN_TOKEN_KEEP_ORIGINAL (0x01L << 7)
 
 typedef struct _grn_token grn_token;
 
-GRN_API grn_obj *grn_token_get_data(grn_ctx *ctx,
-                                    grn_token *token);
-GRN_API const char *grn_token_get_data_raw(grn_ctx *ctx,
-                                           grn_token *token,
-                                           size_t *length);
-GRN_API grn_rc grn_token_set_data(grn_ctx *ctx,
-                                  grn_token *token,
-                                  const char *str_ptr,
-                                  int str_length);
-GRN_API grn_id grn_token_get_domain(grn_ctx *ctx, grn_token *token);
-GRN_API grn_rc grn_token_set_domain(grn_ctx *ctx,
-                                    grn_token *token,
-                                    grn_id domain);
-GRN_API grn_token_status grn_token_get_status(grn_ctx *ctx,
-                                              grn_token *token);
-GRN_API grn_rc grn_token_set_status(grn_ctx *ctx,
-                                    grn_token *token,
-                                    grn_token_status status);
-GRN_API grn_rc grn_token_add_status(grn_ctx *ctx,
-                                    grn_token *token,
-                                    grn_token_status status);
-GRN_API grn_rc grn_token_remove_status(grn_ctx *ctx,
-                                       grn_token *token,
-                                       grn_token_status status);
+GRN_API grn_obj *
+grn_token_get_data(grn_ctx *ctx, grn_token *token);
+GRN_API const char *
+grn_token_get_data_raw(grn_ctx *ctx, grn_token *token, size_t *length);
+GRN_API grn_rc
+grn_token_set_data(grn_ctx *ctx,
+                   grn_token *token,
+                   const char *str_ptr,
+                   int str_length);
+GRN_API grn_id
+grn_token_get_domain(grn_ctx *ctx, grn_token *token);
+GRN_API grn_rc
+grn_token_set_domain(grn_ctx *ctx, grn_token *token, grn_id domain);
+GRN_API grn_token_status
+grn_token_get_status(grn_ctx *ctx, grn_token *token);
+GRN_API grn_rc
+grn_token_set_status(grn_ctx *ctx, grn_token *token, grn_token_status status);
+GRN_API grn_rc
+grn_token_add_status(grn_ctx *ctx, grn_token *token, grn_token_status status);
+GRN_API grn_rc
+grn_token_remove_status(grn_ctx *ctx,
+                        grn_token *token,
+                        grn_token_status status);
 GRN_API uint64_t
-grn_token_get_source_offset(grn_ctx *ctx,
-                            grn_token *token);
+grn_token_get_source_offset(grn_ctx *ctx, grn_token *token);
 GRN_API grn_rc
-grn_token_set_source_offset(grn_ctx *ctx,
-                            grn_token *token,
-                            uint64_t offset);
+grn_token_set_source_offset(grn_ctx *ctx, grn_token *token, uint64_t offset);
 GRN_API uint32_t
-grn_token_get_source_length(grn_ctx *ctx,
-                            grn_token *token);
+grn_token_get_source_length(grn_ctx *ctx, grn_token *token);
 GRN_API grn_rc
-grn_token_set_source_length(grn_ctx *ctx,
-                            grn_token *token,
-                            uint32_t length);
+grn_token_set_source_length(grn_ctx *ctx, grn_token *token, uint32_t length);
 GRN_API uint32_t
-grn_token_get_source_first_character_length(grn_ctx *ctx,
-                                            grn_token *token);
+grn_token_get_source_first_character_length(grn_ctx *ctx, grn_token *token);
 GRN_API grn_rc
 grn_token_set_source_first_character_length(grn_ctx *ctx,
                                             grn_token *token,
                                             uint32_t length);
 GRN_API grn_bool
-grn_token_have_overlap(grn_ctx *ctx,
-                       grn_token *token);
+grn_token_have_overlap(grn_ctx *ctx, grn_token *token);
 GRN_API grn_rc
-grn_token_set_overlap(grn_ctx *ctx,
-                      grn_token *token,
-                      grn_bool have_overlap);
+grn_token_set_overlap(grn_ctx *ctx, grn_token *token, grn_bool have_overlap);
 
 GRN_API grn_obj *
-grn_token_get_metadata(grn_ctx *ctx,
-                       grn_token *token);
+grn_token_get_metadata(grn_ctx *ctx, grn_token *token);
 GRN_API bool
-grn_token_get_force_prefix_search(grn_ctx *ctx,
-                                  grn_token *token);
+grn_token_get_force_prefix_search(grn_ctx *ctx, grn_token *token);
 GRN_API grn_rc
 grn_token_set_force_prefix_search(grn_ctx *ctx,
                                   grn_token *token,
                                   grn_bool force);
 GRN_API uint32_t
-grn_token_get_position(grn_ctx *ctx,
-                       grn_token *token);
+grn_token_get_position(grn_ctx *ctx, grn_token *token);
 GRN_API grn_rc
-grn_token_set_position(grn_ctx *ctx,
-                       grn_token *token,
-                       uint32_t position);
+grn_token_set_position(grn_ctx *ctx, grn_token *token, uint32_t position);
 GRN_API float
-grn_token_get_weight(grn_ctx *ctx,
-                     grn_token *token);
+grn_token_get_weight(grn_ctx *ctx, grn_token *token);
 GRN_API grn_rc
-grn_token_set_weight(grn_ctx *ctx,
-                     grn_token *token,
-                     float weight);
+grn_token_set_weight(grn_ctx *ctx, grn_token *token, float weight);
 
 #ifdef __cplusplus
-}  /* extern "C" */
-#endif  /* __cplusplus */
+} /* extern "C" */
+#endif /* __cplusplus */
