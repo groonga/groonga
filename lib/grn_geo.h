17862e1130ce81378c86289f3b2ffb3d720fa30f
Sutou Kouhei <kou@clear-code.com>
2024-02-29 11:49:47 +0900

    clang-format: add lib/grn_geo.h

--- lib/grn_geo.h
+++ lib/grn_geo.h
@@ -22,14 +22,14 @@
 #include "grn_db.h"
 
 #if defined(WIN32) || defined(__sun)
-# define _USE_MATH_DEFINES
-# ifndef MAX
-#  define MAX(a, b) ((a) > (b) ? (a) : (b))
-# endif
+#  define _USE_MATH_DEFINES
+#  ifndef MAX
+#    define MAX(a, b) ((a) > (b) ? (a) : (b))
+#  endif
 
-# ifndef MIN
-#  define MIN(a, b) ((a) < (b) ? (a) : (b))
-# endif
+#  ifndef MIN
+#    define MIN(a, b) ((a) < (b) ? (a) : (b))
+#  endif
 #endif /* WIN32 or __sun */
 #include <math.h>
 
@@ -37,21 +37,21 @@
 extern "C" {
 #endif
 
-#define GRN_GEO_RESOLUTION   3600000
-#define GRN_GEO_RADIUS       6357303
-#define GRN_GEO_BES_C1       6334834
-#define GRN_GEO_BES_C2       6377397
-#define GRN_GEO_BES_C3       0.006674
-#define GRN_GEO_GRS_C1       6335439
-#define GRN_GEO_GRS_C2       6378137
-#define GRN_GEO_GRS_C3       0.006694
+#define GRN_GEO_RESOLUTION    3600000
+#define GRN_GEO_RADIUS        6357303
+#define GRN_GEO_BES_C1        6334834
+#define GRN_GEO_BES_C2        6377397
+#define GRN_GEO_BES_C3        0.006674
+#define GRN_GEO_GRS_C1        6335439
+#define GRN_GEO_GRS_C2        6378137
+#define GRN_GEO_GRS_C3        0.006694
 
 #define GRN_GEO_MAX_LATITUDE  324000000       /*  90 * 60 * 60 * 1000 */
 #define GRN_GEO_MAX_LONGITUDE (648000000 - 1) /* 180 * 60 * 60 * 1000 - 1 */
 #define GRN_GEO_MIN_LATITUDE  -GRN_GEO_MAX_LATITUDE
 #define GRN_GEO_MIN_LONGITUDE -GRN_GEO_MAX_LONGITUDE
 
-#define GRN_GEO_KEY_MAX_BITS 64
+#define GRN_GEO_KEY_MAX_BITS  64
 
 typedef enum {
   GRN_GEO_APPROXIMATE_RECTANGLE,
@@ -60,12 +60,12 @@ typedef enum {
 } grn_geo_approximate_type;
 
 typedef enum {
-  GRN_GEO_CURSOR_ENTRY_STATUS_NONE            = 0,
-  GRN_GEO_CURSOR_ENTRY_STATUS_TOP_INCLUDED    = 1 << 0,
+  GRN_GEO_CURSOR_ENTRY_STATUS_NONE = 0,
+  GRN_GEO_CURSOR_ENTRY_STATUS_TOP_INCLUDED = 1 << 0,
   GRN_GEO_CURSOR_ENTRY_STATUS_BOTTOM_INCLUDED = 1 << 1,
-  GRN_GEO_CURSOR_ENTRY_STATUS_LEFT_INCLUDED   = 1 << 2,
-  GRN_GEO_CURSOR_ENTRY_STATUS_RIGHT_INCLUDED  = 1 << 3,
-  GRN_GEO_CURSOR_ENTRY_STATUS_LATITUDE_INNER  = 1 << 4,
+  GRN_GEO_CURSOR_ENTRY_STATUS_LEFT_INCLUDED = 1 << 2,
+  GRN_GEO_CURSOR_ENTRY_STATUS_RIGHT_INCLUDED = 1 << 3,
+  GRN_GEO_CURSOR_ENTRY_STATUS_LATITUDE_INNER = 1 << 4,
   GRN_GEO_CURSOR_ENTRY_STATUS_LONGITUDE_INNER = 1 << 5
 } grn_geo_cursor_entry_status_flag;
 
@@ -112,11 +112,13 @@ typedef struct {
   grn_geo_cursor_area areas[GRN_GEO_N_AREAS];
 } grn_geo_cursor_in_rectangle;
 
-grn_rc grn_geo_cursor_close(grn_ctx *ctx, grn_obj *geo_cursor);
+grn_rc
+grn_geo_cursor_close(grn_ctx *ctx, grn_obj *geo_cursor);
 
-
-grn_rc grn_geo_resolve_approximate_type(grn_ctx *ctx, grn_obj *type_name,
-                                        grn_geo_approximate_type *type);
+grn_rc
+grn_geo_resolve_approximate_type(grn_ctx *ctx,
+                                 grn_obj *type_name,
+                                 grn_geo_approximate_type *type);
 
 /**
  * grn_geo_select_in_circle:
@@ -138,54 +140,82 @@ grn_rc grn_geo_resolve_approximate_type(grn_ctx *ctx, grn_obj *type_name,
  * are searched by @index. Found records are added to @res
  * table with @op operation.
  **/
-grn_rc grn_geo_select_in_circle(grn_ctx *ctx,
-                                grn_obj *index,
-                                grn_obj *center_point,
-                                grn_obj *distance,
-                                grn_geo_approximate_type approximate_type,
-                                grn_obj *res,
-                                grn_operator op);
+grn_rc
+grn_geo_select_in_circle(grn_ctx *ctx,
+                         grn_obj *index,
+                         grn_obj *center_point,
+                         grn_obj *distance,
+                         grn_geo_approximate_type approximate_type,
+                         grn_obj *res,
+                         grn_operator op);
 
-grn_rc grn_selector_geo_in_circle(grn_ctx *ctx, grn_obj *table, grn_obj *index,
-                                  int nargs, grn_obj **args,
-                                  grn_obj *res, grn_operator op);
-grn_rc grn_selector_geo_in_rectangle(grn_ctx *ctx,
-                                     grn_obj *table, grn_obj *index,
-                                     int nargs, grn_obj **args,
-                                     grn_obj *res, grn_operator op);
+grn_rc
+grn_selector_geo_in_circle(grn_ctx *ctx,
+                           grn_obj *table,
+                           grn_obj *index,
+                           int nargs,
+                           grn_obj **args,
+                           grn_obj *res,
+                           grn_operator op);
+grn_rc
+grn_selector_geo_in_rectangle(grn_ctx *ctx,
+                              grn_obj *table,
+                              grn_obj *index,
+                              int nargs,
+                              grn_obj **args,
+                              grn_obj *res,
+                              grn_operator op);
 
-GRN_API grn_bool grn_geo_in_circle(grn_ctx *ctx, grn_obj *point, grn_obj *center,
-                           grn_obj *radius_or_point,
-                           grn_geo_approximate_type approximate_type);
-GRN_API grn_bool grn_geo_in_rectangle(grn_ctx *ctx, grn_obj *point,
-                                      grn_obj *top_left, grn_obj *bottom_right);
-grn_bool grn_geo_in_rectangle_raw(grn_ctx *ctx, grn_geo_point *point,
-                                  grn_geo_point *top_left,
-                                  grn_geo_point *bottom_right);
-double grn_geo_distance(grn_ctx *ctx, grn_obj *point1, grn_obj *point2,
-                        grn_geo_approximate_type type);
-GRN_API double grn_geo_distance_rectangle(grn_ctx *ctx, grn_obj *point1,
-                                          grn_obj *point2);
-GRN_API double grn_geo_distance_sphere(grn_ctx *ctx, grn_obj *point1,
-                                       grn_obj *point2);
-GRN_API double grn_geo_distance_ellipsoid(grn_ctx *ctx, grn_obj *point1,
-                                          grn_obj *point2);
-double grn_geo_distance_rectangle_raw(grn_ctx *ctx,
-                                      grn_geo_point *point1,
-                                      grn_geo_point *point2);
-double grn_geo_distance_sphere_raw(grn_ctx *ctx,
-                                   grn_geo_point *point1,
-                                   grn_geo_point *point2);
-double grn_geo_distance_ellipsoid_raw(grn_ctx *ctx,
-                                      grn_geo_point *point1,
-                                      grn_geo_point *point2,
-                                      int c1, int c2, double c3);
-double grn_geo_distance_ellipsoid_raw_tokyo(grn_ctx *ctx,
-                                            grn_geo_point *point1,
-                                            grn_geo_point *point2);
-double grn_geo_distance_ellipsoid_raw_wgs84(grn_ctx *ctx,
-                                            grn_geo_point *point1,
-                                            grn_geo_point *point2);
+GRN_API grn_bool
+grn_geo_in_circle(grn_ctx *ctx,
+                  grn_obj *point,
+                  grn_obj *center,
+                  grn_obj *radius_or_point,
+                  grn_geo_approximate_type approximate_type);
+GRN_API grn_bool
+grn_geo_in_rectangle(grn_ctx *ctx,
+                     grn_obj *point,
+                     grn_obj *top_left,
+                     grn_obj *bottom_right);
+grn_bool
+grn_geo_in_rectangle_raw(grn_ctx *ctx,
+                         grn_geo_point *point,
+                         grn_geo_point *top_left,
+                         grn_geo_point *bottom_right);
+double
+grn_geo_distance(grn_ctx *ctx,
+                 grn_obj *point1,
+                 grn_obj *point2,
+                 grn_geo_approximate_type type);
+GRN_API double
+grn_geo_distance_rectangle(grn_ctx *ctx, grn_obj *point1, grn_obj *point2);
+GRN_API double
+grn_geo_distance_sphere(grn_ctx *ctx, grn_obj *point1, grn_obj *point2);
+GRN_API double
+grn_geo_distance_ellipsoid(grn_ctx *ctx, grn_obj *point1, grn_obj *point2);
+double
+grn_geo_distance_rectangle_raw(grn_ctx *ctx,
+                               grn_geo_point *point1,
+                               grn_geo_point *point2);
+double
+grn_geo_distance_sphere_raw(grn_ctx *ctx,
+                            grn_geo_point *point1,
+                            grn_geo_point *point2);
+double
+grn_geo_distance_ellipsoid_raw(grn_ctx *ctx,
+                               grn_geo_point *point1,
+                               grn_geo_point *point2,
+                               int c1,
+                               int c2,
+                               double c3);
+double
+grn_geo_distance_ellipsoid_raw_tokyo(grn_ctx *ctx,
+                                     grn_geo_point *point1,
+                                     grn_geo_point *point2);
+double
+grn_geo_distance_ellipsoid_raw_wgs84(grn_ctx *ctx,
+                                     grn_geo_point *point1,
+                                     grn_geo_point *point2);
 
 #ifdef __cplusplus
 }
