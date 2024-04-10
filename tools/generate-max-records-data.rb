52ec6e49f396e1a2a0f73970b7ac9146e3ad4518
Abe Tomoaki <abe@clear-code.com>
2024-04-10 15:04:10 +0900

    Add a script to help load the maximum number of records for TABLE_HASH_KEY (#1757)

---
+++ tools/generate-max-records-data.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+#
+# Copyright (C) 2024  Abe Tomoaki <abe@clear-code.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License version 2.1 as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+N_RECORDS = 2 ** 29
+
+puts(<<-COMMANDS)
+table_create Data TABLE_HASH_KEY Int64
+COMMANDS
+
+puts(<<-LOAD)
+load --table Data
+[
+LOAD
+N_RECORDS.times do |i|
+  puts(",") unless i.zero?
+  print("{\"_key\":#{i}}")
+end
+puts
+puts(<<-LOAD)
+]
+LOAD
