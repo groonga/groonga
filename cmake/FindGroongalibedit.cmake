ff726f802f134d9750029037e7c14d2b18dfb1cd
Watson <watson1978@gmail.com>
2024-08-02 17:28:13 +0900

    cmake libedit: fix typo
    Co-authored-by: Horimoto Yasuhiro <horimoto@clear-code.com>

--- cmake/FindGroongalibedit.cmake
+++ cmake/FindGroongalibedit.cmake
@@ -1,4 +1,4 @@
-# Copyright(C) 2043  Shizuo Fujita <fujita@clear-code.com>
+# Copyright(C) 2024  Shizuo Fujita <fujita@clear-code.com>
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
