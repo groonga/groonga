b5aa2fe5b5571898db13d3372e86b0969115b2c5
otegami <otegami@clear-code.com>
2024-08-27 11:11:03 +0900

    Update `grn_obj_set_value` documentation to clarify return codes

--- include/groonga/groonga.h
+++ include/groonga/groonga.h
@@ -1009,7 +1009,8 @@ grn_obj_get_values(grn_ctx *ctx, grn_obj *obj, grn_id offset, void **values);
  *              \ref GRN_OBJ_APPEND \ref GRN_OBJ_PREPEND
  *
  * \return \ref GRN_SUCCESS on success, or \ref GRN_INVALID_ARGUMENT if the
- *         record does not exist.
+ *         record does not exist, and the appropriate `grn_rc` for any other
+ *         errors.
  */
 GRN_API grn_rc
 grn_obj_set_value(
