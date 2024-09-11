67057347071208e00c8393b72cc00022071549e5
Abe Tomoaki <abe@clear-code.com>
2024-09-12 08:51:36 +0900

    docs: api/grn_table: move grn_table_update_by_id() reference to the header file
    GitHub: GH-1817
    
    Prepare to switch to documents automatically generated by Doxygen.

--- include/groonga/table.h
+++ include/groonga/table.h
@@ -178,6 +178,20 @@ grn_table_delete(grn_ctx *ctx,
  */
 GRN_API grn_rc
 grn_table_delete_by_id(grn_ctx *ctx, grn_obj *table, grn_id id);
+/**
+ * \brief Update the key of the record matching ID in the table.
+ *        This operation supports only \ref GRN_TABLE_DAT_KEY.
+ *
+ * \param ctx The context object.
+ * \param table The table.
+ * \param id ID of record to be updated.
+ * \param dest_key New key.
+ * \param dest_key_size Size of `dest_key` in bytes.
+ *
+ * \return \ref GRN_SUCCESS on success, the appropriate \ref grn_rc on error.
+ *         For example, \ref GRN_OPERATION_NOT_PERMITTED is returned if the
+ *         other than \ref GRN_TABLE_DAT_KEY is specified.
+ */
 GRN_API grn_rc
 grn_table_update_by_id(grn_ctx *ctx,
                        grn_obj *table,
