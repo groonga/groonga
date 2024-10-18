af9e205eaa019ec01418c58e5ca354c2bd866cae
otegami <otegami@clear-code.com>
2024-10-18 17:01:17 +0900

    Fix style

--- include/groonga/groonga.h
+++ include/groonga/groonga.h
@@ -1728,7 +1728,7 @@ grn_obj_delete_by_id(grn_ctx *ctx, grn_obj *db, grn_id id, bool remove_p);
  *               stored. For example, `char buffer[PATH_MAX]` can be used.
  *
  * \return \ref GRN_SUCCESS on success, or the appropriate \ref grn_rc on error.
- *         For example, \ref GRN_INVALID_ARGUMENT is returned if db` is not
+ *         For example, \ref GRN_INVALID_ARGUMENT is returned if `db` is not
  *         a database object or if `buffer` is `NULL`.
  */
 GRN_API grn_rc
