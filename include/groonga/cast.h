da5911be3359387282844aab5da083cd0252f78d
otegami <otegami@clear-code.com>
2024-10-23 09:03:00 +0900

    Improve the explanation of `dst`

--- include/groonga/cast.h
+++ include/groonga/cast.h
@@ -37,15 +37,15 @@ grn_caster_cast(grn_ctx *ctx, grn_caster *caster);
 /**
  * \brief Cast and append a value from the source bulk to the destination bulk.
  *
- * If `dest` is a reference type bulk. (Reference type bulk means that type of
- * `dest` is a table.), `add_record_if_not_exist` is used.
+ * If `dest` is a reference type bulk/vector, `add_record_if_not_exist` is used.
+ * Reference type bulk/vector means that type of `dest` is a table.
  * If `src` value doesn't exist in the table that is a type of `dest`. The `src`
  * value is added to the table.
  *
  * \param ctx The context object.
  * \param src The bulk object containing the value to be casted.
- * \param dest The bulk object specifying the target type and where the casted
- *             value will be stored.
+ * \param dest The bulk/vector object specifying the target type and where the
+ *             casted value will be stored.
  * \param add_record_if_not_exist If `dest` is a reference type bulk and the
  *                                `src` value doesn't exist in the target table,
  *                                setting this to true will add the `source`
