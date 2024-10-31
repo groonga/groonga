86df04d40038886c8b942b3112ac38f8f4b583ed
otegami <otegami@clear-code.com>
2024-10-31 12:09:20 +0900

    doc conf: allow include directives to contain Groonga command as a content
    This change resolves warnings during documentation generation related to
    the `include` directive containing content. By default, the include directive
    does not permit any content, which resulted in warnings like:
    
    ```console
    $ cmake \
        -S .
        -B ../groonga.doc
        --preset=doc && \
      cmake
        --build ../groonga.doc
    ...
    /home/otegami/work/c/groonga/doc/source/reference/functions/language_model_vectorize.md.rst:61: WARNING: 'include': Has content, but none permitted [myst.directive_parse]
    ...
    ```
    
    In the Groonga project, we need to embed the results of executing
    Groonga commands within include directives. To achieve this, we
    modify the default behavior of the `include` directive to allow it
    to contain content. This prevents the warnings and facilitates the
    integration of Groonga command outputs into the documentation.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -11,11 +11,17 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
+import docutils.parsers.rst.directives.misc
 import re
 import sphinx
 import sys, os
 import gettext
 from datetime import datetime
+
+# Hack to allow include directives to contain content.
+# This modification is necessary to embed Groonga execution commands
+# within include blocks. It prevents warnings during documentation generation.
+docutils.parsers.rst.directives.misc.Include.has_content = True
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
