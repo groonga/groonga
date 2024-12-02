dcd3bc84445c367ddca25fc92c5addeaffadac80
takuya kodama <otegami@clear-code.com>
2024-12-02 16:06:08 +0900

    release blog: generate blog post from release note (#2115)
    GitHub: GH-2114
    
    In this PR, we prepare a rake task for generating release
    announces in blog from the release note. 
    
    At the following PRs, we will implement the logic and contents
    step by step.
    
    ## What I checked in this PR
    
    - The rake task is registered.
    
    ```console
    $ rake -T | grep announce
    git ls-files
    rake release:blog:generate    # Generate release announce posts from a release note
    ```
    
    - Call this rake task
    
    ```console
    rake release:blog:generate
    git ls-files
    TODO: Generate release announce posts for Groonga
    ```

---
+++ release_task.rb
@@ -0,0 +1,41 @@
+# Copyright (C) 2024  Kodama Takuya <otegami@clear-code.com>
+# Copyright (C) 2024  Horimoto Yasuhiro <horimoto@clear-code.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+class ReleaseTask
+  include Rake::DSL
+
+  def initialize(package)
+    @package = package
+  end
+
+  def define
+    define_generate_blog_task
+  end
+
+  private
+
+  def define_generate_blog_task
+    namespace :release do
+      namespace :blog do
+        desc "Generate release announce posts from a release note"
+        task :generate do
+          puts "TODO: Generate release announce posts for #{@package}"
+        end
+      end
+    end
+  end
+end
