69da6fc7786025d54309cb55979882093ea71f91
Abe Tomoaki <abe@clear-code.com>
2025-01-21 09:54:12 +0900

    Update comment

--- lib/msgpack.c
+++ lib/msgpack.c
@@ -76,7 +76,7 @@ grn_msgpack_pack_raw_internal(grn_ctx *ctx,
                               size_t value_size,
                               grn_id value_domain)
 {
-  /* If the length is 0, if text, set to an empty string.
+  /* If the length is 0 and the given value is a text, set to an empty string.
    * Otherwise, set nil. */
   if (value_size == 0 && !grn_type_id_is_text_family(ctx, value_domain)) {
     msgpack_pack_nil(packer);
