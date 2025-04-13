c5adefd751633224ab7403ed69231ccecd7bcca1
otegami <otegami@clear-code.com>
2025-04-14 08:40:43 +0900

    docs: api/grn_expr move grn_expr_syntax_escape_query reference to the header file
    GitHub: GH-1817
    
    Prepare to switch to documents automatically generated by Doxygen.

--- include/groonga/expr.h
+++ include/groonga/expr.h
@@ -162,6 +162,21 @@ grn_expr_syntax_escape(grn_ctx *ctx,
                        const char *target_characters,
                        char escape_character,
                        grn_obj *escaped_query);
+/**
+ * \brief Escape special characters in a query.
+ *
+ * \param ctx The context object. Its encoding must match that of the input
+ *            \p query. It is used for allocating buffer for \p escaped_query.
+ * \param query The input query to be escaped.
+ * \param query_size The byte size of \p query. A value of -1 indicates that
+ *                   the query is null-terminated.
+ * \param escaped_query The buffer where the escaped query is stored.
+ *
+ * \return \ref GRN_SUCCESS on success, the appropriate \ref grn_rc on error.
+ *
+ * \see
+ * https://groonga.org/docs/reference/grn_expr/query_syntax.html
+ */
 GRN_API grn_rc
 grn_expr_syntax_escape_query(grn_ctx *ctx,
                              const char *query,
