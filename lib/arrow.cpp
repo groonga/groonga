e8a3362e63dd9c97824a22c89878fe2c32d78f43
Abe Tomoaki <abe@clear-code.com>
2025-04-15 15:54:43 +0900

    output: apache-arrow: Support for multiple metadata
    Currently only one metadata can be set.
    Change it so that multiple metadata can be set.
    
    Related: GitHub GH-2274

--- lib/arrow.cpp
+++ lib/arrow.cpp
@@ -2012,6 +2012,7 @@ namespace grnarrow {
     StreamWriter(grn_ctx *ctx, grn_obj *bulk)
       : ctx_(ctx),
         output_(ctx, bulk),
+        metadata_(),
         schema_builder_(),
         schema_(),
         writer_(),
@@ -2036,9 +2037,8 @@ namespace grnarrow {
     void
     add_metadata(const char *key, const char *value)
     {
-      arrow::KeyValueMetadata metadata;
-      metadata.Append(key, value);
-      auto status = schema_builder_.AddMetadata(metadata);
+      metadata_.Append(key, value);
+      auto status = schema_builder_.AddMetadata(metadata_);
       if (!status.ok()) {
         std::stringstream context;
         check(ctx_,
@@ -2526,6 +2526,7 @@ namespace grnarrow {
   private:
     grn_ctx *ctx_;
     BulkOutputStream output_;
+    arrow::KeyValueMetadata metadata_;
     arrow::SchemaBuilder schema_builder_;
     std::shared_ptr<arrow::Schema> schema_;
     std::shared_ptr<arrow::ipc::RecordBatchWriter> writer_;
