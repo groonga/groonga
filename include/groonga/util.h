267b40ec9f2c65668c492754087ab1ae2942abcc
takuya kodama <otegami@clear-code.com>
2025-05-22 10:33:24 +0900

    docs: api/grn_inspect move grn_inspect_indented reference to the header file (#2340)
    GitHub: GH-1817
        
    Prepare to switch to documents automatically generated by Doxygen.

--- include/groonga/util.h
+++ include/groonga/util.h
@@ -32,11 +32,11 @@ extern "C" {
  *
  * For example usage:
  * ```c
- *  grn_obj inspected;
- *  GRN_TEXT_INIT(&inspected, 0);
- *  grn_inspect(ctx, &inspected, obj);
- *  printf("%.*s\n", (int)GRN_TEXT_LEN(&inspected), GRN_TEXT_VALUE(&inspected));
- *  GRN_OBJ_FIN(ctx, &inspected);
+ * grn_obj inspected;
+ * GRN_TEXT_INIT(&inspected, 0);
+ * grn_inspect(ctx, &inspected, obj);
+ * printf("%.*s\n", (int)GRN_TEXT_LEN(&inspected), GRN_TEXT_VALUE(&inspected));
+ * GRN_OBJ_FIN(ctx, &inspected);
  * ```
  *
  * \param ctx The context object.
@@ -52,6 +52,48 @@ grn_inspect_indent(grn_ctx *ctx,
                    grn_obj *buffer,
                    grn_obj *content,
                    const char *indent);
+/**
+ * \brief Inspect specified object and produce an indented textual
+ *        representation.
+ *
+ * \since 4.0.8
+ *
+ * This function combines \ref grn_inspect and \ref grn_inspect_indent. It first
+ * inspects the given \p obj into a temporary buffer, then prepends the given
+ * \p indent string to each line of that inspected text and stores the result in
+ * \p buffer.
+ *
+ * \note The \p indent string is only applied when the inspected text contains
+ *       one or more newline characters (when the output spans multiple lines).
+ *
+ * For example usage:
+ * ```c
+ * grn_obj inspected;
+ * GRN_TEXT_INIT(&inspected, 0);
+ * grn_inspect_indented(ctx, &inspected, obj, "***");
+ * printf("%.*s\n", (int)GRN_TEXT_LEN(&inspected), GRN_TEXT_VALUE(&inspected));
+ * GRN_OBJ_FIN(ctx, &inspected);
+ * ```
+ *
+ * If \p obj is \ref GRN_TABLE_PAT_KEY table, it prints like the following.
+ * ```
+ * ***#<table:pat Users key:ShortText value:(nil) size:7 columns:[] \
+ *      default_tokenizer:(nil) normalizer:(nil) \
+ *      keys:["a", "b", "c", "d", "e", "f", "g"] subrec:none nodes:{
+ * ***4{0,5,0}
+ * ***  L:2{0,6,0}
+ * ***    L:1{0,7,0}
+ * ***      L:0{0,0,0}
+ * ***      R:1{0,7,0}("a")[01100001]
+ * ```
+ *
+ * \param ctx The context object.
+ * \param buffer The buffer where the indented inspected text will be stored.
+ * \param obj The target object to inspect.
+ * \param indent The indentation string to prepend to each line of output.
+ *
+ * \return The indented inspected object in text.
+ */
 GRN_API grn_obj *
 grn_inspect_indented(grn_ctx *ctx,
                      grn_obj *buffer,
