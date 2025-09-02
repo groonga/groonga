f2b7b2426bad8f8dd527f40fc63ffe708b6f6cdc
takuya kodama <otegami@clear-code.com>
2025-09-02 15:41:34 +0800

    NormalizerNFKC unify-iteration-mark: support Katakana Iteration Mark (U+30FD) (#2513)
    GitHub: GH-2487
    
    This PR supports for the Katakana Iteration Mark
    "ヽ" (U+30FD).
    
    This implementation handles:
    
    - バナヽ -> バナナ(repeat unvoiced katakana as-is)
    - バヽナ -> バハナ(repeat voiced katakana with voiced mark removed)

--- lib/normalizer.c
+++ lib/normalizer.c
@@ -3452,26 +3452,42 @@ grn_nfkc_normalize_unify_iteration_mark(grn_ctx *ctx,
   *n_used_bytes = current_length;
   *n_used_characters = 1;
 
-  /* U+309D HIRAGANA ITERATION MARK */
-  if (previous_length > 0 && current_length == 3 && current[0] == 0xe3 &&
-      current[1] == 0x82 && current[2] == 0x9d) {
-    const unsigned char *previous = current - previous_length;
-    if (GRN_CHAR_TYPE(grn_nfkc_char_type(previous)) == GRN_CHAR_HIRAGANA) {
-#  define N_HIRAGANA_BYTES 3
-      unsigned char unvoiced_buffer[N_HIRAGANA_BYTES];
-      const unsigned char *unvoiced_char =
-        grn_nfkc_normalize_unify_hiragana_voiced_sound_mark(previous,
-                                                            unvoiced_buffer);
-      for (size_t i = 0; i < N_HIRAGANA_BYTES; i++) {
-        unified_buffer[(*n_unified_bytes)++] = unvoiced_char[i];
-      }
-      data->previous_length = N_HIRAGANA_BYTES;
-#  undef N_HIRAGANA_BYTES
-      (*n_unified_characters)++;
+  const unsigned char *previous = current - previous_length;
 
-      return unified_buffer;
+#  define N_HIRAGANA_BYTES 3
+  if (current_length == 3 && current[0] == 0xe3 && current[1] == 0x82 &&
+      current[2] == 0x9d && previous_length == N_HIRAGANA_BYTES &&
+      GRN_CHAR_TYPE(grn_nfkc_char_type(previous)) == GRN_CHAR_HIRAGANA) {
+    /* U+309D HIRAGANA ITERATION MARK */
+    unsigned char unvoiced_buffer[N_HIRAGANA_BYTES];
+    const unsigned char *unvoiced_char =
+      grn_nfkc_normalize_unify_hiragana_voiced_sound_mark(previous,
+                                                          unvoiced_buffer);
+    for (size_t i = 0; i < N_HIRAGANA_BYTES; i++) {
+      unified_buffer[(*n_unified_bytes)++] = unvoiced_char[i];
     }
+    data->previous_length = N_HIRAGANA_BYTES;
+    (*n_unified_characters)++;
+    return unified_buffer;
   }
+#  undef N_HIRAGANA_BYTES
+#  define N_KATAKANA_BYTES 3
+  else if (current_length == 3 && current[0] == 0xe3 && current[1] == 0x83 &&
+           current[2] == 0xbd && previous_length == N_KATAKANA_BYTES &&
+           GRN_CHAR_TYPE(grn_nfkc_char_type(previous)) == GRN_CHAR_KATAKANA) {
+    /* U+30FD KATAKANA ITERATION MARK */
+    unsigned char unvoiced_buffer[N_KATAKANA_BYTES];
+    const unsigned char *unvoiced_char =
+      grn_nfkc_normalize_unify_katakana_voiced_sound_mark(previous,
+                                                          unvoiced_buffer);
+    for (size_t i = 0; i < N_KATAKANA_BYTES; i++) {
+      unified_buffer[(*n_unified_bytes)++] = unvoiced_char[i];
+    }
+    data->previous_length = N_KATAKANA_BYTES;
+    (*n_unified_characters)++;
+    return unified_buffer;
+  }
+#  undef N_KATAKANA_BYTES
 
   *n_unified_bytes = *n_used_bytes;
   *n_unified_characters = *n_used_characters;
