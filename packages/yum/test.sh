966312efa9e8bcab0fb12d13282fbfddb17f2b19
Horimoto Yasuhiro <horimoto@clear-code.com>
2026-08-31 23:46:03 +0900

    Pin grntest version

--- packages/yum/test.sh
+++ packages/yum/test.sh
@@ -92,7 +92,7 @@ if [ "${run_test}" = "yes" ]; then
     libffi-devel \
     make \
     redhat-rpm-config
-  MAKEFLAGS=-j$(nproc) gem install grntest
+  MAKEFLAGS=-j$(nproc) gem install grntest -v "1.7.5"
 
   export TZ=Asia/Tokyo
 
