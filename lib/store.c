881d77903e94122393973efebf551ef096156c1a
Horimoto Yasuhiro <horimoto@clear-code.com>
2026-09-25 21:24:43 +0900

    Chekking invalid values

--- lib/store.c
+++ lib/store.c
@@ -755,7 +755,8 @@ grn_ja_init_from_env(void)
                                           strlen(grn_ja_n_data_segments_env),
                                         NULL);
     }
-    if (grn_ja_n_data_segments > JA_N_DATA_SEGMENTS) {
+    if (grn_ja_n_data_segments > JA_N_DATA_SEGMENTS ||
+        grn_ja_n_data_segments == 0) {
       grn_ja_n_data_segments = JA_N_DATA_SEGMENTS;
     }
   }
